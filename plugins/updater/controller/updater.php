68111ab1ea8f6a80954339233c3af11f564467a3
Cesar.Saez.Rodriguez@gmail.com <Cesar.Saez.Rodriguez@gmail.com@fe615fe8-962f-0410-811c-9ded47c8d053>
2014-05-20 16:30:55 +0000

    subida versión que comprueba si está en la última versión o hay una nueva
    git-svn-id: http://facturascripts.googlecode.com/svn/branches/0.9@547 fe615fe8-962f-0410-811c-9ded47c8d053

--- plugins/updater/controller/updater.php
+++ plugins/updater/controller/updater.php
@@ -30,7 +30,29 @@ class updater extends fs_controller
 	{	
 		if( isset($_GET['buscar']) )
 		{
-			file_put_contents(sys_get_temp_dir()."/Tmpfile.zip", fopen("https://github.com/NeoRazorX/facturascripts/archive/master.zip", 'r'));
+			$tmpfile = sys_get_temp_dir().DIRECTORY_SEPARATOR."tmpfile.zip";
+			file_put_contents($tmpfile, fopen("https://github.com/NeoRazorX/facturascripts/archive/master.zip", 'r'));
+			$releaseZip = new PclZip($tmpfile);
+			if ($releaseZip->extract(PCLZIP_OPT_PATH, sys_get_temp_dir(),PCLZIP_OPT_BY_NAME, 
+									'facturascripts-master'.DIRECTORY_SEPARATOR.'VERSION',
+									PCLZIP_OPT_REMOVE_ALL_PATH) == 0) 
+			{
+				$this->new_error_msg("Hubo un error al descomprimir el fichero remoto: ".$releaseZip->errorInfo(true));
+  			}
+			$releaseVersion = file_get_contents(sys_get_temp_dir().DIRECTORY_SEPARATOR.'VERSION');
+			if (strcmp($this->getVersion(), $releaseVersion) == 0)
+			{
+				$this->new_message("Est&aacute;s actualizado a la &uacute;ltima versi&oacute;n de facturascripts: ".$releaseVersion);
+				unlink(sys_get_temp_dir().DIRECTORY_SEPARATOR.'VERSION');
+				unlink($tmpfile);
+			}
+			else
+			{				
+				$this->new_message("Hay una versi&oacute;n distinta disponible: tu versi&oacute;n es la "
+						.$this->getVersion()." y est&aacute; disponible la ".$releaseVersion.
+						"&nbsp;&nbsp;<a class='submit' style='padding: 10px;' href='index.php?page=".$this->template."&actualizar=TRUE'>ACTUALIZAR</a>");
+				$this->new_message("ATENCI&Oacute;N: PROCEDE CON CUIDADO. HAZ UN BACKUP COMPLETO DE LA BASE DE DATOS ANTES DE ACTUALIZAR");
+			}
 		}
 	}
 	
