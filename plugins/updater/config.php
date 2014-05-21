9c3287220f8afaf7d84103c687b86b8637c84490
Cesar.Saez.Rodriguez@gmail.com <Cesar.Saez.Rodriguez@gmail.com@fe615fe8-962f-0410-811c-9ded47c8d053>
2014-05-21 16:29:14 +0000

    error menor
    git-svn-id: http://facturascripts.googlecode.com/svn/branches/0.9@552 fe615fe8-962f-0410-811c-9ded47c8d053

--- plugins/updater/config.php
+++ plugins/updater/config.php
@@ -2,3 +2,4 @@
 $config['versionFile'] = "VERSION";
 $config['rootFolderOnRelease'] = "facturascripts-master";
 $config['remoteServer'] = "https://github.com/NeoRazorX/facturascripts/archive/master.zip";
+?>
