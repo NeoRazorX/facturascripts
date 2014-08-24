ac2221ee273eec06326f599c07b5790a837a1541
shawe.ewahs@gmail.com <shawe.ewahs@gmail.com@fe615fe8-962f-0410-811c-9ded47c8d053>
2014-08-24 01:50:56 +0000

    - Añadidos comentarios para que sea más claro para novatos - Añadida notificación de inserción - Faltaría completar el plugin con cosas básicas, no crea ni la tabla en la DB :P
    git-svn-id: http://facturascripts.googlecode.com/svn/branches/0.9@758 fe615fe8-962f-0410-811c-9ded47c8d053

--- plugins/gatos/controller/gatos.php
+++ plugins/gatos/controller/gatos.php
@@ -2,54 +2,68 @@
 
 class gatos extends fs_controller
 {
-    public function __construct() {
-        parent::__construct(__CLASS__, 'Gatos', 'demo', FALSE, TRUE);
-    }
-    
-    protected function process() {
-       /* Añade soporte para búsquedas */
-       $this->custom_search = TRUE;
-       
-        if( isset($_POST['peso']) )
-        {
-            $numero = $this->empresa->var2str($this->nuevo_numero());
-            $peso = $this->empresa->var2str(intval($_POST['peso']));
-            $entrada = $this->empresa->var2str($_POST['entrada']);
-            
-            if($_POST['fechafin'] != '')
-               $fecha_fin = $this->empresa->var2str($_POST['fechafin']);
-            else
-               $fecha_fin = $this->empresa->var2str(NULL);
-            
-            $this->db->exec("INSERT INTO gatos (numero,peso,entrada,fecha_fin)
-                    VALUES (".$numero.",".$peso.",".$entrada.",".$fecha_fin.");");
-        }
-        else if( isset($_GET['delete']) )
-        {
-           $this->db->exec("delete from gatos where numero = ".$this->empresa->var2str($_GET['delete']).";");
-           $this->new_message('Gato '.$_GET['delete'].' eliminado.');
-        }
-    }
-    
-    public function listar_gatos()
-    {
-       $sql = "select * from gatos;";
-       if( isset($_POST['query']) )
-          $sql = "select * from gatos where peso = ".$this->empresa->var2str($_POST['query']).";";
-       
-        $data = $this->db->select($sql);
-        if($data)
-            return $data;
-        else
-            return array();
-    }
-    
-    public function nuevo_numero()
-    {
-        $data = $this->db->select("select max(numero) as num from gatos;");
-        if($data)
-            return intval($data[0]['num']) + 1;
-        else
-            return 1;
-    }
+   public function __construct() {
+      /* Añade el submenú "Gatos" al menú "demo" */
+      parent::__construct(__CLASS__, 'Gatos', 'demo', FALSE, TRUE);
+   }
+   
+   protected function process() {
+      /* Añade soporte para búsquedas */
+      $this->custom_search = TRUE;
+      
+      /* Si se ha pasado un peso */
+      if( isset($_POST['peso']) )
+      {
+         /* var2str se utiliza para evitar SQL Injection */
+         $numero = $this->empresa->var2str($this->nuevo_numero());
+         $peso = $this->empresa->var2str(intval($_POST['peso']));
+         $entrada = $this->empresa->var2str($_POST['entrada']);
+         
+         /* Si se ha pasado una fecha fin */
+         if($_POST['fechafin'] != '') /* Se utiliza la fecha definida*/
+            $fecha_fin = $this->empresa->var2str($_POST['fechafin']);
+         else /* En caso contrario, se usa un valor nulo */
+            $fecha_fin = $this->empresa->var2str(NULL);
+         
+         /* Insertar en la base de datos */
+         $this->db->exec("INSERT INTO gatos (numero,peso,entrada,fecha_fin)
+                 VALUES (".$numero.",".$peso.",".$entrada.",".$fecha_fin.");");
+         /* Mostramos mensaje por pantalla */
+         $this->new_message('Gato '.$numero.' insertado.');
+      }
+      else if( isset($_GET['delete']) )
+      {
+         /* Borrar de la base de datos */
+         $this->db->exec("DELETE FROM gatos WHERE numero = ".$this->empresa->var2str($_GET['delete']).";");
+         /* Mostramos mensaje por pantalla */
+         $this->new_message('Gato '.$_GET['delete'].' eliminado.');
+      }
+   }
+   
+   public function listar_gatos()
+   {
+      $sql = "SELECT * FROM gatos;";
+      
+      /* Si se recibe una búsqueda se omite la consulta anterior*/
+      if( isset($_POST['query']) )
+         $sql = "SELECT * FROM gatos WHERE peso = ".$this->empresa->var2str($_POST['query']).";";
+      
+      /* Ejecutamos la consulta SQL */
+      $data = $this->db->select($sql);
+      if($data) /* Se devuelven los datos de la consulta SQL */
+         return $data;
+      else /* Se devuelve un array vacío porque no se han devuelto datos de la consulta SQL*/
+         return array();
+   }
+   
+   public function nuevo_numero()
+   {
+      /* Se obtiene el último número de la tabla */
+      $data = $this->db->select("SELECT max(numero) AS num FROM gatos;");
+      if($data) /* Al elemento devuelto se le suma 1 para el siguiente */
+         return intval($data[0]['num']) + 1;
+      else /* Si no se ha devuelto nada no hay elementos, el primero es 1 */
+         return 1;
+   }
+   
 }
