a033fd8600204c33972d65a12468fd5798464207
valengon@gmail.com <valengon@gmail.com@fe615fe8-962f-0410-811c-9ded47c8d053>
2014-09-02 16:38:53 +0000

    Probando NetBeans... que es la 1ª vez que lo uso... xD  (Modificado mi email en backupms.php)
    git-svn-id: http://facturascripts.googlecode.com/svn/branches/0.9@800 fe615fe8-962f-0410-811c-9ded47c8d053

--- plugins/backup_MySql/controller/backupms.php
+++ plugins/backup_MySql/controller/backupms.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * This file is part of FacturaSctipts
- * Copyright (C) 2014  Valentín González   		valengon@hotmail.com
+ * Copyright (C) 2014  Valentín González        valengon@gmail.com
  * Copyright (C) 2014  Carlos Garcia Gomez      neorazorx@gmail.com
  *
  * This program is free software: you can redistribute it and/or modify
