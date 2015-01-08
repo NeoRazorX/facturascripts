dfb48aa1ee273a9634fdebb38baa2b7aab1e57e9
Cesar.Saez.Rodriguez@gmail.com <Cesar.Saez.Rodriguez@gmail.com@fe615fe8-962f-0410-811c-9ded47c8d053>
2015-01-08 00:40:18 +0000

    actualizado updater con los nuevos requisitos
    git-svn-id: http://facturascripts.googlecode.com/svn/branches/0.9@1017 fe615fe8-962f-0410-811c-9ded47c8d053

--- plugins/updater/controller/updater.php
+++ plugins/updater/controller/updater.php
@@ -23,7 +23,7 @@ require_once 'plugins/updater/pclzip/pclzip.lib.php';
 class updater extends fs_controller
 {
 	private $config;
-   public $hay_update;
+   	public $hay_update;
 	
 	public function __construct()
 	{
@@ -33,29 +33,29 @@ class updater extends fs_controller
 	
 	protected function process()
 	{	
-      $this->hay_update = FALSE;
+      	$this->hay_update = FALSE;
 		$tmpfile = getcwd().'/update.zip';
 		
-      if( isset($_GET['todo_ok']) )
-      {
-         $this->new_message("FACTURASCRIPTS SE HA ACTUALIZADO.");
-         $this->new_message("Recuerda dar permisos de escritura a la carpeta tmp si no los tiene");
-         $this->new_message("INFO: Ha quedado una copia de seguridad del sistema anterior en ".$_GET['todo_ok']);
-      }
-      else if( isset($_GET['buscar']) )
+		if( isset($_GET['todo_ok']) )
+		{
+			$this->new_message("FACTURASCRIPTS SE HA ACTUALIZADO.");
+			$this->new_message("Recuerda dar permisos de escritura a la carpeta tmp si no los tiene");
+			$this->new_message("INFO: Ha quedado una copia de seguridad del sistema anterior en ".$_GET['todo_ok']);
+		}
+		else if( isset($_GET['buscar']) )
 		{
 			try 
 			{
 				// obtenemos la versión de la release
 				$releaseVersion = $this->__getReleaseVersion();
             
-				if (strcmp($this->getVersion(), $releaseVersion) == 0)
-				{
-					$this->new_message("Est&aacute;s actualizado a la &uacute;ltima versi&oacute;n de facturascripts: ".$releaseVersion);
+				if (strcmp($this->getVersion(), $releaseVersion) >= 0)
+				{ 
+					$this->new_message("Est&aacute;s actualizado a la &uacute;ltima versi&oacute;n de facturascripts: ".$this->getVersion());
 				}
 				else
 				{
-               $this->hay_update = TRUE;
+               		$this->hay_update = TRUE;
                
 					$this->new_message("La versión <b>".$releaseVersion."</b> de FacturaScripts está disponible.");
 					$this->new_message("ATENCIÓN: HAZ UN BACKUP COMPLETO DE LA BASE DE DATOS ANTES DE ACTUALIZAR.");
@@ -68,68 +68,79 @@ class updater extends fs_controller
 		}
 		else if( isset($_GET['actualizar']) ) // actualizar
 		{
-         // descargamos la release
-         $this->__downloadRelease($tmpfile);
-         
-			if( file_exists($tmpfile) )
+			try
 			{
-				try
+				$dirs = $this->__getAllSubDirectories(".",DIRECTORY_SEPARATOR);
+				$nowr = $this->__areWritable($dirs);
+				if (count($nowr) > 0)
 				{
-               $path = getcwd();
-               
-					// comprobamos que tenemos permisos de escritura en el directorio
-					$this->__checkForRights();
-					
-					// hacemos copia de seguridad en el directorio temporal del sistema
-					$backupDest = getcwd().'/backup-'.date("dmy-Hi");
-					$this->__systemBackup(getcwd(), $backupDest);
-					
-               /// eliminamos controladores para evitar conflictos
-               foreach( scandir($path.'/controller') as $f )
-               {
-                  if( substr($f, -4) == '.php' )
-                     unlink($path.'/controller/'.$f);
-               }
-               
-               /// también eliminamos los modelos para evitar conflictos
-               foreach( scandir($path.'/model') as $f )
-               {
-                  if( substr($f, -4) == '.php' )
-                     unlink($path.'/model/'.$f);
-               }
-               
-               // descomprimimos
-					$this->__descomprime($tmpfile);
-               
-					// permisos de escritura para tmp
-					$mode = 775;
-					chmod("tmp", octdec($mode));
-					
-					/// borramos el zip de la actualización
-					unlink($tmpfile);
-               
-               /// borramos los archivos temporales del motor de plantillas
-               foreach( scandir($path.'/tmp') as $f )
-               {
-                  if( substr($f, -4) == '.php' )
-                     unlink($path.'/tmp/'.$f);
-               }
-               
-               /// borramos la caché
-               $this->cache->clean();
-               
-               /// recargamos
-               header( 'Location: '.$this->url().'&todo_ok='.$backupDest );
+					$possixuid = posix_getuid();
+					$possixpwuid = posix_getpwuid ( $possixuid );
+					$message = "Los siguientes directorios no son escribibles: </br>";
+					foreach ($nowr as $dir)
+						$message .= $dir."</br>";
+					$message .= "Por favor, conceda permisos de escritura al usuario '".$possixpwuid['name']."' para estos directorios antes de continuar";
+					throw new Exception($message);
 				}
-				catch (Exception $e)
+				// descargamos la release
+				$this->__downloadRelease($tmpfile);
+				
+				if( file_exists($tmpfile) )
+				{					
+						$path = getcwd();					
+							
+						// hacemos copia de seguridad en el directorio temporal del sistema
+						$backupDest = getcwd().DIRECTORY_SEPARATOR.'backup-'.date("dmy-Hi");
+						$this->__systemBackup(getcwd(), $backupDest);
+
+						// llegados a este punto se creó correctamente la copia de seguridad (si no, habrían saltado excepciones)
+						// borramos todo salvo config.php, los directorios de backup, el fichero descargado y tmp
+						// Si algo falla, es importante mostrar la localización del fichero de backup !!
+						try 
+						{
+							$this->__delete(".", $tmpfile);
+						}
+						catch (Exception $e)
+						{
+							$msg = "Ha habido un problema al borrar el sistema antiguo:</br>";
+							$msg.= $e->getMessage();
+							$msg.= "</br>Por favor, restablezca la copia de seguridad localizada en ".$backupDest;
+							throw new Exception($msg);
+						}						
+				
+						// descomprimimos
+						$this->__descomprime($tmpfile);
+				
+						// permisos de escritura para tmp
+						$mode = 775;
+						chmod("tmp", octdec($mode));
+				
+						/// borramos el zip de la actualización
+						unlink($tmpfile);
+				
+						/// borramos los archivos temporales del motor de plantillas
+						foreach( scandir($path.'/tmp') as $f )
+						{
+							if( substr($f, -4) == '.php' )
+								unlink($path.'/tmp/'.$f);
+						}
+				
+						/// borramos la caché
+						$this->cache->clean();
+				
+						/// recargamos
+						header( 'Location: '.$this->url().'&todo_ok='.$backupDest );					
+					
+				}
+				else
 				{
-					$this->new_error_msg($e->getMessage());
+					throw new Exception("No se ha podido descargado el fichero de actualización. "
+							. "Vuelve a intentarlo en unos minutos.");
 				}
 			}
-			else
+			catch (Exception $e)
 			{
-				$this->new_error_msg("No se ha podido descargado el fichero de actualización. "
-                    . "Vuelve a intentarlo en unos minutos.");
+				$this->new_error_msg($e->getMessage());
 			}
 		}
 	}
@@ -188,11 +199,12 @@ class updater extends fs_controller
        */
       foreach( scandir($source) as $f )
       {
-         if( !in_array($f, array('.', '..', 'tmp', 'doc', 'update.zip')) AND substr($f, 0, 6) != 'backup')
+         if( !in_array($f, array('.', '..', 'tmp', 'doc', 'update.zip')) AND strcmp(substr($f, 0, 6) , 'backup') != 0)
          {
             if( is_dir($f) )
             {
-               mkdir($dest.DIRECTORY_SEPARATOR.$f);
+               if ( !mkdir($dest.DIRECTORY_SEPARATOR.$f))
+               		throw new Exception("Hubo un problema al crear la copia de seguridad: error creando el directorio ".$dest.DIRECTORY_SEPARATOR.$f);
                $this->__copyr($source.DIRECTORY_SEPARATOR.$f, $dest.DIRECTORY_SEPARATOR.$f);
             }
             else
@@ -206,16 +218,97 @@ class updater extends fs_controller
     */
    private function __copyr($source, $dest)
    {
-      foreach (
-              $iterator = new RecursiveIteratorIterator(
-                      new RecursiveDirectoryIterator($source, RecursiveDirectoryIterator::SKIP_DOTS),
-                      RecursiveIteratorIterator::SELF_FIRST) as $item
-      ) {
-         if ($item->isDir()) {
-            mkdir( $dest.DIRECTORY_SEPARATOR.$iterator->getSubPathName() );
-         } else {
-            copy( $item, $dest.DIRECTORY_SEPARATOR.$iterator->getSubPathName() );
+      foreach ( $iterator = new RecursiveIteratorIterator(
+                      			new RecursiveDirectoryIterator($source, RecursiveDirectoryIterator::SKIP_DOTS),
+                      			RecursiveIteratorIterator::SELF_FIRST) as $item )
+      {
+         if ($item->isDir())
+         {
+            if ( !mkdir( $dest.DIRECTORY_SEPARATOR.$iterator->getSubPathName() ))
+            	throw new Exception("Hubo un problema al crear la copia de seguridad: error creando el directorio ".
+            			$dest.DIRECTORY_SEPARATOR.$iterator->getSubPathName());
+         } 
+         else
+         {
+            if ( !copy( $item, $dest.DIRECTORY_SEPARATOR.$iterator->getSubPathName() ))
+	            throw new Exception("Hubo un problema al crear la copia de seguridad: error copiando el fichero ".$item." en el directorio ".
+	            		$dest.DIRECTORY_SEPARATOR.$iterator->getSubPathName());
          }
-      }
+      } // foreach
+   }
+   
+
+   /*
+    * Get Subdirectorios
+   */
+   private function __getAllSubDirectories( $directory, $directory_seperator )
+   {
+   		$dirs = array_map( function($item)use($directory_seperator){ return $item . $directory_seperator;}, array_filter( glob( $directory . '*' ), 'is_dir') );
+   
+	   	foreach( $dirs AS $dir )
+	   	{
+	   		if (strcmp ($dir, "../") != 0)
+	   			$dirs = array_merge( $dirs, $this->__getAllSubDirectories( $dir, $directory_seperator ) );
+	   	}
+   
+   		return $dirs;
+   }
+   
+   /*
+    * Comprueba que un listado de subdirectorios es escribible
+    * Devuelve una lista con los no escribibles 
+    *
+    */
+   private function __areWritable( $dirlist )
+   { 
+   		$notwritable = array();  	 
+	   	foreach( $dirlist as $dir )
+	   	{
+		   	if (!is_writable($dir) && strcmp ($dir, "../") != 0)
+		   		$notwritable[] = $dir;
+	   	}   	 
+   		return $notwritable;
+   }
+   
+   /*
+    * Borra el contenido de un directorio excluyendo config.php, tmp y directorios de backup
+    *  Se le pueden pasar un fichero adicional (normalmente el de la release)
+   *
+   */
+   private function __delete( $dir, $tmpfile )
+   {
+   	    // listamos los ficheros
+   		$list = glob( $dir . '*' );
+   		foreach ($list as $l)
+   		{
+   			if( !in_array($l, array('.', '..', 'tmp', 'config.php', $tmpfile)) AND strcmp(substr($l, 0, 6) , 'backup') != 0)
+   			{
+   				// si es directorio borramos recursivamente
+   				if (is_dir($l))
+   				{
+	   				foreach(new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dirPath, FilesystemIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST) as $path)
+	   				{
+	   					if($path->isDir() && !$path->isLink())
+	   					{
+	   						if (!rmdir($path->getPathname()))
+	   							throw new Exception("Hubo un problema al borrar el directorio ".$path->getPathname());
+	   					} 
+	   					else
+	   					{
+	   						if (!unlink($path->getPathname()))
+	   							throw new Exception("Hubo un problema al borrar el fichero ".$path->getPathname());
+	   					}
+	   				}
+	   				if (!rmdir($dirPath))
+	   					throw new Exception("Hubo un problema al borrar el directorio ".$dirPath);
+   				}
+   				// si no, borramos
+   				else
+   				{
+   					if (!unlink($l))
+	   					throw new Exception("Hubo un problema al borrar el fichero ".$l);
+   				}
+   			} // not in array
+   		} // foreach
    }
 }
