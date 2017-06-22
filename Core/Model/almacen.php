58f8fbcd8b447d35959b17d70fb76539e67dc3ec
Adelantia <alvaro.castro.tomas@gmail.com>
2017-06-22 09:36:04 +0200

    Correciones en nombre de variables

--- Core/Model/almacen.php
+++ Core/Model/almacen.php
@@ -150,10 +150,11 @@ class almacen extends \FacturaScripts\Core\Base\Model {
      * Devuelve TRUE si este es almacén predeterminado de la empresa.
      * @return boolean
      */
+    /*
     public function is_default() {
         return ( $this->codalmacen == $this->default_items->codalmacen() );
     }
-
+    */
     /**
      * Devuelve el almacén con codalmacen = $cod
      * @param string $cod
