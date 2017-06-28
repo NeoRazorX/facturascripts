cb47aa9e36308bc782a7d7ae1bf9e3c36e363d2f
Jose Antonio Cuello <jcuello@artextrading.com>
2017-06-28 15:24:27 +0200

    [Beta] Database class
    Aplicación de la clase Database para acceso a bases de datos MySql y
    PostgreSQL

--- Core/Base/DataBase.php
+++ Core/Base/DataBase.php
@@ -20,55 +20,166 @@
 
 namespace FacturaScripts\Core\Base;
 
+define('FS_FOREIGN_KEYS', '1');
+define('FS_DB_INTEGER', 'INTEGER');
+define('FS_CHECK_DB_TYPES', '1');
+
 /**
  * Clase genérica de acceso a la base de datos, ya sea MySQL o PostgreSQL.
- * Esta clase se utiliza únicamente para tener los métodos visibles en netbeans.
  *
  * @author Carlos García Gómez <neorazorx@gmail.com>
+ * @author Artex Trading sa <jcuello@artextrading.com>
  */
 class DataBase {
+    
+    /**
+     * El enlace con la base de datos.
+     * @var resource
+     */
+    private static $link;
 
-    private static $autoTransactions;
+    /**
+     * Enlace al motor de base de datos seleccionado en la configuración
+     * @var DatabaseEngine 
+     */
     private static $engine;
-    private static $tableList;
 
-    public function __construct() {
-        if (!isset(self::$engine)) {
-            
-            if (strtolower(FS_DB_TYPE) == 'mysql') {
-                self::$engine = new Mysql();
-            } else {
-                self::$engine = new Postgresql();
-            }
+    /**
+     * El enlace con las utilidades de base de datos.
+     * @var DataBaseUtils
+     */
+    private static $utils;
+    
+    /**
+     * Gestiona el log de todos los controladores, modelos y base de datos.
+     * @var MiniLog
+     */
+    private static $miniLog;
+    
+    /**
+     * Nº de selects ejecutados.
+     * @var integer 
+     */
+    private static $totalSelects;
 
-            self::$autoTransactions = TRUE;
-            self::$tableList = FALSE;
+    /**
+     * Nº de transacciones ejecutadas.
+     * @var integer 
+     */
+    private static $totalTransactions;
+        
+    /**
+     * Lista de tablas de la base de datos
+     * @var array
+     */
+    private static $tables;
+
+    /**
+     * Devuelve el número de selects ejecutados
+     * @return integer
+     */
+    public function getTotalSelects() {
+        return self::$totalSelects;
+    }
+
+    /**
+     * Devuele le número de transacciones realizadas
+     * @return integer
+     */
+    public function getTotalTransactions() {
+        return self::$totalTransactions;
+    }
+
+    /**
+     * Devuelve un array con los nombres de las tablas de la base de datos.
+     * @return array
+     */
+    public function getTables() {
+        if (count(self::$tables) == 0) {
+            self::$tables = self::$engine->listTables(self::$link);
         }
+        
+        return self::$tables;
     }
-
+    
     /**
-     * Devuelve el valor de autoTransacions, para saber si las transacciones
-     * automáticas están activadas o no.
-     * @return boolean
+     * Devuelve un array con las columnas de una tabla dada.
+     * @param string $tableName
+     * @return array
      */
-    public function getAutoTransactions() {
-        return self::$autoTransactions;
+    public function getColumns($tableName) {
+        $result = [];
+        $aux = $this->select(self::$engine->sqlColumns($tableName));
+        if ($aux) {
+            foreach ($aux as $data) {
+                $result[] = self::$engine->columnFromData($data);
+            }
+        }
+        return $result;
     }
-
+    
     /**
-     * Activa/desactiva las transacciones automáticas en la función exec()
-     * @param boolean $value
+     * Devuelve una array con las restricciones de una tabla.
+     * @param string $tableName
+     * @param boolean $extended
+     * @return array
      */
-    public function setAutoTransactions($value) {
-        self::$autoTransactions = $value;
+    public function getConstraints($tableName, $extended = FALSE) {
+        if ($extended) {
+            $sql = self::$engine->sqlConstraintsExtended($tableName);
+        } else {
+            $sql = self::$engine->sqlConstraints($tableName);
+        }
+        
+        $data = $this->select($sql);
+        $result = $data ? array_values($data) : [];        
+        return $result;
     }
-
+    
     /**
-     * Conecta a la base de datos.
-     * @return boolean
+     * Devuelve una array con los indices de una tabla dada.
+     * @param string $tableName
+     * @return array
      */
-    public function connect() {
-        return self::$engine->connect();
+    public function getIndexes($tableName) {        
+        $result = [];
+        $data = $this->select(self::$engine->sqlIndexes($tableName));
+        if ($data) {
+            foreach ($data as $row) {
+                $result[] = ['name' => $row['Key_name']];
+            }
+            
+        }
+        return $result;        
+    }
+    
+    /**
+     * Construye y prepara la clase para su uso
+     */
+    public function __construct() {
+        if (!isset(self::$link)) {
+            self::$miniLog = new MiniLog();
+            self::$totalSelects = 0;
+            self::$totalTransactions = 0;
+            self::$tables = [];
+
+            switch (strtolower(FS_DB_TYPE)) {
+                case 'mysql':
+                        self::$engine = new Mysql();
+                        break;
+
+                case 'postgresql':
+                        self::$engine = new Postgresql();
+                        break;
+
+                default:
+                        self::$engine = NULL;
+                        self::$miniLog->critical('No se reconoce el tipo de conexión. Debe ser MySQL o PostgreSQL');
+                        break;
+            }
+            
+            self::$utils = new DataBaseUtils(self::$engine);
+        }
     }
 
     /**
@@ -76,7 +187,26 @@ class DataBase {
      * @return boolean
      */
     public function connected() {
-        return self::$engine->connected();
+        return (bool) self::$link;
+    }
+
+    /**
+     * Conecta a la base de datos.
+     * @return boolean
+     */
+    public function connect() {
+        if ($this->connected()) {
+            return TRUE;
+        }
+
+        $error = '';
+        self::$link = self::$engine->connect($error);
+
+        if ($error != '') {
+            self::$miniLog->critical($error);
+        }
+
+        return $this->connected();
     }
 
     /**
@@ -84,100 +214,62 @@ class DataBase {
      * @return boolean
      */
     public function close() {
-        return self::$engine->close();
-    }
-
-    /**
-     * Devuelve el motor de base de datos usado y la versión.
-     * @return string
-     */
-    public function version() {
-        return self::$engine->version();
-    }
-
-    /**
-     * Devuelve el nº de selects a la base de datos.
-     * @return integer
-     */
-    public function getSelects() {
-        return self::$engine->getSelects();
-    }
-
-    /**
-     * Devuelve el nº de transacciones con la base de datos.
-     * @return integer
-     */
-    public function getTransactions() {
-        return self::$engine->getTransactions();
-    }
-
-    /**
-     * Devuelve un array con las columnas de una tabla dada.
-     * @param string $tableName
-     * @return mixed
-     */
-    public function getColumns($tableName) {
-        return self::$engine->getColumns($tableName);
-    }
-
-    /**
-     * Devuelve una array con las restricciones de una tabla dada.
-     * @param string $tableName
-     * @param boolean $extended
-     * @return mixed
-     */
-    public function getConstraints($tableName, $extended = FALSE) {
-        if ($extended) {
-            return self::$engine->getConstraintsExtended($tableName);
-        } else {
-            return self::$engine->getConstraints($tableName);
-        }
-    }
-
-    /**
-     * Devuelve una array con los indices de una tabla dada.
-     * @param string $tableName
-     * @return mixed
-     */
-    public function getIndexes($tableName) {
-        return self::$engine->getIndexes($tableName);
-    }
-
-    /**
-     * Devuelve un array con los nombres de las tablas de la base de datos.
-     * @return mixed
-     */
-    public function listTables() {
-        if (self::$tableList === FALSE) {
-            self::$tableList = self::$engine->listTables();
+        if (!$this->connected()) {
+            return TRUE;
         }
 
-        return self::$tableList;
+        $inTransaction = self::$engine->inTransaction(self::$link);
+        if ($inTransaction) {
+            if (!$this->rollback()) {
+                return FALSE;
+            }
+        }
+
+        if (self::$engine->close(self::$link)) {
+            self::$link = NULL;
+        }
+
+        return !$this->connected();
     }
 
     /**
-     * Devuelve TRUE si la tabla existe, FALSE en caso contrario.
-     * @param string $name
-     * @param mixed $list
+     * Inicia una transaccion en la base de datos
      * @return boolean
      */
-    public function tableExists($name, $list = FALSE) {
-        $result = FALSE;
-
-        if ($list === FALSE) {
-            $list = $this->listTables();
-        }
-
-        foreach ($list as $table) {
-            if ($table['name'] == $name) {
-                $result = TRUE;
-                break;
-            }
+    public function beginTransaction() {
+        $result = TRUE;
+        $inTransaction = self::$engine->inTransaction(self::$link);
+        if (!$inTransaction) {
+            self::$miniLog->sql('Begin Transaction');
+            $result = self::$engine->beginTransaction(self::$link);
         }
 
         return $result;
     }
-
+    
+    /**
+     * Graba las sentencias ejecutadas en la base de datos
+     * @return boolean
+     */
+    public function commit() {
+        $result = self::$engine->commit(self::$link);
+        if ($result) {
+            self::$miniLog->sql('Commit Transaction');
+            self::$totalTransactions++;
+        }
+        
+        return $result;
+    }
+    
+    /**
+     * Deshace las sentencias ejecutadas en la base de datos
+     * @return boolean
+     */
+    public function rollback() {        
+        self::$miniLog->sql('Rollback Transaction');
+        return self::$engine->rollback(self::$link);
+    }
+    
     /**
      * Ejecuta una sentencia SQL de tipo select, y devuelve un array con los resultados,
      * o false en caso de fallo.
@@ -185,7 +277,7 @@ class DataBase {
      * @return mixed
      */
     public function select($sql) {
-        return self::$engine->select($sql);
+        return $this->selectLimit($sql, 0, 0);
     }
 
     /**
@@ -199,62 +291,149 @@ class DataBase {
      * @return mixed
      */
     public function selectLimit($sql, $limit = FS_ITEM_LIMIT, $offset = 0) {
-        return self::$engine->selectLimit($sql, $limit, $offset);
+        if (!$this->connected()) {
+            return FALSE;
+        }
+
+        if ($limit > 0) {
+            $sql .= ' LIMIT ' . $limit . ' OFFSET ' . $offset . ';'; /// añadimos limit y offset a la consulta sql
+        }
+
+        self::$miniLog->sql($sql); /// añadimos la consulta sql al historial
+        $result = self::$engine->select(self::$link, $sql);
+        if (!$result) {
+            self::$miniLog->sql(self::$engine->errorMessage(self::$link));
+            return FALSE;
+        }
+
+        self::$totalSelects++;
+        return $result;
     }
 
     /**
      * Ejecuta sentencias SQL sobre la base de datos (inserts, updates o deletes).
      * Para hacer selects, mejor usar select() o selecLimit().
-     * Por defecto se inicia una transacción, se ejecutan las consultas, y si todo
-     * sale bien, se guarda, sino se deshace.
-     * Se puede evitar este modo de transacción si se pone false
-     * en el parametro transaction, o con la función setAutoTransactions(FALSE)
+     * Si no hay transacción abierta se inicia una, se ejecutan las consultas
+     * Si la transaccion la ha abierto en la llamada la cierra confirmando o descartando
+     * según haya ido todo bien o haya dado algún error
      * @param string $sql
-     * @param boolean $transaction
      * @return boolean
      */
-    public function exec($sql, $transaction = NULL) {
-        /// usamos self::$autoTransactions como valor por defecto para la función
-        if (is_null($transaction)) {
-            $transaction = self::$autoTransactions;
+    public function exec($sql) {
+        if (!$this->connected()) {
+            return FALSE;
         }
 
-        /// limpiamos la lista de tablas, ya que podría haber cambios al ejecutar este sql.
-        self::$tableList = FALSE;
+        self::$tables = []; /// limpiamos la lista de tablas, ya que podría haber cambios al ejecutar este sql.
 
-        return self::$engine->exec($sql, $transaction);
+        $inTransaction = self::$engine->inTransaction(self::$link);
+        if (!$inTransaction) {
+            $this->beginTransaction();
+        }
+               
+        self::$miniLog->sql($sql); /// añadimos la consulta sql al historial
+        $result = self::$engine->exec(self::$link, $sql);
+        if ($result) {
+            if (!$inTransaction) {
+                $result = self::$engine->commit(self::$link);
+            }
+        } else {
+            if (!$inTransaction) {
+                $error = self::$engine->errorMessage(self::$link)
+                        . '. La secuencia ocupa la posición ' 
+                        . count(self::$miniLog->read('sql'));
+
+                self::$miniLog->error($error); 
+                self::$engine->rollback(self::$link);
+            } 
+        }
+
+        return $result;
     }
 
     /**
-     * Devuleve el último ID asignado al hacer un INSERT en la base de datos.
-     * @return integer
+     * Devuleve el último ID asignado al hacer un INSERT
+     * en la base de datos.
+     * @return integer|boolean
      */
     public function lastval() {
-        return self::$engine->lastval();
+        $aux = $this->select(self::$engine->sqlLastValue());
+        return $aux ? $aux[0]['num'] : FALSE;
     }
 
     /**
-     * Inicia una transacción SQL.
-     * @return boolean
+     * Devuelve el motor de base de datos usado y la versión.
+     * @return mixed
      */
-    public function beginTransaction() {
-        return self::$engine->beginTransaction();
-    }
+    public function version() {
+        if (!$this->connected()) {
+            return FALSE;
+        }
 
+        return self::$engine->version(self::$link);
+    }    
+    
     /**
-     * Guarda los cambios de una transacción SQL.
+     * Devuelve TRUE si la tabla existe, FALSE en caso contrario.
+     * @param string $tableName
+     * @param mixed $list
      * @return boolean
      */
-    public function commit() {
-        return self::$engine->commit();
+    public function tableExists($tableName, $list = FALSE) {
+        if ($list === FALSE) {
+            $list = $this->getTables();
+        }
+        
+        return in_array($tableName, $list);
     }
-
+    
     /**
-     * Deshace los cambios de una transacción SQL.
+     * Realiza comprobaciones extra a la tabla.
+     * @param string $tableName
      * @return boolean
      */
-    public function rollback() {
-        return self::$engine->rollback();
+    public function checkTableAux($tableName) {
+        $error = '';
+        $result = self::$engine->checkTableAux(self::$link, $tableName, $error);
+        if (!$result) {
+            self::$miniLog->critical($error);
+        }
+        
+        return $result;
+    }   
+    
+    /**
+     * Crea la tabla con la estructura indicada.
+     * @param string $tableName
+     * @param array $xmlCols
+     * @param array $xmlCons
+     * @return boolean
+     */
+    public function generateTable($tableName, $xmlCols, $xmlCons) {
+        return self::$utils->generateTable($tableName, $xmlCols, $xmlCons);
+    }
+    
+    /**
+     * Compara dos arrays de restricciones, devuelve una sentencia SQL en caso de encontrar diferencias.
+     * @param string $tableName
+     * @param array $xmlCons
+     * @param array $dbCons
+     * @param boolean $deleteOnly
+     * @return boolean
+     */
+    public function compareConstraints($tableName, $xmlCons, $dbCons, $deleteOnly = FALSE) {
+        return self::$utils->compareConstraints($tableName, $xmlCons, $dbCons, $deleteOnly);
+    }
+    
+    /**
+     * Compara dos arrays de columnas, devuelve una sentencia sql en caso de encontrar diferencias.
+     * @param string $tableName
+     * @param array $xmlCols
+     * @param array $dbCols
+     * @return string
+     */
+    public function compareColumns($tableName, $xmlCols, $dbCols) {
+        return self::$utils->compareColumns($tableName, $xmlCols, $dbCols);
     }
 
     /**
@@ -263,9 +442,13 @@ class DataBase {
      * @return string
      */
     public function escapeString($str) {
-        return self::$engine->escapeString($str);
-    }
+        if (self::$engine) {
+            $str = self::$engine->escapeString(self::$link, $str);
+        }
 
+        return $str;
+    }
+    
     /**
      * Devuelve el estilo de fecha del motor de base de datos.
      * @return string
@@ -281,49 +464,5 @@ class DataBase {
      */
     public function sql2int($colName) {
         return self::$engine->sql2int($colName);
-    }
-
-    /**
-     * Compara dos arrays de columnas, devuelve una sentencia sql en caso de encontrar diferencias.
-     * @param string $tableName
-     * @param array $xmlCols
-     * @param array $dbCols
-     * @return string
-     */
-    public function compareColumns($tableName, $xmlCols, $dbCols) {
-        return self::$engine->compareColumns($tableName, $xmlCols, $dbCols);
-    }
-
-    /**
-     * Compara dos arrays de restricciones, devuelve una sentencia sql en caso de encontrar diferencias.
-     * @param string $tableName
-     * @param array $xmlCons
-     * @param array $dbCons
-     * @param boolean $deleteOnly
-     * @return string
-     */
-    public function compareConstraints($tableName, $xmlCons, $dbCons, $deleteOnly = FALSE) {
-        return self::$engine->compareConstraints($tableName, $xmlCons, $dbCons, $deleteOnly);
-    }
-
-    /**
-     * Devuelve la sentencia sql necesaria para crear una tabla con la estructura proporcionada.
-     * @param string $tableName
-     * @param array $xmlCols
-     * @param array $xmlCons
-     * @return string
-     */
-    public function generateTable($tableName, $xmlCols, $xmlCons) {
-        return self::$engine->generateTable($tableName, $xmlCols, $xmlCons);
-    }
-
-    /**
-     * Realiza comprobaciones extra a la tabla.
-     * @param string $tableName
-     * @return boolean
-     */
-    public function checkTableAux($tableName) {
-        return self::$engine->checkTableAux($tableName);
-    }
-
+    } 
 }
