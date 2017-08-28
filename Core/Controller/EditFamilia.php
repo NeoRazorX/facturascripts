eefe8ca25e5d995620f405e9f3f3b0aeb731d647
Jose Antonio Cuello <jcuello@artextrading.com>
2017-08-28 18:39:23 +0200

    - Añadido controladores para Familias y Fabricantes
    Añadidos controladores List y Edit para los modelos de Familias y
    Fabricantes

---
+++ Core/Controller/EditFamilia.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This file is part of FacturaScripts
+ * Copyright (C) 2013-2017  Carlos Garcia Gomez  carlos@facturascripts.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace FacturaScripts\Core\Controller;
+
+use FacturaScripts\Core\Base\ExtendedController;
+use FacturaScripts\Core\Model;
+
+/**
+ * Controlador para la edición de un registro del modelo Familia
+ *
+ * @author Carlos García Gómez <carlos@facturascripts.com>
+ * @author Artex Trading sa <jcuello@artextrading.com>
+ */
+class EditFamilia extends ExtendedController\EditController
+{
+    public function __construct(&$cache, &$i18n, &$miniLog, $className)
+    {
+        parent::__construct($cache, $i18n, $miniLog, $className);
+        
+        // Establecemos el modelo de datos
+        $this->model = new Model\Familia();
+    }
+    
+    public function privateCore(&$response, $user)
+    {
+        parent::privateCore($response, $user);
+    }
+        
+    public function getPageData()
+    {
+        $pagedata = parent::getPageData();
+        $pagedata['title'] = 'Familias';
+        $pagedata['icon'] = 'fa-object-group';
+        $pagedata['showonmenu'] = FALSE;
+        
+        return $pagedata;
+    }    
+}
--- Core/Model/Fabricante.php
+++ Core/Model/Fabricante.php
@@ -22,6 +22,7 @@ namespace FacturaScripts\Core\Model;
  * Un fabricante de artículos.
  *
  * @author Carlos García Gómez <neorazorx@gmail.com>
+ * @author Artex Trading sa <jcuello@artextrading.com>
  */
 class Fabricante
 {
@@ -51,47 +52,6 @@ class Fabricante
     }
 
     /**
-     * Devuelve la url donde ver/modificar estos datos
-     * @return string
-     */
-    public function url()
-    {
-        if ($this->codfabricante === null) {
-            return 'index.php?page=VentasFabricantes';
-        }
-        return 'index.php?page=VentasFabricante&cod=' . urlencode($this->codfabricante);
-    }
-
-    /**
-     * TODO
-     *
-     * @param int $len
-     *
-     * @return string
-     */
-    public function getNombre($len = 12)
-    {
-        if (mb_strlen($this->nombre) > $len) {
-            return substr($this->nombre, 0, $len) . '...';
-        }
-        return $this->nombre;
-    }
-
-    /**
-     * TODO
-     *
-     * @param int $offset
-     * @param int $limit
-     *
-     * @return array
-     */
-    public function getArticulos($offset = 0, $limit = FS_ITEM_LIMIT)
-    {
-        $articulo = new Articulo();
-        return $articulo->allFromFabricante($this->codfabricante, $offset, $limit);
-    }
-
-    /**
      * TODO
      * @return bool
      */
@@ -114,55 +74,6 @@ class Fabricante
     }
 
     /**
-     * Devuelve un array con todos los fabricantes
-     * @return array
-     */
-    public function all()
-    {
-        /// leemos la lista de la caché
-        $fablist = $this->cache->get('m_fabricante_all');
-        if (!$fablist) {
-            /// si la lista no está en caché, leemos de la base de datos
-            $sql = 'SELECT * FROM ' . $this->tableName() . ' ORDER BY nombre ASC;';
-            $data = $this->dataBase->select($sql);
-            if (!empty($data)) {
-                foreach ($data as $d) {
-                    $fablist[] = new Fabricante($d);
-                }
-            }
-
-            /// guardamos la lista en caché
-            $this->cache->set('m_fabricante_all', $fablist);
-        }
-
-        return $fablist;
-    }
-
-    /**
-     * TODO
-     *
-     * @param string $query
-     *
-     * @return array
-     */
-    public function search($query)
-    {
-        $fablist = [];
-        $query = self::noHtml(mb_strtolower($query, 'UTF8'));
-
-        $sql = 'SELECT * FROM ' . $this->tableName()
-            . " WHERE lower(nombre) LIKE '%" . $query . "%' ORDER BY nombre ASC;";
-        $data = $this->dataBase->select($sql);
-        if (!empty($data)) {
-            foreach ($data as $f) {
-                $fablist[] = new Fabricante($f);
-            }
-        }
-
-        return $fablist;
-    }
-
-    /**
      * Esta función es llamada al crear la tabla del modelo. Devuelve el SQL
      * que se ejecutará tras la creación de la tabla. útil para insertar valores
      * por defecto.
@@ -172,13 +83,5 @@ class Fabricante
     {
         $this->cleanCache();
         return 'INSERT INTO ' . $this->tableName() . " (codfabricante,nombre) VALUES ('OEM','OEM');";
-    }
-
-    /**
-     * TODO
-     */
-    private function cleanCache()
-    {
-        $this->cache->delete('m_fabricante_all');
     }
 }
