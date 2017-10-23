b2ac49487715801718665b37b508de52641105bd
RaulJJNazca <comercial@nazcanetworks.com>
2017-10-23 21:38:40 +0200

    Update WidgetItem.php

--- Core/Base/ExtendedController/WidgetItem.php
+++ Core/Base/ExtendedController/WidgetItem.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2013-2017  Carlos Garcia Gomez  carlos@facturascripts.com
+ * Copyright (C) 2013-2017  Carlos Garcia Gomez  <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -16,7 +16,10 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 namespace FacturaScripts\Core\Base\ExtendedController;
+
+
 /**
  * Description of WidgetItem
  *
@@ -24,60 +27,63 @@ namespace FacturaScripts\Core\Base\ExtendedController;
  */
 class WidgetItem
 {
+
     /**
      * Nombre del campo con los datos que visualiza el widget
      *
      * @var string
      */
     public $fieldName;
+
     /**
      * Tipo de widget que se visualiza
      *
      * @var string
      */
     public $type;
+
     /**
      * Información adicional para el usuario
      *
      * @var string
      */
     public $hint;
+
     /**
      * Indica que el campo es no editable
      *
      * @var boolean
      */
     public $readOnly;
+
     /**
      * Indica que el campo es obligatorio y debe contener un valor
      *
      * @var boolean
      */
     public $required;
+
     /**
      * Icono que se usa como valor o acompañante del widget
      *
      * @var string
      */
     public $icon;
+
     /**
      * Controlador destino al hacer click sobre los datos visualizados
      *
      * @var string
      */
     public $onClick;
+
     /**
      * Opciones visuales para configurar el widget
      *
      * @var array
      */
     public $options;
-    
-    /**
-     * atributo acceskey/Tecla de acceso rápido
-     * @var string
-     */
-    public $acceskey;
+
     /**
      * Constructor dinámico de la clase.
      * Crea un objeto Widget del tipo informado
@@ -89,24 +95,31 @@ class WidgetItem
         switch ($type) {
             case 'number':
                 return new WidgetItemNumber();
+
             case 'money':
                 return new WidgetItemMoney();
+
             case 'checkbox':
                 return new WidgetItemCheckBox();
+
             case 'datepicker':
                 return new WidgetItemDateTime();
+
             case 'select':
                 return new WidgetItemSelect();
+
             case 'radio':
                 return new WidgetItemRadio();
+
             default:
                 return new WidgetItemText($type);
         }
     }
+
     /**
      * Crea y carga la estructura de atributos en base a un archivo XML
      *
-     * @param SimpleXMLElement $column
+     * @param \SimpleXMLElement $column
      * @return WidgetItem
      */
     public static function newFromXMLColumn($column)
@@ -117,10 +130,11 @@ class WidgetItem
         $widget->loadFromXMLColumn($column, $widgetAtributes);
         return $widget;
     }
+
     /**
      * Crea y carga la estructura de atributos en base a la base de datos
      *
-     * @param SimpleXMLElement $column
+     * @param array $column
      * @return WidgetItem
      */
     public static function newFromJSONColumn($column)
@@ -130,6 +144,7 @@ class WidgetItem
         $widget->loadFromJSONColumn($column);
         return $widget;
     }
+
     /**
      * Constructor de la clase
      */
@@ -137,18 +152,19 @@ class WidgetItem
     {
         $this->fieldName = '';
         $this->hint = '';
-        $this->readOnly = FALSE;
-        $this->required = FALSE;
+        $this->readOnly = false;
+        $this->required = false;
         $this->icon = null;
         $this->onClick = '';
         $this->options = [];
     }
+
     /**
      * Carga el diccionario de atributos de un grupo de opciones o valores
      * del widget
      *
      * @param array            $property
-     * @param SimpleXMLElement $group
+     * @param \SimpleXMLElement $group
      */
     protected function getAttributesGroup(&$property, $group)
     {
@@ -163,36 +179,40 @@ class WidgetItem
             unset($values);
         }
     }
+
     /**
      * Carga la estructura de atributos en base a un archivo XML
      *
-     * @param SimpleXMLElement $column
-     * @param SimpleXMLElement $widgetAtributes
+     * @param \SimpleXMLElement $column
+     * @param \SimpleXMLElement $widgetAtributes
      */
     protected function loadFromXMLColumn($column, $widgetAtributes) {
         $this->fieldName = (string) $widgetAtributes->fieldname;
         $this->hint = (string) $widgetAtributes->hint;
-        $this->readOnly = (bool) boolval($widgetAtributes->readonly);
-        $this->required = (bool) boolval($widgetAtributes->required);
+        $this->readOnly = (bool) $widgetAtributes->readonly;
+        $this->required = (bool) $widgetAtributes->required;
         $this->icon = (string) $widgetAtributes->icon;
         $this->onClick = (string) $widgetAtributes->onclick;
+
         $this->getAttributesGroup($this->options, $column->widget->option);
     }
+
     /**
      * Carga la estructura de atributos en base a la base de datos
      *
-     * @param SimpleXMLElement $column
+     * @param array $column
      */
     protected function loadFromJSONColumn($column)
     {
         $this->fieldName = (string) $column['widget']['fieldName'];
         $this->hint = (string) $column['widget']['hint'];
-        $this->readOnly = (bool) boolval($column['widget']['readOnly']);
-        $this->required = (bool) boolval($column['widget']['required']);
+        $this->readOnly = (bool) $column['widget']['readOnly'];
+        $this->required = (bool) $column['widget']['required'];
         $this->icon = (string) $column['widget']['icon'];
         $this->onClick = (string) $column['widget']['onClick'];
         $this->options = (array) $column['widget']['options'];
     }
+
     /**
      * Indica si se cumple la condición para aplicar un Option Text
      *
@@ -202,21 +222,24 @@ class WidgetItem
      */
     private function canApplyOptions($optionValue, $valueItem)
     {
-        switch (substr($optionValue, 0, 1)) {
+        switch ($optionValue[0]) {
             case '<':
                 $optionValue = substr($optionValue, 1);
-                $result = (floatval($valueItem) < floatval($optionValue));
+                $result = ((float) $valueItem < (float) $optionValue);
                 break;
+
             case '>':
                 $optionValue = substr($optionValue, 1);
-                $result = (floatval($valueItem) > floatval($optionValue));
+                $result = ((float) $valueItem > (float) $optionValue);
                 break;
+
             default:
                 $result = ($optionValue == $valueItem);
                 break;
         }
         return $result;
     }
+
     /**
      * Genera el código CSS para el style del widget en base a los options
      *
@@ -239,8 +262,10 @@ class WidgetItem
                 break;
             }
         }
+
         return $html;
     }
+
     /**
      * Devuelve el código HTML para la visualización de un popover
      * con el texto indicado.
@@ -253,6 +278,7 @@ class WidgetItem
     {
         return empty($hint) ? '' : ' data-toggle="popover" data-placement="auto" data-trigger="hover" data-content="' . $hint . '" ';
     }
+
     /**
      * General el código html para la visualización del icono en el lado
      * izquierda de los datos
@@ -264,12 +290,15 @@ class WidgetItem
         if (empty($this->icon)) {
             return '';
         }
+
         $html = '<div class="input-group"><span class="input-group-addon">';
         if (strpos($this->icon, 'fa-') === 0) {
             return $html . '<i class="fa ' . $this->icon . '" aria-hidden="true"></i></span>';
         }
+
         return $html . '<i aria-hidden="true">' . $this->icon . '</i></span>';
     }
+
     /**
      * Genera el código html para atributos especiales como:
      * hint
@@ -281,10 +310,12 @@ class WidgetItem
     protected function specialAttributes()
     {
         $hint = $this->getHintHTML($this->hint);
-        $readOnly = (empty($this->readOnly)) ? '' : ' readonly="readonly"';
-        $required = (empty($this->required)) ? '' : ' required="required"';
+        $readOnly = empty($this->readOnly) ? '' : ' readonly="readonly"';
+        $required = empty($this->required) ? '' : ' required="required"';
+
         return $hint . $readOnly . $required;
     }
+
     /**
      * Devuelve el código HTML para lista de controles no especiales
      * @param mixed $value
@@ -297,15 +328,19 @@ class WidgetItem
         if ($value === null || $value === '') {
             return '';
         }
+
         if (empty($text)) {
             $text = $value;
         }
+
         $style = $this->getTextOptionsHTML($value);
         $html = (empty($this->onClick))
             ? '<span' . $style . '>' . $text . '</span>'
             : '<a href="?page=' . $this->onClick . '&code=' . $value . '"' . $style . '>' . $text . '</a>';
+
         return $html;
     }
+
     /**
      * Devuelve el código HTML para edición de controles no especiales
      * @param mixed $value
@@ -318,12 +353,16 @@ class WidgetItem
     {
         $fieldName = '"' . $this->fieldName . '"';
         $icon = $this->getIconHTML();
+
         $html = $icon
             . '<input id=' . $fieldName . ' type="' . $this->type . '" class="form-control' . $extraClass . '"'
             . 'name=' . $fieldName . ' value="' . $value . '"' . $specialAttributes . ' />';
+
         if (!empty($this->icon)) {
             $html .= '</div>';
         }
+
         return $html;
     }
 }
+ 
