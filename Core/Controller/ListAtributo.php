85fff3d63f46e0138510b8137d409e55e8f8c909
Fco. Antonio Moreno Pérez <famphuelva@gmail.com>
2017-10-25 16:53:28 +0200

    Añadido controlador y vista ListAtributo

---
+++ Core/Controller/ListAtributo.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * This file is part of FacturaScripts
+ * Copyright (C) 2013-2017  Carlos Garcia Gomez  <carlos@facturascripts.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace FacturaScripts\Core\Controller;
+
+use FacturaScripts\Core\Base\ExtendedController;
+
+/**
+ * Controlador para la lista de Atributo
+ *
+ * @author Carlos García Gómez <carlos@facturascripts.com>
+ * @author Artex Trading sa <jcuello@artextrading.com>
+ */
+class ListAtributo extends ExtendedController\ListController
+{
+    /**
+     * Devuelve los datos básicos de la página
+     *
+     * @return array
+     */
+    public function getPageData()
+    {
+        $pagedata = parent::getPageData();
+        $pagedata['title'] = 'Atributos';
+        $pagedata['icon'] = 'fa-list';
+        $pagedata['menu'] = 'warehouse';
+
+        return $pagedata;
+    }
+
+    /**
+     * Procedimiento para insertar vistas en el controlador
+     */
+    protected function createViews()
+    {
+        $className = $this->getClassName();
+        $this->addView('FacturaScripts\Core\Model\Atributo', $className);
+        $this->addSearchFields($className, ['nombre', 'codatributo']);
+
+        $this->addOrderBy($className, 'codatributo', 'code');
+        $this->addOrderBy($className, 'nombre', 'name');
+    }
+}
