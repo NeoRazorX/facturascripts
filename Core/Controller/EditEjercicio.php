ff0f0864eb7d65dc901b7d6597a86a27b7068935
RaulJJNazca <comercial@nazcanetworks.com>
2017-11-15 21:40:35 +0100

    Correccion a panel EditEjercicio. Sub-vistas cambiadas a ListView (No edit)

--- Core/Controller/EditEjercicio.php
+++ Core/Controller/EditEjercicio.php
@@ -38,104 +38,16 @@ class EditEjercicio extends ExtendedController\PanelController
 {
 
     /**
-     * Devuelve un array de grupos de epigrafes, el resultado estará filtrado
-     * por el ejercicio que se recibe como parámetro
-     * 
-     * @param integer exercise_code
-     * @return array
-     */
-    private function getGroups($exercise_code)
-    {
-        $groups = [];
-        $group_model = new GrupoEpigrafes();
-
-        $obj_groups = $group_model->all([new DataBase\DataBaseWhere('codejercicio' , $exercise_code)]);   
-      
-        foreach ($obj_groups as $group_item) {
-          
-            $item['value'] = $group_item -> idgrupo;
-            $item['title'] = $group_item -> descripcion;
-            $groups[] = $item;
-   
-        }
-        
-        return $groups;
-    }
-    /**
-     * Devuelve un array de epigrafes, el resultado estará filtrado
-     * por el ejercicio que se recibe como parámetro
-     * 
-     * @param integer exercise_code
-     * @return array
-     */
-    private function getEpigrafes($exercise_code)
-    {
-        $epigrafes = [];
-        $epigrafe_model = new Epigrafe();
-
-        $obj_epigrafes = $epigrafe_model->all([new DataBase\DataBaseWhere('codejercicio' , $exercise_code)]);   
-      
-        foreach ($obj_epigrafes as $epigrafe_item) {
-          
-            $item['value'] = $epigrafe_item -> idepigrafe;
-            $item['title'] = $epigrafe_item -> descripcion;
-            $epigrafes[] = $item;
-   
-        }
-        
-        return $epigrafes;
-    }
-    
-    /**
-     * Devuelve un array de Cuentas, el resultado estará filtrado
-     * por el ejercicio que se recibe como parámetro
-     * 
-     * @param integer exercise_code
-     * @return array
-     */
-    private function getCuentas($exercise_code)
-    {
-        $cuentas = [];
-        $cuentas_model = new Cuenta();
-
-        $obj_cuentas = $cuentas_model->all([new DataBase\DataBaseWhere('codejercicio' , $exercise_code)]);   
-      
-        foreach ($obj_cuentas as $cuenta_item) {
-          
-            $item['value'] = $cuenta_item -> idcuenta;
-            $item['title'] = $cuenta_item -> descripcion;
-            $cuentas[] = $item;
-   
-        }
-       
-        return $cuentas;
-    }
-    /**
      * Procedimiento para insertar vistas en el controlador
      */
     protected function createViews()
     {
         $this->addEditView('FacturaScripts\Core\Model\Ejercicio', 'EditEjercicio', 'exercise');
-        $this->addEditListView('FacturaScripts\Core\Model\GrupoEpigrafes', 'EditEjercicioGrupoEpigrafes', 'Grupo epigrafe');
-        $this->addEditListView('FacturaScripts\Core\Model\Epigrafe', 'EditEjercicioEpigrafe', 'Epigrafes');
-        $this->addEditListView('FacturaScripts\Core\Model\Cuenta', 'EditEjercicioCuenta', 'account','fa-book');
-        $this->addEditListView('FacturaScripts\Core\Model\SubCuenta', 'EditEjercicioSubCuenta', 'sub-account','fa-th-list fa-fw');
-        
-         /// Cargamos valores para Los grupos de Epigrafes para EditEjercicioEpigrafe
-        $columnGroupCode = $this->views['EditEjercicioEpigrafe']->columnForName('heading-account-group-id');
-        $groups = $this->getGroups($this->request->get('code'));
-        $columnGroupCode->widget->setValuesFromArray($groups);
-        
-        /// Cargamos los valores par los Epigrafs para la vista Cuentas filtrando por ejercicio
-        $columnEpigrafeCode = $this->views['EditEjercicioCuenta']->columnForName('accounting-heading');
-        $epigrafes = $this->getEpigrafes($this->request->get('code'));
-        $columnEpigrafeCode->widget->setValuesFromArray($epigrafes);
-        
-        /// Cargamos los valores para las Cuetnas para la vista SubCuentas filtrando por ejercicio
-      $columnCuentaCode = $this->views['EditEjercicioSubCuenta']->columnForName('account-id');
-        $cuentas = $this->getCuentas($this->request->get('code'));
-        $columnCuentaCode->widget->setValuesFromArray($cuentas);
-       
+        $this->addListView('FacturaScripts\Core\Model\GrupoEpigrafes', 'EditEjercicioGrupoEpigrafes', 'Grupo epigrafe');
+        $this->addListView('FacturaScripts\Core\Model\Epigrafe', 'EditEjercicioEpigrafe', 'Epigrafes');
+        $this->addListView('FacturaScripts\Core\Model\Cuenta', 'EditEjercicioCuenta', 'account','fa-book');
+        $this->addListView('FacturaScripts\Core\Model\SubCuenta', 'EditEjercicioSubCuenta', 'sub-account','fa-th-list fa-fw');
+           
     }
 
     /**
