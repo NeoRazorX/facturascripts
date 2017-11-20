7dde8815f6c38009acda91c0d1301b0cb0e2a367
Fco. Antonio Moreno Pérez <famphuelva@gmail.com>
2017-11-20 18:04:45 +0100

    Añadir vistas EditListView al controlador EditArticulo para poder  ver el stock relacionado y los artículos de proveedor  con la misma referencia.

---
+++ Core/Controller/PanelArticulo.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * This file is part of FacturaScripts
+ * Copyright (C) 2013-2017  Carlos Garcia Gomez  <carlos@facturascripts.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace FacturaScripts\Core\Controller;
+
+use FacturaScripts\Core\Base\ExtendedController;
+use FacturaScripts\Core\Base\DataBase;
+
+/**
+ * Description of PanelSettings
+ *
+ * @author Artex Trading sa <jcuello@artextrading.com>
+ * @author Fco. Antonio Moreno Pérez <famphuelva@gmail.com>
+ */
+class PanelArticulo extends ExtendedController\PanelController
+{
+    /**
+     * Procedimiento para insertar vistas en el controlador
+     */
+    protected function createViews()
+    {
+        $this->addEditView('FacturaScripts\Core\Model\Articulo', 'EditArticulo', 'products', 'fa-cubes');
+        $this->addListView('FacturaScripts\Core\Model\Articulo', 'ListFabricante', 'same-suppliers', 'fa-users');
+        $this->addListView('FacturaScripts\Core\Model\Articulo', 'ListFamilia', 'same-families', 'fa-object-group');
+        $this->addListView('FacturaScripts\Core\Model\Articulo', 'ListImpuesto', 'same-taxes', 'fa-plus-square-o'); 
+        $this->addListView('FacturaScripts\Core\Model\Articulo', 'ListArticuloProveedor', 'supplier-products', 'fa-cubes'); 
+        
+    }
+
+    /**
+     * Devuele el campo $fieldName del articulo
+     *
+     * @param string $fieldName
+     *
+     * @return mixed
+     */
+    private function getArticuFieldValue($fieldName)
+    {
+        $model = $this->views['EditArticulo']->getModel();
+        return $model->{$fieldName};
+    }
+
+    /**
+     * Procedimiento encargado de cargar los datos a visualizar
+     *
+     * @param string $keyView
+     * @param ExtendedController\EditView $view
+     */
+    protected function loadData($keyView, $view)
+    {
+        switch ($keyView) {
+            case 'EditArticulo':
+                $value = $this->request->get('code');
+                $view->loadData($value);
+                break;
+        
+            case 'ListFabricante':
+                $codfabricante = $this->getArticuFieldValue('codfabricante');
+
+                if (!empty($codfabricante)) {
+                    $where = [new DataBase\DataBaseWhere('codfabricante', $codfabricante)];
+                    $view->loadData($where);
+                }
+                break;
+                
+            case 'ListFamilia':
+                $codfamilia = $this->getArticuFieldValue('codfamilia');
+
+                if (!empty($codfamilia)) {
+                    $where = [new DataBase\DataBaseWhere('codfamilia', $codfamilia)];
+                    $view->loadData($where);
+                }
+                break;
+            
+            case 'ListImpuesto':
+                $codimpuesto = $this->getArticuFieldValue('codimpuesto');
+
+                if (!empty($codimpuesto)) {
+                    $where = [new DataBase\DataBaseWhere('codimpuesto', $codimpuesto)];
+                    $view->loadData($where);
+                }
+            case 'ListArticuloProveedor':
+                $referencia = $this->getArticuFieldValue('referencia');
+
+                if (!empty($referencia)) {
+                    $where = [new DataBase\DataBaseWhere('referencia', $referencia)];
+                    $view->loadData($where);
+                }
+                
+        }
+    }
+
+    /**
+     * Devuelve los datos básicos de la página
+     *
+     * @return array
+     */
+    public function getPageData()
+    {
+        $pagedata = parent::getPageData();
+        $pagedata['title'] = 'products';
+        $pagedata['icon'] = 'fa-cube';
+        $pagedata['showonmenu'] = false;
+
+        return $pagedata;
+    }
+}
