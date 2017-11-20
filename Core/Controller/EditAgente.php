d108dc2aa9512d9b11b98be687baa2c9ea900328
Raul Nazca <comercial@nazcanetworks.com>
2017-11-20 22:53:12 +0100

    Agente convertido a PanelController Añadidas ListView para presupuestos,albaranes y facturas

--- Core/Controller/EditAgente.php
+++ Core/Controller/EditAgente.php
@@ -17,16 +17,19 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 namespace FacturaScripts\Core\Controller;
-
+use FacturaScripts\Core\Base\DataBase;
 use FacturaScripts\Core\Base\ExtendedController;
+use FacturaScripts\Core\Model\PresupuestoCliente;
+use FacturaScripts\Core\Model\AlbaranCliente;
+use FacturaScripts\Core\Model\FacturaCliente;
 
 /**
  * Controller to edit a single item from the Agente model
  *
- * @author Raul
+ * @author Raul <comercial@nazcanetworks.com>
  *  Edit Agente class based upon Editcliente's functionality
  */
-class EditAgente extends ExtendedController\EditController
+class EditAgente extends ExtendedController\PanelController
 {
 
     /**
@@ -36,6 +39,55 @@ class EditAgente extends ExtendedController\EditController
     {
         return 'FacturaScripts\Core\Model\Agente';
     }
+    
+    /**
+     * Load views.
+     */
+    protected function createViews()
+    {
+        $this->addEditView('FacturaScripts\Core\Model\Agente', 'EditAgente', 'agent');
+        $this->addListView('FacturaScripts\Core\Model\PresupuestoCliente', 'EditAgentePresupuestos', 'Presupuestos');
+        $this->addListView('FacturaScripts\Core\Model\AlbaranCliente', 'EditAgenteAlbaranes', 'Albaranes');
+        $this->addListView('FacturaScripts\Core\Model\FacturaCliente', 'EditAgenteFacturas', 'Facturas', 'fa-book');
+        //$this->addEditListView('FacturaScripts\Core\Model\Subcuenta', 'EditEjercicioSubcuenta', 'subaccount');
+    }
+    
+     /**
+     * Load view data procedure
+     *
+     * @param string $keyView
+     * @param ExtendedController\EditView $view
+     */
+    protected function loadData($keyView, $view)
+    {
+        switch ($keyView) {
+            case 'EditAgente':
+                $value = $this->request->get('code');
+                $view->loadData($value);
+                break;
+
+            case 'EditAgentePresupuestos':
+                $where = [new DataBase\DataBaseWhere('codagente', $this->request->get('code'))];
+                $view->loadData($where);
+                break;
+
+            case 'EditAgenteAlbaranes':
+                $where = [new DataBase\DataBaseWhere('codagente', $this->request->get('code'))];
+                $view->loadData($where);
+                break;
+
+            case 'EditAgenteFacturas':
+                $where = [new DataBase\DataBaseWhere('codagente', $this->request->get('code'))];
+                $view->loadData($where);
+                break;
+
+            case 'EditEjercicioSubcuenta':
+                $where = [new DataBase\DataBaseWhere('codejercicio', $this->request->get('code'))];
+                $view->loadData($where);
+                break;
+        }
+    }
+
 
     /**
      * Returns the text for the data panel footer
