645f7b6bd5c2d98e4521c830f2c0ae6dc5cb0aa7
Raul Nazca <comercial@nazcanetworks.com>
2017-12-24 17:45:31 +0100

    Generar BalanceAmmounts.
    Creo que habrá que ampliar esta clase para que permita elegir el nivel de cuentas (1,2,3,4...) para compbrobaciones de otros balances.

--- Core/Lib/Accounting/BalanceAmmounts.php
+++ Core/Lib/Accounting/BalanceAmmounts.php
@@ -16,28 +16,39 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 namespace FacturaScripts\Core\Lib\Accounting;
+
+use FacturaScripts\Core\Base\DataBase;
 
 /**
  * Description of BalanceAmmounts
  *
  * @author carlos
+ * @author nazca <comercial@nazcanetworks.com>
  */
 class BalanceAmmounts
 {
-    
+
+    /**
+     * Generate the balance ammounts between two dates.
+     * 
+     * @param date $dateFrom
+     * @param date $dateTo
+     * @return array
+     */
     public function generate($dateFrom, $dateTo)
     {
+        //SELECT subcta.codsubcuenta,subcta.descripcion,sum(partida.debe) SDebe,sum(partida.haber) SHaber,sum(partida.debe)-sum(partida.haber) saldo FROM `co_subcuentas` subcta,co_partidas partida,co_asientos asiento where subcta.codsubcuenta = partida.codsubcuenta and asiento.idasiento=partida.idasiento group by subcta.codsubcuenta,subcta.descripcion and asiento.fecha>='2017-01-01' and asiento.fecha<='2017-12-31'
         /// TODO
-        return [
-            [
-                'cuenta' => '',
-                'descripcion' => '',
-                'debe' => '',
-                'haber' => '',
-                'saldo' => ''
-            ]
-        ];
+        $sql = 'SELECT subcta.codsubcuenta,subcta.descripcion,sum(partida.debe) SDebe,sum(partida.haber) SHaber,sum(partida.debe)-sum(partida.haber) saldo ' .
+            ' FROM `co_subcuentas` subcta,co_partidas partida,co_asientos asiento ' .
+            'where subcta.codsubcuenta = partida.codsubcuenta and asiento.idasiento=partida.idasiento ' .
+            ' and asiento.fecha>="' . date('Y-m-d', strtotime($dateFrom)) . '" and asiento.fecha<="' . date('Y-m-d', strtotime($dateTo)) . '"' .
+            'group by subcta.codsubcuenta,subcta.descripcion ';
+
+        $datb = new Database();
+        $resultados = $datb->select($sql);
+
+        return $resultados;
     }
 }
