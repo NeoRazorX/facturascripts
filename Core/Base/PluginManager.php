cf2db32ca17eb3ac5d0d0c84db61c34df80a36ad
Zapasoft <Zapasoft@192.168.0.160>
2018-01-15 23:32:48 +0100

    Fix Issues Codacy

--- Core/Base/PluginManager.php
+++ Core/Base/PluginManager.php
@@ -403,7 +403,7 @@ class PluginManager
     {
         $zipFile = new ZipArchive();
         $zip_status = $zipFile->open($filePath, ZipArchive::CHECKCONS);
-        if ($zip_status !== TRUE) {
+        if ($zip_status !== true) {
             return $zip_status;
         }
         $zipFile->close();
@@ -421,7 +421,7 @@ class PluginManager
     {
         $zipFile = new ZipArchive();
         $result = $zipFile->open($filePath, ZipArchive::CHECKCONS);
-        if ($result === TRUE) {
+        if ($result === true) {
             $pathINI = $zipFile->getNameIndex($zipFile->locateName('facturascripts.ini', ZipArchive::FL_NOCASE | ZipArchive::FL_NODIR));
             $folderPluginZip = explode('/', $pathINI);
             $pluginName = $this->getValuePluginINI($filePath, 'name');
@@ -456,7 +456,7 @@ class PluginManager
     {
         $zipFile = new ZipArchive();
         $result = $zipFile->open($pluginUnzipped, ZipArchive::CHECKCONS);
-        if ($result === TRUE) {
+        if ($result === true) {
             $fsIni = $zipFile->getFromIndex($zipFile->locateName('facturascripts.ini', ZipArchive::FL_NOCASE | ZipArchive::FL_NODIR));
             $zipFile->close();
             if ($fsIni) {
--- Core/Controller/AdminHome.php
+++ Core/Controller/AdminHome.php
@@ -243,7 +243,7 @@ class AdminHome extends Base\Controller
                     $this->miniLog->info($this->i18n->trans('plugin-installed', ['%pluginName%' => $result]));
                     $this->enablePlugin($result);
                 } else {
-                    $this->miniLog->error($this->i18n->trans('plugin-not-installed: ' . $result));
+                    $this->miniLog->error($this->i18n->trans('plugin-not-installed'));
                 }
                 unlink($uploadFile->getPathname());
             } else {
