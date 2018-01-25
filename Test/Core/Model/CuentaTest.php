b305b25f3e9b282e18692d6683f1f1708ceca4d1
Francesc Pineda Segarra <shawe.ewahs@gmail.com>
2018-01-26 00:09:34 +0100

    Initial test version for rest of models (mainly the basic skeleton).
    Requires more work, but now is more direct to distribute all work between contributors.
    
    NOTE: To test with PHP 7.2 at this moment is required in composer upgrade "parsecsv/php-parsecsv" to "dev-master" or enable on travis allow_failures for PHP 7.2.

---
+++ Test/Core/Model/CuentaTest.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * This file is part of FacturaScripts
+ * Copyright (C) 2018  Francesc Pineda Segarra <francesc.pineda.segarra@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace FacturaScripts\Test\Core\Model;
+
+use FacturaScripts\Core\Base\DataBase;
+use FacturaScripts\Core\Model\Cuenta;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Cuenta
+ */
+final class CuentaTest extends TestCase
+{
+    public function testNewCuenta()
+    {
+        $model = new Cuenta();
+
+        $this->assertInstanceOf(Cuenta::class, $model);
+        $this->assertEquals('', $model->descripcion);
+        $this->assertFalse($model->test());
+
+        $model->descripcion = 'Test description';
+        $model->codcuenta = 'CODC1';
+
+        $this->assertTrue($model->test());
+    }
+
+    public function testTable()
+    {
+        $model = new Cuenta();
+
+        $this->assertInternalType('string', $model::tableName());
+    }
+
+    public function testPrimaryColumn()
+    {
+        $model = new Cuenta();
+
+        $this->assertInternalType('string', $model::primaryColumn());
+    }
+
+    public function testInstall()
+    {
+        $model = new Cuenta();
+
+        $this->assertInternalType('string', $model->install());
+    }
+
+    public function testSave()
+    {
+        $dataBase = new DataBase();
+
+        $this->assertEquals(true, $dataBase->connect());
+
+        $model = new Cuenta();
+        $sql = $model->install();
+
+        if ($sql !== '') {
+            $result = $dataBase->exec($sql);
+            $this->assertFalse($result);
+        }
+    }
+
+    public function testAll()
+    {
+        $model = new Cuenta();
+        $list = $model->all();
+
+        if (!empty($list)) {
+            $this->assertInternalType('array', $list);
+        } else {
+            $this->assertSame([], $list);
+        }
+    }
+}
---
+++ Test/Core/Model/SubcuentaTest.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * This file is part of FacturaScripts
+ * Copyright (C) 2018  Francesc Pineda Segarra <francesc.pineda.segarra@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace FacturaScripts\Test\Core\Model;
+
+use FacturaScripts\Core\App\AppSettings;
+use FacturaScripts\Core\Base\DataBase;
+use FacturaScripts\Core\Model\Subcuenta;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Subcuenta
+ */
+final class SubcuentaTest extends TestCase
+{
+    public function testNewSubcuenta()
+    {
+        $model = new Subcuenta();
+
+        $this->assertInstanceOf(Subcuenta::class, $model);
+        $this->assertEquals(AppSettings::get('default', 'coddivisa'), $model->coddivisa);
+        $this->assertEquals('', $model->descripcion);
+        $this->assertEquals(0.0, $model->debe);
+        $this->assertEquals(0.0, $model->haber);
+        $this->assertEquals(0.0, $model->saldo);
+        $this->assertEquals(0.0, $model->recargo);
+        $this->assertEquals(0.0, $model->iva);
+        $this->assertFalse($model->test());
+
+        $model->nombre = 'Test name';
+        $model->codalmacen = 'ALG';
+    }
+
+    public function testTable()
+    {
+        $model = new Subcuenta();
+
+        $this->assertInternalType('string', $model::tableName());
+    }
+
+    public function testPrimaryColumn()
+    {
+        $model = new Subcuenta();
+
+        $this->assertInternalType('string', $model::primaryColumn());
+    }
+
+    public function testInstall()
+    {
+        $model = new Subcuenta();
+
+        $this->assertInternalType('string', $model->install());
+    }
+
+    public function testSave()
+    {
+        $dataBase = new DataBase();
+
+        $this->assertEquals(true, $dataBase->connect());
+
+        $model = new Subcuenta();
+        $sql = $model->install();
+
+        if ($sql !== '') {
+            $result = $dataBase->exec($sql);
+            $this->assertFalse($result);
+        }
+    }
+
+    public function testAll()
+    {
+        $model = new Subcuenta();
+        $list = $model->all();
+
+        if (!empty($list)) {
+            $this->assertInternalType('array', $list);
+        } else {
+            $this->assertSame([], $list);
+        }
+    }
+}
