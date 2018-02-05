96ca36458eaf0972a1b26b729cc835e09050b5f4
Ángel Guzmán Maeso <shakaran@gmail.com>
2018-02-06 00:29:10 +0100

    <feature> Add sensio generator bundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle::class => ['all' => true],
 ];
