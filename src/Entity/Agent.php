cc26bce68ec4368fc87170f34d238a9f379d6dfa
Ángel Guzmán Maeso <shakaran@gmail.com>
2018-02-06 00:50:51 +0100

    <feature> Generate Agent entity config

---
+++ src/Entity/Agent.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace FacturaScripts\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AgentRepository")
+ */
+class Agent
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+}
