fe64848ae702a9e10c5d9dd087a3e598a2457732
Yopli2k <yopli2000@gmail.com>
2018-02-24 11:53:24 +0100

    Created new view and Grid controller
    A new type of view and Twig visual controller have been created for the representation of data type Master/Detail where the detail is edited using the Handsontable component.

--- Core/Assets/JS/accountingEntry.js
+++ Core/Assets/JS/accountingEntry.js
@@ -16,40 +16,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-var documentLineData = [];
 var documentUrl = "";
-var accountEntries = null;
 var accountGraph = null;
-var calculateAccount = null;
-
-function configureAutocompleteColumns(columns) {
-    Object.keys(columns).forEach(function (key) {
-        if (columns[key]['type'] === 'autocomplete') {
-            var data = columns[key]['data-source'];
-            columns[key]['source'] = function (query, process) {
-                query = query.split(' - ', 1)[0];
-                $.ajax({
-                    url: data.url,
-                    dataType: 'json',
-                    data: {
-                        term: query,
-                        action: 'autocomplete',
-                        source: data.source,
-                        field: data.field,
-                        title: data.title
-                    },
-                    success: function (response) {
-                        process(response);
-                    }
-                });
-            };
-            delete columns[key]['data-source'];
-        }
-    });
-}
 
 function loadAccountData(account) {
-    if (account === null) {
+    if (account === null || account === '') {
         clearAccountData();
         return;
     }
@@ -67,7 +38,6 @@ function setAccountData(data) {
     // Update data labels
     $('#account-description').text(data.description);
     $('#account-balance').text(data.balance);
-    calculateAccount = data.subaccount;
 
     // Update graphic bars
     accountGraph.data.datasets.forEach((dataset) => {
@@ -80,7 +50,6 @@ function setAccountData(data) {
 function clearAccountData() {
     $('#account-description').text('');
     $('#account-balance').text('');
-    calculateAccount = null;
 
     // Update graphic bars
     accountGraph.data.datasets.forEach((dataset) => {
@@ -90,66 +59,10 @@ function clearAccountData() {
     accountGraph.update();
 }
 
-function getGridFieldData(row, fieldName) {
-    return documentLineData['rows'][row][fieldName];
-}
-
-function getGridColumnName(col) {
-    return documentLineData['columns'][col]['data'];
-}
-
-function afterSelection(row1, col1, row2, col2, preventScrolling) {
-    // Check if editing
-    var editor = accountEntries.getActiveEditor();
-    if (editor && editor.isOpened()) {
-        return;
-    }
-
-    // Not multiselection
-    if (col1 === col2 && row1 === row2) {
-        var newAccount = getGridFieldData(row1, 'codsubcuenta');
-        if (newAccount !== calculateAccount) {
-            loadAccountData(newAccount);
-        }
-    }
-    return;
-}
-
-function beforeChange(changes, source) {
-    if (changes !== null) {
-        if (changes[0][1] === 'codsubcuenta') {
-            if (changes[0][2] !== changes[0][3]) {
-                var value = changes[0][3];
-                value = value.split(' - ', 1)[0];
-                changes[0][3] = value;
-                loadAccountData(value);
-            }
-        }
-    }
-}
-
 $(document).ready(function () {
-    // Grid Data
-    var container = document.getElementById("document-lines");
-    if (container) {
-        accountEntries = new Handsontable(container, {
-            data: documentLineData.rows,
-            columns: documentLineData.columns,
-            rowHeaders: true,
-            colHeaders: documentLineData.headers,
-            stretchH: "all",
-            autoWrapRow: false,
-            manualRowResize: true,
-            manualColumnResize: true,
-            manualRowMove: true,
-            manualColumnMove: true,
-            minSpareRows: 1,
-            minRows: 6
-        });
-
-        Handsontable.hooks.add('afterSelection', afterSelection);
-        Handsontable.hooks.add('beforeChange', beforeChange);
-    }
+    // Add control events to Grid Controller
+    controlledColumns['codsubcuenta'].beforeChange = loadAccountData;
+    controlledColumns['codsubcuenta'].afterSelection = loadAccountData;
 
     // Graphic bars
     var ctx = document.getElementById('detail-balance');
