3573d87f25b4e429dc145039d5b40f3c61424ea6
Fco. Antonio Moreno Pérez <famphuelva@gmail.com>
2018-03-01 19:33:08 +0100

    Añadir botón de eliminar en EditPageOption. En caso de que haya datos guardados para ese usuario, poder eliminarlos y usar la configuración por defecto. En esta ocación subo fichero de idioma en ingles para realizar traducion por la web.

--- Core/Controller/EditPageOption.php
+++ Core/Controller/EditPageOption.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of FacturaScripts
  * Copyright (C) 2017-2018  Carlos Garcia Gomez  <carlos@facturascripts.com>
@@ -16,36 +17,43 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 namespace FacturaScripts\Core\Controller;
+
 use FacturaScripts\Core\Base;
 use FacturaScripts\Core\Model;
 use Symfony\Component\HttpFoundation\Response;
+
 /**
  * Edit option for any page.
  *
  * @author Carlos García Gómez <carlos@facturascripts.com>
  * @author Artex Trading sa <jcuello@artextrading.com>
+ * @author Fco. Antonio Moreno Pérez <famphuelva@gmail.com>
  */
-class EditPageOption extends Base\Controller
-{
+class EditPageOption extends Base\Controller {
+
     /**
      * Selected user, for which the controller columns are created or modified
      *
      * @var string
      */
     public $selectedUser;
+
     /**
      * Selected view, for which columns are created or modified
      *
      * @var string
      */
     public $selectedViewName;
+
     /**
      * Details of the view configuration
      *
      * @var Model\PageOption
      */
     public $model;
+
     /**
      * Initialize all objects and properties.
      *
@@ -54,20 +62,20 @@ class EditPageOption extends Base\Controller
      * @param MiniLog    $miniLog
      * @param string     $className
      */
-    public function __construct(&$cache, &$i18n, &$miniLog, $className)
-    {
+    public function __construct(&$cache, &$i18n, &$miniLog, $className) {
         parent::__construct($cache, $i18n, $miniLog, $className);
         $this->setTemplate('EditPageOption');
         $this->model = new Model\PageOption();
     }
+
     /**
      * Load and initialize the parameters sent by the form
      */
-    private function getParams()
-    {
+    private function getParams() {
         $this->selectedViewName = $this->request->get('code');
         $this->selectedUser = $this->user->admin ? $this->request->get('nick', null) : $this->user->nick;
     }
+
     /**
      * Runs the controller's private logic.
      *
@@ -75,35 +83,42 @@ class EditPageOption extends Base\Controller
      * @param Model\User                 $user
      * @param Base\ControllerPermissions $permissions
      */
-    public function privateCore(&$response, $user, $permissions)
-    {
+    public function privateCore(&$response, $user, $permissions) {
         parent::privateCore($response, $user, $permissions);
+
         $this->getParams();
         $this->model->getForUser($this->selectedViewName, $this->selectedUser);
-        if ($this->request->get('action', '') === 'save') {
-            $this->saveData();
+        $get = $this->request->get('action', '');
+        switch ($get) {
+            case 'save':
+                $this->saveData();
+                break;
+            case 'delete':
+                $this->deleteData();
+                break;
         }
     }
+
     /**
      * Checking the value of the nick field.
      * It determines if we edit a configuration for all the users or one,
      * and if there is already configuration for the nick
      */
-    private function checkNickAndID()
-    {
+    private function checkNickAndID() {
         if ($this->model->nick != $this->selectedUser) {
             $this->model->id = null;
             $this->model->nick = empty($this->selectedUser) ? null : $this->selectedUser;
         }
+
         if ($this->model->nick === '') {
             $this->model->nick = null;
         }
     }
+
     /**
      * Save new configuration for view
      */
-    private function saveData()
-    {
+    private function saveData() {
         $this->checkNickAndID();
         $data = $this->request->request->all();
         foreach ($data as $key => $value) {
@@ -112,54 +127,84 @@ class EditPageOption extends Base\Controller
                 $this->model->columns[$path[0]]->columns[$path[1]]->{$path[2]} = $value;
             }
         }
+
         if ($this->model->save()) {
             $this->miniLog->notice($this->i18n->trans('record-updated-correctly'));
+
             return;
         }
         $this->miniLog->alert($this->i18n->trans('data-save-error'));
     }
+
+    /**
+     * Delete configuration for view
+     */
+    private function deleteData() {
+        $nick = $this->request->get('nick');
+        if (!$nick) {
+            $where = [
+                new Base\DataBase\DataBaseWhere('nick', 'null', 'IS'),
+                new Base\DataBase\DataBaseWhere('name', $this->selectedViewName)
+            ];
+        } else {
+            $where = [
+                new Base\DataBase\DataBaseWhere('nick', $nick),
+                new Base\DataBase\DataBaseWhere('name', $this->selectedViewName)
+            ];
+        }
+
+        $id_all = $this->model->all($where, [], 0, 0);
+
+        if ($id_all[0] && $id_all[0]->delete()) {
+            $this->miniLog->notice($this->i18n->trans('record-deleted-correctly'));
+            $this->model->getForUser($this->selectedViewName, $this->selectedUser);
+        } else {
+            $this->miniLog->alert($this->i18n->trans('default-not-deletable'));
+        }
+    }
+
     /**
      * Returns basic page attributes
      *
      * @return array
      */
-    public function getPageData()
-    {
+    public function getPageData() {
         $pagedata = parent::getPageData();
         $pagedata['title'] = 'page-configuration';
         $pagedata['menu'] = 'admin';
         $pagedata['icon'] = 'fa-wrench';
         $pagedata['showonmenu'] = false;
+
         return $pagedata;
     }
+
     /**
      * Returns the text for the data main panel header
      *
      * @return string
      */
-    public function getPanelHeader()
-    {
+    public function getPanelHeader() {
         return $this->i18n->trans('configure-columns');
     }
+
     /**
      * Returns the text for the data main panel footer
      *
      * @return string
      */
-    public function getPanelFooter()
-    {
+    public function getPanelFooter() {
         return '<strong>'
-            . $this->i18n->trans('page') . ':&nbsp;' . $this->selectedViewName . '<br>'
-            . $this->i18n->trans('user') . ':&nbsp;' . $this->selectedUser
-            . '</strong>';
+                . $this->i18n->trans('page') . ':&nbsp;' . $this->selectedViewName . '<br>'
+                . $this->i18n->trans('user') . ':&nbsp;' . $this->selectedUser
+                . '</strong>';
     }
+
     /**
      * Get the list of users, excluding the user admin
      *
      * @return Array
      */
-    public function getUserList()
-    {
+    public function getUserList() {
         $result = [];
         $users = Model\CodeModel::all(Model\User::tableName(), 'nick', 'nick', false);
         foreach ($users as $codeModel) {
@@ -167,6 +212,8 @@ class EditPageOption extends Base\Controller
                 $result[$codeModel->code] = $codeModel->description;
             }
         }
+
         return $result;
     }
+
 }
