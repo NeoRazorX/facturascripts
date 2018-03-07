f791dd40b24b04a595454f4f9f73fc290184da30
NeoRazorX <neorazorx@gmail.com>
2018-03-07 20:19:04 +0100

    Fixed bugs from scrutinizer.

--- Core/Base/DataBase/DataBaseWhere.php
+++ Core/Base/DataBase/DataBaseWhere.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2013-2017  Carlos Garcia Gomez  <carlos@facturascripts.com>
+ * Copyright (C) 2013-2018  Carlos Garcia Gomez  <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -116,7 +116,7 @@ class DataBaseWhere
 
             case 'IS':
             case 'IS NOT':
-                $result = $this->value;
+                $result = (string) $this->value;
                 break;
 
             case 'IN':
