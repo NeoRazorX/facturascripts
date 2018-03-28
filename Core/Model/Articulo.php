e8ad3f90c0e5a869224975d9c9f92a136ec983d1
NeoRazorX <neorazorx@gmail.com>
2018-03-28 18:32:35 +0200

    Removed ArticuloCombinacion, ArticuloPropiedad and ArticuloTraza models. This models must be redesigned.

--- Core/Model/Articulo.php
+++ Core/Model/Articulo.php
@@ -18,7 +18,6 @@
  */
 namespace FacturaScripts\Core\Model;
 
-use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
 use FacturaScripts\Core\Base\Utils;
 
 /**
@@ -32,19 +31,11 @@ class Articulo extends Base\Product
     use Base\ModelTrait;
 
     /**
-     * Define the type of item, so you can set distinctions
-     * according to one type or another. Varchar (10)
+     * True => the articles are locked / obsolete.
      *
-     * @var string
+     * @var bool
      */
-    public $tipo;
-
-    /**
-     * Code of the family to which it belongs. In the family class.
-     *
-     * @var string
-     */
-    public $codfamilia;
+    public $bloqueado;
 
     /**
      * Code of the manufacturer to which it belongs. In the manufacturer class.
@@ -54,86 +45,11 @@ class Articulo extends Base\Product
     public $codfabricante;
 
     /**
-     * Price of the item, without taxes.
-     *
-     * @var float|int
-     */
-    public $pvp;
-
-    /**
-     * Stores the value of the pvp before making the change.
-     * This value is not stored in the database, that is,
-     * is not remembered.
-     *
-     * @var float|int
-     */
-    public $pvp_ant;
-
-    /**
-     * Average cost when buying the item. Calculated.
-     *
-     * @var float|int
-     */
-    public $costemedio;
-
-    /**
-     * Cost price manually edited
-     * It is not necessarily the purchase price, it can include
-     * also other costs.
-     *
-     * @var float|int
-     */
-    public $preciocoste;
-
-    /**
-     * True => the articles are locked / obsolete.
-     *
-     * @var bool
-     */
-    public $bloqueado;
-
-    /**
-     * True => the item is purchased.
-     *
-     * @var bool
-     */
-    public $secompra;
-
-    /**
-     * True => the item is sold.
-     *
-     * @var bool
-     */
-    public $sevende;
-
-    /**
-     * True -> will be synchronized with the online store.
-     *
-     * @var bool
-     */
-    public $publico;
-
-    /**
-     * Equivalence code. Varchar (18).
-     * Two or more articles are equivalent if they have the same equivalence code.
+     * Code of the family to which it belongs. In the family class.
      *
      * @var string
      */
-    public $equivalencia;
-
-    /**
-     * True -> allow sales without stock.
-     *
-     * @var bool
-     */
-    public $ventasinstock;
-
-    /**
-     * Observations of the article.
-     *
-     * @var string
-     */
-    public $observaciones;
+    public $codfamilia;
 
     /**
      * Sub-account code for purchases.
@@ -150,6 +66,73 @@ class Articulo extends Base\Product
     public $codsubcuentairpfcom;
 
     /**
+     * Average cost when buying the item. Calculated.
+     *
+     * @var float|int
+     */
+    public $costemedio;
+
+    /**
+     * Equivalence code. Varchar (18).
+     * Two or more articles are equivalent if they have the same equivalence code.
+     *
+     * @var string
+     */
+    public $equivalencia;
+
+    /**
+     * Observations of the article.
+     *
+     * @var string
+     */
+    public $observaciones;
+
+    /**
+     * Cost price manually edited
+     * It is not necessarily the purchase price, it can include
+     * also other costs.
+     *
+     * @var float|int
+     */
+    public $preciocoste;
+
+    /**
+     * True -> will be synchronized with the online store.
+     *
+     * @var bool
+     */
+    public $publico;
+
+    /**
+     * Price of the item, without taxes.
+     *
+     * @var float|int
+     */
+    public $pvp;
+
+    /**
+     * True => the item is purchased.
+     *
+     * @var bool
+     */
+    public $secompra;
+
+    /**
+     * True => the item is sold.
+     *
+     * @var bool
+     */
+    public $sevende;
+
+    /**
+     * Defines the type of item, so you can set distinctions
+     * according to one type or another. Varchar (10)
+     *
+     * @var string
+     */
+    public $tipo;
+
+    /**
      * Traceability control.
      *
      * @var bool
@@ -157,23 +140,23 @@ class Articulo extends Base\Product
     public $trazabilidad;
 
     /**
-     * Returns the name of the table that uses this model.
+     * True -> allow sales without stock.
      *
-     * @return string
+     * @var bool
      */
-    public static function tableName()
-    {
-        return 'articulos';
-    }
+    public $ventasinstock;
 
     /**
-     * Returns the name of the column that is the model's primary key.
-     *
-     * @return string
+     * Reset the values of all model properties.
      */
-    public static function primaryColumn()
+    public function clear()
     {
-        return 'referencia';
+        parent::clear();
+        $this->costemedio = 0.0;
+        $this->preciocoste = 0.0;
+        $this->pvp = 0.0;
+        $this->secompra = true;
+        $this->sevende = true;
     }
 
     /**
@@ -196,205 +179,23 @@ class Articulo extends Base\Product
     }
 
     /**
-     * Reset the values of all model properties.
+     * Returns the name of the column that is the model's primary key.
+     *
+     * @return string
      */
-    public function clear()
+    public static function primaryColumn()
     {
-        parent::clear();
-        $this->costemedio = 0.0;
-        $this->preciocoste = 0.0;
-        $this->pvp = 0.0;
-        $this->secompra = true;
-        $this->sevende = true;
+        return 'referencia';
     }
 
     /**
-     * Returns the stock of the item.
+     * Returns the name of the table that uses this model.
      *
-     * @return Stock[]
+     * @return string
      */
-    public function getStock()
+    public static function tableName()
     {
-        $stock = new Stock();
-
-        return $this->nostock ? [] : $stock->all([new DataBaseWhere('referencia', $this->referencia)]);
-    }
-
-    /**
-     * Sets the retail price.
-     *
-     * @param float $pvp
-     */
-    public function setPvp($pvp)
-    {
-        $pvp = round($pvp, FS_NF0 + 2);
-
-        if (!Utils::floatcmp($this->pvp, $pvp, FS_NF0 + 2)) {
-            $this->pvp_ant = $this->pvp;
-            $this->pvp = $pvp;
-        }
-    }
-
-    /**
-     * Sets the retail price with VAT.
-     *
-     * @param float $pvp
-     */
-    public function setPvpIva($pvp)
-    {
-        $this->setPvp((100 * $pvp) / (100 + $this->getIva()));
-    }
-
-    /**
-     * Modifies the stock of the item in a specific warehouse.
-     * Already responsible for executing save() if necessary.
-     *
-     * @param string $codalmacen
-     * @param int    $cantidad
-     *
-     * @return bool
-     */
-    public function setStock($codalmacen, $cantidad = 1)
-    {
-        if ($this->nostock) {
-            return true;
-        }
-
-        $result = false;
-        $stock = new Stock();
-        $encontrado = false;
-        foreach ($stock->all([new DataBaseWhere('referencia', $this->referencia)]) as $sto) {
-            if ($sto->codalmacen === $codalmacen) {
-                $sto->setCantidad($cantidad);
-                $result = $sto->save();
-                $encontrado = true;
-                break;
-            }
-        }
-
-        if (!$encontrado) {
-            $stock->referencia = $this->referencia;
-            $stock->codalmacen = $codalmacen;
-            $stock->setCantidad($cantidad);
-            $result = $stock->save();
-        }
-
-        if (!$result) {
-            self::$miniLog->alert(self::$i18n->trans('error-saving-stock'));
-            return false;
-        }
-
-        /// this code is highly optimized to save only the changes
-        $nuevoStock = $stock->totalFromArticulo($this->referencia);
-        if ($this->stockfis !== $nuevoStock) {
-            $this->stockfis = $nuevoStock;
-
-            if ($this->exists()) {
-                $sql = 'UPDATE ' . static::tableName()
-                    . ' SET stockfis = ' . self::$dataBase->var2str($this->stockfis)
-                    . ' WHERE referencia = ' . self::$dataBase->var2str($this->referencia) . ';';
-                return self::$dataBase->exec($sql);
-            }
-
-            if (!$this->save()) {
-                self::$miniLog->alert(self::$i18n->trans('error-updating-product-stock'));
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * Add the specified amount to the stock of the item in the specified store.
-     * Already responsible for executing save() if necessary.
-     *
-     * @param string $codalmacen
-     * @param int    $cantidad
-     * @param bool   $recalculaCoste
-     * @param string $codcombinacion
-     *
-     * @return bool
-     */
-    public function sumStock($codalmacen, $cantidad = 1, $recalculaCoste = false, $codcombinacion = null)
-    {
-        if ($recalculaCoste) {
-            // TODO: Uncomplete condition
-            $this->costemedio = 1;
-        }
-
-        if ($this->nostock) {
-            if ($recalculaCoste) {
-                /// this code is highly optimized to save only the changes
-                if ($this->exists()) {
-                    $sql = 'UPDATE ' . static::tableName()
-                        . '  SET costemedio = ' . self::$dataBase->var2str($this->costemedio)
-                        . '  WHERE referencia = ' . self::$dataBase->var2str($this->referencia) . ';';
-                    return self::$dataBase->exec($sql);
-                }
-
-                if (!$this->save()) {
-                    self::$miniLog->alert(self::$i18n->trans('error-updating-product-stock'));
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        $result = false;
-        $stock = new Stock();
-        $encontrado = false;
-        foreach ($stock->all([new DataBaseWhere('referencia', $this->referencia)]) as $sto) {
-            if ($sto instanceof Stock && $sto->codalmacen === $codalmacen) {
-                $sto->sumCantidad($cantidad);
-                $result = $sto->save();
-                $encontrado = true;
-                break;
-            }
-        }
-
-        if (!$encontrado) {
-            $stock->referencia = $this->referencia;
-            $stock->codalmacen = $codalmacen;
-            $stock->setCantidad($cantidad);
-            $result = $stock->save();
-        }
-
-        if (!$result) {
-            self::$miniLog->alert(self::$i18n->trans('error-saving-stock'));
-            return false;
-        }
-
-        /// this code is highly optimized to save only the changes
-        $nuevoStock = $stock->totalFromArticulo($this->referencia);
-        if ($this->stockfis !== $nuevoStock) {
-            $this->stockfis = $nuevoStock;
-
-            if ($this->exists()) {
-                $sql = 'UPDATE ' . static::tableName()
-                    . '  SET stockfis = ' . self::$dataBase->var2str($this->stockfis)
-                    . ', costemedio = ' . self::$dataBase->var2str($this->costemedio)
-                    . '  WHERE referencia = ' . self::$dataBase->var2str($this->referencia) . ';';
-                $result = self::$dataBase->exec($sql);
-            } elseif (!$this->save()) {
-                self::$miniLog->alert(self::$i18n->trans('error-updating-product-stock'));
-                return false;
-            }
-
-            /// Any combination?
-            if ($codcombinacion !== null && $result) {
-                $com0 = new ArticuloCombinacion();
-                foreach ($com0->allFromCodigo($codcombinacion) as $combi) {
-                    if ($combi instanceof ArticuloCombinacion) {
-                        $combi->stockfis += $cantidad;
-                        $combi->save();
-                    }
-                }
-            }
-        }
-
-        return $result;
+        return 'articulos';
     }
 
     /**
@@ -418,6 +219,6 @@ class Articulo extends Base\Product
             $this->publico = false;
         }
 
-        return true;
+        return parent::test();
     }
 }
