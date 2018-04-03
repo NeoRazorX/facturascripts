b403603b8a68afa084aac805dfdaef2e1c7a696d
Francesc Pineda Segarra <francesc.pineda.segarra@gmail.com>
2018-04-03 20:50:23 +0200

    Trello task 312: - Added random generation of data for supplier estimations.

--- Core/Model/PresupuestoProveedor.php
+++ Core/Model/PresupuestoProveedor.php
@@ -60,7 +60,7 @@ class PresupuestoProveedor extends Base\PurchaseDocument
 
     /**
      * Returns a new line for this document.
-     * 
+     *
      * @param array $data
      *
      * @return LineaPresupuestoProveedor
@@ -76,6 +76,13 @@ class PresupuestoProveedor extends Base\PurchaseDocument
         return $newLine;
     }
 
+    /**
+     * This function is called when creating the model table. Returns the SQL
+     * that will be executed after the creation of the table. Useful to insert values
+     * default.
+     *
+     * @return string
+     */
     public function install()
     {
         parent::install();
