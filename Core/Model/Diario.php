acb07d64b2d931b726827965fd0171e3e15e0119
Raúl Jiménez <raul.jimenez@nazcanetworks.com>
2018-05-19 20:52:23 +0200

    Create module of accounting books.
    Create table, model, views for accounting books.
    accounting notes will be classified in accounting books

--- Core/Model/Diario.php
+++ Core/Model/Diario.php
@@ -15,7 +15,6 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
-namespace FacturaScripts\Core\Model;
 
 */
 namespace FacturaScripts\Core\Model;
