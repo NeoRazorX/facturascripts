c7082f57da438faa7b42236fc75e6cd21d119093
Cristo Estévez <cristom.estevez@gmail.com>
2018-05-20 11:22:26 +0100

    Create model of Retenciones table
    Foul made the function test to check if all word fine

---
+++ Core/Model/Retenciones.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * This file is part of FacturaScripts
+ * Copyright (C) 2015-2018  Carlos Garcia Gomez  <carlos@facturascripts.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace FacturaScripts\Core\Model;
+
+/**
+ * Class to manage the data of retenciones table
+ *
+ * @author Cristo M. Estévez Hernández <cristom.estevez@gmail.com>
+ */
+class Retenciones extends Base\ModelClass
+{
+    use Base\ModelTrait;
+
+    /**
+     * Primary key. varchar(10).
+     *
+     * @var string
+     */
+    public $codretencion;
+
+    /**
+     * Description of the tax.
+     *
+     * @var string
+     */
+    public $descripcion;
+
+    /**
+     * Percent of the retention
+     *
+     * @var int
+     */
+    public $porcentaje;
+
+    /**
+     * Returns the name of the column that is the primary key of the model.
+     *
+     * @return string
+     */
+    public static function primaryColumn()
+    {
+        return 'codretencion';
+    }
+
+    /**
+     * Returns the name of the table that uses this model.
+     *
+     * @return string
+     */
+    public static function tableName()
+    {
+        return 'retenciones';
+    }
+
+    /**
+     * Reset the values of all model properties.
+     */
+    public function clear()
+    {
+        parent::clear();
+        $this->descripcion = '';
+        $this->porcentaje = 0;
+    }
+
+    //TODO make function test 
+}
