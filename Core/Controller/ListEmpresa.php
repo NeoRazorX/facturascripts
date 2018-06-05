1bd06932e002b19c986f862f4d9c7a45b68cefc5
NeoRazorX <neorazorx@gmail.com>
2018-06-06 00:35:15 +0200

    Fixed bug search in ListEmpresa controller.

--- Core/Controller/ListEmpresa.php
+++ Core/Controller/ListEmpresa.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2018  Carlos Garcia Gomez  <carlos@facturascripts.com>
+ * Copyright (C) 2017-2018 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -50,9 +50,8 @@ class ListEmpresa extends ExtendedController\ListController
     protected function createViews()
     {
         $this->addView('ListEmpresa', 'Empresa');
-        $this->addSearchFields('ListEmpresa', ['nombre', 'nombrecorto', 'CAST(idempresa AS VARCHAR)']);
-
-        $this->addOrderBy('ListEmpresa', 'idempresa', 'code');
-        $this->addOrderBy('ListEmpresa', 'nombre', 'name');
+        $this->addSearchFields('ListEmpresa', ['nombre', 'nombrecorto']);
+        $this->addOrderBy('ListEmpresa', ['idempresa'], 'code');
+        $this->addOrderBy('ListEmpresa', ['nombre'], 'name');
     }
 }
