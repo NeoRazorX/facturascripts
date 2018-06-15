6514164c3627eb3b95870811dec2751052da42c5
NeoRazorX <neorazorx@gmail.com>
2018-06-15 19:57:28 +0200

    - Added expire parameter to Cache class and cache engines. - Improved cache engines.

--- Core/Base/Cache/FileCache.php
+++ Core/Base/Cache/FileCache.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2013-2017  Carlos Garcia Gomez  <carlos@facturascripts.com>
+ * Copyright (C) 2013-2018 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 namespace FacturaScripts\Core\Base\Cache;
 
 use FacturaScripts\Core\Base\MiniLog;
@@ -36,6 +35,7 @@ use FacturaScripts\Core\Base\Translator;
  */
 class FileCache implements AdaptorInterface
 {
+
     /**
      * Cache configuration
      *
@@ -64,7 +64,7 @@ class FileCache implements AdaptorInterface
     {
         self::$config = [
             'cache_path' => FS_FOLDER . '/MyFiles/Cache/FileCache',
-            'expires' => 180,
+            'expires' => 3600,
         ];
 
         $this->i18n = new Translator();
@@ -79,64 +79,20 @@ class FileCache implements AdaptorInterface
     }
 
     /**
-     * Get a route to the file associated to that key.
+     * Flush all cache.
      *
-     * @param string $key
-     *
-     * @return string the filename of the php file
+     * @return bool always true
      */
-    private function getRoute($key)
+    public function clear()
     {
-        return self::$config['cache_path'] . '/' . md5($key) . '.php';
-    }
-
-    /**
-     * Get the data associated with a key.
-     *
-     * @param string   $key
-     * @param bool     $raw
-     * @param int|null $custom_time
-     *
-     * @return mixed the content you put in, or null if expired or not found
-     */
-    public function get($key, $raw = false, $custom_time = null)
-    {
-        $this->minilog->debug($this->i18n->trans('filecache-get-key-item', ['%item%' => $key]));
-        if (!$this->fileExpired($file = $this->getRoute($key), $custom_time)) {
-            $content = file_get_contents($file);
-            /**
-             * Perhaps it's possible to exploit the unserialize via: file_get_contents(...).
-             * Documentation can be found here:
-             * https://github.com/kalessil/phpinspectionsea/blob/master/docs/security.md#exploiting-unserialize
-             */
-            return $raw ? $content : unserialize($content);
+        $this->minilog->debug($this->i18n->trans('filecache-clear'));
+        foreach (scandir(self::$config['cache_path'], SCANDIR_SORT_ASCENDING) as $fileName) {
+            if (substr($fileName, -4) === '.php') {
+                unlink(self::$config['cache_path'] . '/' . $fileName);
+            }
         }
 
-        return null;
-    }
-
-    /**
-     * Put content into the cache.
-     *
-     * @param string $key
-     * @param mixed  $content the the content you want to store
-     * @param bool   $raw     whether if you want to store raw data or not. If it is true, $content *must* be a string
-     *
-     * @return bool whether if the operation was successful or not
-     */
-    public function set($key, $content, $raw = false)
-    {
-        $this->minilog->debug($this->i18n->trans('filecache-set-key-item', ['%item%' => $key]));
-        $dest_file_name = $this->getRoute($key);
-        /** Use a unique temporary filename to make writes atomic with rewrite */
-        $temp_file_name = str_replace('.php', uniqid('-', true) . '.php', $dest_file_name);
-        $ret = @file_put_contents($temp_file_name, $raw ? $content : serialize($content));
-        if ($ret !== false) {
-            return @rename($temp_file_name, $dest_file_name);
-        }
-        @unlink($temp_file_name);
-
-        return false;
+        return true;
     }
 
     /**
@@ -158,34 +114,80 @@ class FileCache implements AdaptorInterface
     }
 
     /**
-     * Flush all cache.
+     * Get the data associated with a key.
      *
-     * @return bool always true
+     * @param string $key
+     *
+     * @return mixed the content you put in, or null if expired or not found
      */
-    public function clear()
+    public function get($key)
     {
-        $this->minilog->debug($this->i18n->trans('filecache-clear'));
-        foreach (scandir(self::$config['cache_path'], SCANDIR_SORT_ASCENDING) as $fileName) {
-            if (substr($fileName, -4) === '.php') {
-                unlink(self::$config['cache_path'] . '/' . $fileName);
-            }
+        $this->minilog->debug($this->i18n->trans('filecache-get-key-item', ['%item%' => $key]));
+        $file = $this->getRoute($key);
+        if (!$this->fileExpired($file)) {
+            $content = file_get_contents($file);
+            /**
+             * Perhaps it's possible to exploit the unserialize via: file_get_contents(...).
+             * Documentation can be found here:
+             * https://github.com/kalessil/phpinspectionsea/blob/master/docs/security.md#exploiting-unserialize
+             */
+            return unserialize($content);
         }
 
-        return true;
+        return null;
+    }
+
+    /**
+     * Put content into the cache.
+     *
+     * @param string $key
+     * @param mixed  $content the the content you want to store
+     * @param int    $expire  time to expire
+     *
+     * @return bool whether if the operation was successful or not
+     */
+    public function set($key, $content, $expire)
+    {
+        $this->minilog->debug($this->i18n->trans('filecache-set-key-item', ['%item%' => $key]));
+        if ($expire < self::$config['expires']) {
+            self::$config['expires'] = $expire;
+        }
+
+        $destFileName = $this->getRoute($key);
+        /** Use a unique temporary filename to make writes atomic with rewrite */
+        $tempFileName = str_replace('.php', uniqid('-', true) . '.php', $destFileName);
+        $ret = @file_put_contents($tempFileName, serialize($content));
+        if ($ret !== false) {
+            return @rename($tempFileName, $destFileName);
+        }
+
+        @unlink($tempFileName);
+        return false;
+    }
+
+    /**
+     * Get a route to the file associated to that key.
+     *
+     * @param string $key
+     *
+     * @return string the filename of the php file
+     */
+    private function getRoute($key)
+    {
+        return self::$config['cache_path'] . '/' . md5($key) . '.php';
     }
 
     /**
      * Check if a file has expired or not.
      *
      * @param string $file the rout to the file
-     * @param int    $time the number of minutes it was set to expire
      *
      * @return bool if the file has expired or not
      */
-    private function fileExpired($file, $time = null)
+    private function fileExpired($file)
     {
         if (file_exists($file)) {
-            return time() > (filemtime($file) + 60 * ($time ?: self::$config['expires']));
+            return time() > (filemtime($file) + self::$config['expires']);
         }
 
         return true;
