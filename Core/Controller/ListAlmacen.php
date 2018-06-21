898d4ec775dc507196e653d18e433c7764219fd2
Cristo Estévez <cristom.estevez@gmail.com>
2018-06-21 08:14:54 +0100

    Added filter select to empresas

--- Core/Controller/ListAlmacen.php
+++ Core/Controller/ListAlmacen.php
@@ -54,5 +54,8 @@ class ListAlmacen extends ExtendedController\ListController
 
         $this->addOrderBy('ListAlmacen', ['codalmacen'], 'code');
         $this->addOrderBy('ListAlmacen', ['nombre'], 'name');
+
+        $selectValues = $this->codeModel->all('empresas', 'idempresa', 'nombrecorto');
+        $this->addFilterSelect('ListAlmacen', 'idempresa', 'business', 'idempresa', $selectValues);
     }
 }
