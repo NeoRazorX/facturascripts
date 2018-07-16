2f05160a447b98ebb8426b587e9208fd2517343c
Adelantia <administracion@adelantia.com>
2018-07-16 16:20:02 +0200

    Modificación codiso para 5 caracteres

--- Core/Model/Divisa.php
+++ Core/Model/Divisa.php
@@ -32,7 +32,7 @@ class Divisa extends Base\ModelClass
     use Base\ModelTrait;
 
     /**
-     * Primary key. Varchar (5).
+     * Primary key. Varchar (3).
      *
      * @var string
      */
@@ -125,9 +125,9 @@ class Divisa extends Base\ModelClass
         $this->descripcion = Utils::noHtml($this->descripcion);
         $this->simbolo = Utils::noHtml($this->simbolo);
 
-        if (!preg_match('/^[A-Z0-9]{1,5}$/i', $this->coddivisa)) {
+        if (!preg_match('/^[A-Z0-9]{1,3}$/i', $this->coddivisa)) {
             self::$miniLog->alert(self::$i18n->trans('invalid-column-lenght', ['%column%' => 'coddivisa', '%min%' => '1', '%max%' => '3']));
-        } elseif ($this->codiso !== null && !preg_match('/^[A-Z0-9]{1,3}$/i', $this->codiso)) {
+        } elseif ($this->codiso !== null && !preg_match('/^[A-Z0-9]{1,5}$/i', $this->codiso)) {
             self::$miniLog->alert(self::$i18n->trans('invalid-column-lenght', ['%column%' => 'codiso', '%min%' => '1', '%max%' => '3']));
         } elseif ($this->tasaconv === 0.0 || $this->tasaconvcompra === 0.0) {
             self::$miniLog->alert(self::$i18n->trans('conversion-rate-not-0'));
