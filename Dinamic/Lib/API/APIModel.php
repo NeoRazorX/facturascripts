0b1e57bb904857ce378ee2a7844c0b649ded9e2b
NeoRazorX <neorazorx@gmail.com>
2018-07-18 17:20:33 +0200

    Updated Dynamic.

---
+++ Dinamic/Lib/API/APIModel.php
@@ -0,0 +1,10 @@
+<?php namespace FacturaScripts\Dinamic\Lib\API;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @package FacturaScripts\Dinamic\Lib\API
+ * @author Carlos García Gómez <carlos@facturascripts.com>
+ */
+class APIModel extends \FacturaScripts\Core\Lib\API\APIModel
+{
+}
---
+++ Dinamic/Model/LogMessage.php
@@ -0,0 +1,10 @@
+<?php namespace FacturaScripts\Dinamic\Model;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @package FacturaScripts\Dinamic\Model
+ * @author Carlos García Gómez <carlos@facturascripts.com>
+ */
+class LogMessage extends \FacturaScripts\Core\Model\LogMessage
+{
+}
