bdc2f6944466c1f7c8fee7f3f59fa7b917a8fd2b
Yopl2k <yopli2000@gmail.com>
2018-09-14 02:07:10 +0200

    Add Row Actions to EditView

--- Core/Lib/Widget/RowActions.php
+++ Core/Lib/Widget/RowActions.php
@@ -18,6 +18,8 @@
  */
 namespace FacturaScripts\Core\Lib\Widget;
 
+use FacturaScripts\Core\Base\Translator;
+
 /**
  * Description of RowActions
  *
@@ -25,5 +27,85 @@ namespace FacturaScripts\Core\Lib\Widget;
  */
 class RowActions
 {
-    //put your code here
+
+    /**
+     *
+     * @var array
+     */
+    protected $children;
+
+    /**
+     *
+     * @var Translator
+     */
+    protected static $i18n;
+
+    /**
+     *
+     * @param array $data
+     */
+    public function __construct($data)
+    {
+        if (!isset(static::$i18n)) {
+            static::$i18n = new Translator();
+        }
+
+        $this->children = $data['children'];
+    }
+
+    /**
+     *
+     * @param string $button
+     * @param string $viewName
+     * @param string $jsFunction
+     *
+     * @return string
+     */
+    protected function renderButton($button, $viewName, $jsFunction)
+    {
+        $color = isset($button['color']) ? $button['color'] : 'light';
+        $icon = isset($button['icon']) ? '<i class="fas ' . $button['icon'] . ' fa-fw"></i> ' : '';
+        $label = isset($button['label']) ? static::$i18n->trans($button['label']) : '';
+
+        if (!isset($button['type'])) {
+            return '';
+        }
+
+        if ($button['type'] === 'modal') {
+            return '<button type="button" class="btn btn-' . $color . '"data-toggle="modal" data-target="#modal'
+                . $button['action'] . '">' . $icon . $label . '</button>';
+        }
+
+        /// type action
+        if (empty($jsFunction)) {
+            return '<button type="submit" name="action" value="' . $button['action'] . '" class="btn btn-'
+                . $color . '">' . $icon . $label . '</button>';
+        }
+
+        return '<button type="button" class="btn btn-' . $color . '" onclick="' . $jsFunction
+            . '(\'' . $viewName . '\',\'' . $button['action'] . '\');">' . $icon . $label . '</button>';
+    }
+
+    /**
+     *
+     * @param string $viewName
+     * @param string $jsFunction
+     *
+     * @return string
+     */
+    public function render($viewName, $jsFunction = '')
+    {
+        $html = '';
+        foreach ($this->children as $child) {
+            if ($child['tag'] == 'button') {
+                $onclick = $child['onclick'] ?? $jsFunction;
+                $html .= $this->renderButton($child, $viewName, $onclick);
+            }
+        }
+
+        if (!empty($html)) {
+            $html = '<div class="col d-flex justify-content-center">' . $html . '</div>';
+        }
+        return $html;
+    }
 }
