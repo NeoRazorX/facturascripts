247a4375595f90b4f9515cf391ef3bf9daecef0e
NeoRazorX <neorazorx@gmail.com>
2018-09-21 12:18:48 +0200

    Updated Dynamic.

---
+++ Dinamic/Lib/Widget/WidgetAutocomplete.php
@@ -0,0 +1,10 @@
+<?php namespace FacturaScripts\Dinamic\Lib\Widget;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @package FacturaScripts\Dinamic\Lib\Widget
+ * @author Carlos García Gómez <carlos@facturascripts.com>
+ */
+class WidgetAutocomplete extends \FacturaScripts\Core\Lib\Widget\WidgetAutocomplete
+{
+}
---
+++ Dinamic/Lib/Widget/WidgetMoney.php
@@ -0,0 +1,10 @@
+<?php namespace FacturaScripts\Dinamic\Lib\Widget;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @package FacturaScripts\Dinamic\Lib\Widget
+ * @author Carlos García Gómez <carlos@facturascripts.com>
+ */
+class WidgetMoney extends \FacturaScripts\Core\Lib\Widget\WidgetMoney
+{
+}
