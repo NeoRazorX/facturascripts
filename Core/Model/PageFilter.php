d9deb18467d25359c9aef573664cd9e5376132da
Yopl2k <yopli2000@gmail.com>
2018-10-17 17:42:00 +0200

    Add PageFilter table and model

---
+++ Core/Model/PageFilter.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * This file is part of FacturaScripts
+ * Copyright (C) 2017-2018 Carlos Garcia Gomez <carlos@facturascripts.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace FacturaScripts\Core\Model;
+
+/**
+ * Visual filter configuration of the FacturaScripts views,
+ * each PageFilter corresponds to a view or tab filter.
+ *
+ * @author Artex Trading sa     <jcuello@artextrading.com>
+ */
+class PageFilter extends Base\ModelClass
+{
+
+    use Base\ModelTrait;
+
+    /**
+     * Human description
+     *
+     * @var string
+     */
+    public $description;
+
+    /**
+     * Definition of filters values
+     *
+     * @var array
+     */
+    public $filters;
+
+    /**
+     * Identifier
+     *
+     * @var int
+     */
+    public $id;
+
+    /**
+     * Name of the page (controller).
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     * User Identifier.
+     *
+     * @var string
+     */
+    public $nick;
+
+    /**
+     * This function is called when creating the model table.
+     * Returns the SQL that will be executed after the creation of the table,
+     * useful to insert default values.
+     *
+     * @return string
+     */
+    public function install()
+    {
+        new Page();
+        new User();
+
+        return parent::install();
+    }
+
+    /**
+     * Load the data from an array
+     *
+     * @param array $data
+     * @param array $exclude
+     */
+    public function loadFromData(array $data = [], array $exclude = [])
+    {
+        array_push($exclude, 'filters', 'code', 'action');
+        parent::loadFromData($data, $exclude);
+
+        $this->filters = json_decode($data['filters'], true);
+    }
+
+    /**
+     * Returns the name of the column that is the model's primary key.
+     *
+     * @return string
+     */
+    public static function primaryColumn()
+    {
+        return 'id';
+    }
+
+    /**
+     * Returns the name of the table that uses this model.
+     *
+     * @return string
+     */
+    public static function tableName()
+    {
+        return 'pages_filters';
+    }
+
+    /**
+     * Returns the values of the view configuration fields in JSON format
+     *
+     * @return array
+     */
+    private function getEncodeValues()
+    {
+        return [
+            'fields' => json_encode($this->fields)
+        ];
+    }
+
+    /**
+     * Insert the model data in the database.
+     *
+     * @param array $values
+     *
+     * @return bool
+     */
+    protected function saveInsert(array $values = [])
+    {
+        return parent::saveInsert($this->getEncodeValues());
+    }
+
+    /**
+     * Update the model data in the database.
+     *
+     * @param array $values
+     *
+     * @return bool
+     */
+    protected function saveUpdate(array $values = [])
+    {
+        return parent::saveUpdate($this->getEncodeValues());
+    }
+}
