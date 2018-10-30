4d636129f53fef697b92dad0008d864af3e98fc7
Yopl2k <yopli2000@gmail.com>
2018-10-31 00:18:00 +0100

    Add tabs to GridView

--- Core/Controller/EditAsiento.php
+++ Core/Controller/EditAsiento.php
@@ -19,10 +19,10 @@
 namespace FacturaScripts\Core\Controller;
 
 use Exception;
-use FacturaScripts\Core\Base\DivisaTools;
 use FacturaScripts\Core\Base\DataBase;
 use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
-use FacturaScripts\Core\Lib\ExtendedController;
+use FacturaScripts\Core\Base\DivisaTools;
+use FacturaScripts\Core\Lib\ExtendedController\EditController;
 use FacturaScripts\Core\Model;
 
 /**
@@ -31,38 +31,110 @@ use FacturaScripts\Core\Model;
  * @author Carlos García Gómez <carlos@facturascripts.com>
  * @author Artex Trading sa <jcuello@artextrading.com>
  */
-class EditAsiento extends ExtendedController\PanelController
+class EditAsiento extends EditController
 {
 
     /**
-     * Starts all the objects and properties.
-     *
-     * @param Base\Cache      $cache
-     * @param Base\Translator $i18n
-     * @param Base\MiniLog    $miniLog
-     * @param string          $className
-     * @param string          $uri
+     * Overwrite autocomplete function to macro concepts in accounting concept.
      */
-    public function __construct(&$cache, &$i18n, &$miniLog, $className, $uri = '')
+    protected function autocompleteAction(): array
     {
-        parent::__construct($cache, $i18n, $miniLog, $className, $uri);
-        $this->setTabsPosition('bottom');
+        if ($this->request->get('source', '') === 'conceptos_partidas') {
+            return $this->replaceConcept(parent::autocompleteAction());
+        }
+
+        return parent::autocompleteAction();
     }
 
     /**
-     * Returns basic page attributes
+     * Calculate unbalance and total imports
      *
-     * @return array
+     * @param array $data
+     * @param float $credit
+     * @param float $debit
      */
-    public function getPageData(): array
+    private function calculateAmounts(array &$data, float $credit, float $debit)
     {
-        $pagedata = parent::getPageData();
-        $pagedata['title'] = 'accounting-entries';
-        $pagedata['menu'] = 'accounting';
-        $pagedata['icon'] = 'fas fa-balance-scale';
-        $pagedata['showonmenu'] = false;
+        $unbalance = round(($credit - $debit), (int) FS_NF0);
+        $index = count($data['lines']) - 1;
+        $line = &$data['lines'][$index];
 
-        return $pagedata;
+        if (($line['debe'] + $line['haber']) === 0.00 && $index > 0) {
+            // if the sub-account is the same as the previous offsetting
+            if ($line['codsubcuenta'] === $data['lines'][$index - 1]['codcontrapartida']) {
+                $field = 'haber';
+                if ($unbalance < 0) {
+                    $field = 'debe';
+                }
+                $line[$field] = abs($unbalance);
+                $unbalance = 0.00;
+            }
+        }
+        $data['unbalance'] = $unbalance;
+        $data['total'] = ($credit > $debit) ? round($credit, (int) FS_NF0) : round($debit, (int) FS_NF0);
+    }
+
+    /**
+     * Auto complete data for new account line
+     *
+     * @param array $line
+     * @param array $previousLine
+     */
+    private function checkEmptyValues(array &$line, array $previousLine)
+    {
+        if (empty($line['concepto'])) {
+            $line['concepto'] = $previousLine['concepto'];
+        }
+
+        if (empty($line['codcontrapartida']) && !empty($line['codsubcuenta'])) {
+            // TODO: [Fix] Go through previous lines in search of the offsetting. The sub-account that uses the offsetting for the first time is needed
+            $line['codcontrapartida'] = ($line['codsubcuenta'] === $previousLine['codcontrapartida']) ? $previousLine['codsubcuenta'] : $previousLine['codcontrapartida'];
+        }
+    }
+
+    private function cloneDocument(&$data): string
+    {
+        // init document
+        $accounting = new Model\Asiento();
+        $accounting->loadFromData($data, ['action', 'activetab']);
+
+        // init line document
+        $entryModel = new Model\Partida();
+        $entries = $entryModel->all([new DataBaseWhere('idasiento', $data['idasiento'])]);
+
+        // start transaction
+        $dataBase = new DataBase();
+        $dataBase->beginTransaction();
+
+        // main save process
+        try {
+            $accounting->idasiento = null;
+            $accounting->fecha = date('d-m-Y');
+            $accounting->numero = $accounting->newCode('numero');
+            if (!$accounting->save()) {
+                throw new Exception(self::$i18n->trans('clone-document-error'));
+            }
+
+            foreach ($entries as $line) {
+                $line->idpartida = null;
+                $line->idasiento = $accounting->idasiento;
+                if (!$line->save()) {
+                    throw new Exception(self::$i18n->trans('clone-line-document-error'));
+                }
+            }
+            // confirm data
+            $dataBase->commit();
+            $result = $accounting->url('type') . '&action=save-ok';
+        } catch (Exception $e) {
+            self::$miniLog->alert($e->getMessage());
+            $result = '';
+        } finally {
+            if ($dataBase->inTransaction()) {
+                $dataBase->rollback();
+            }
+        }
+
+        return $result;
     }
 
     /**
@@ -74,6 +146,7 @@ class EditAsiento extends ExtendedController\PanelController
         $detail = ['name' => 'EditPartida', 'model' => 'Partida'];
         $this->addGridView($master, $detail, 'accounting-entry', 'fas fa-balance-scale');
         $this->views['EditAsiento']->template = 'EditAsiento.html.twig';
+        $this->setTabsPosition('bottom');
     }
 
     /**
@@ -163,39 +236,106 @@ class EditAsiento extends ExtendedController\PanelController
         return $result;
     }
 
-    private function calculateAmounts(array &$data, float $credit, float $debit)
+    /**
+     * Get VAT information for a sub-account
+     *
+     * @param string $exercise
+     * @param string $codeSubAccount
+     * @return array
+     */
+    private function getAccountVatID($exercise, $codeSubAccount): array
     {
-        $unbalance = round(($credit - $debit), (int) FS_NF0);
-        $index = count($data['lines']) - 1;
-        $line = &$data['lines'][$index];
+        $result = ['group' => '', 'code' => '', 'description' => '', 'id' => '', 'surcharge' => false];
+        if (empty($exercise) || empty($codeSubAccount)) {
+            return $result;
+        }
 
-        if (($line['debe'] + $line['haber']) === 0.00 && $index > 0) {
-            // if the sub-account is the same as the previous offsetting
-            if ($line['codsubcuenta'] === $data['lines'][$index - 1]['codcontrapartida']) {
-                $field = 'haber';
-                if ($unbalance < 0) {
-                    $field = 'debe';
-                }
-                $line[$field] = abs($unbalance);
-                $unbalance = 0.00;
+        $where = [
+            new DataBaseWhere('codsubcuenta', $codeSubAccount),
+            new DataBaseWhere('codejercicio', $exercise)
+        ];
+
+        $subAccount = new Model\Subcuenta();
+        if ($subAccount->loadFromCode(null, $where)) {
+            $result['group'] = $subAccount->getSpecialAccountCode();
+            switch ($result['group']) {
+                case 'CLIENT':
+                    $this->searchVatDataFromClient($codeSubAccount, $result);
+                    break;
+
+                case 'ACREED':
+                case 'PROVEE':
+                    $this->searchVatDataFromSupplier($codeSubAccount, $result);
+                    break;
             }
         }
-        $data['unbalance'] = $unbalance;
-        $data['total'] = ($credit > $debit) ? round($credit, (int) FS_NF0) : round($debit, (int) FS_NF0);
+        return $result;
     }
 
-    private function checkEmptyValues(array &$line, array $previousLine)
+    /**
+     * Returns basic page attributes
+     *
+     * @return array
+     */
+    public function getPageData(): array
     {
-        if (empty($line['concepto'])) {
-            $line['concepto'] = $previousLine['concepto'];
-        }
+        $pagedata = parent::getPageData();
+        $pagedata['title'] = 'accounting-entries';
+        $pagedata['menu'] = 'accounting';
+        $pagedata['icon'] = 'fas fa-balance-scale';
+        $pagedata['showonmenu'] = false;
 
-        if (empty($line['codcontrapartida']) && !empty($line['codsubcuenta'])) {
-            // TODO: [Fix] Go through previous lines in search of the offsetting. The sub-account that uses the offsetting for the first time is needed
-            $line['codcontrapartida'] = ($line['codsubcuenta'] === $previousLine['codcontrapartida']) ? $previousLine['codsubcuenta'] : $previousLine['codcontrapartida'];
-        }
+        return $pagedata;
     }
 
+    /**
+     * Calculate data document
+     *
+     * @param type $data
+     * @return array
+     */
+    protected function recalculateDocument(&$data): array
+    {
+        $result = [
+            'total' => 0.00,
+            'unbalance' => 0.00,
+            'lines' => [],
+            'subaccount' => [],
+            'vat' => []
+        ];
+
+        if (isset($data['lines'])) {
+            // Prepare lines data
+            $lines = $this->views['EditAsiento']->processFormLines($data['lines']);
+
+            // Recalculate lines data and amounts
+            $totalCredit = $totalDebit = 0.00;
+            $result['lines'] = $this->recalculateLines($lines, $totalCredit, $totalDebit);
+            $this->calculateAmounts($result, $totalCredit, $totalDebit);
+
+            // If only change subaccount, search for subaccount data
+            if (count($data['changes']) === 1 && $data['changes'][0][1] === 'codsubcuenta') {
+                $index = $data['changes'][0][0];
+                $line = &$result['lines'][$index];
+                $result['subaccount'] = $this->getAccountData($data['document']['codejercicio'], $line['codsubcuenta']);
+
+                $result['vat'] = $this->recalculateVatRegister(
+                    $line, $data['document'], $result['subaccount']['codevat'], $result['unbalance']
+                );
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Calculate data lines and credit/debit imports
+     *
+     * @param array $lines
+     * @param float $totalCredit
+     * @param float $totalDebit
+     * @return array
+     */
     private function recalculateLines(array $lines, float &$totalCredit, float &$totalDebit): array
     {
         // Work variables
@@ -227,6 +367,65 @@ class EditAsiento extends ExtendedController\PanelController
             $result[] = $item;
         }
         return $result;
+    }
+
+    /**
+     * Calculate Vat Register data
+     *
+     * @param array $line
+     * @param array $document
+     * @param string $codevat
+     * @param float $base
+     * @return array
+     */
+    private function recalculateVatRegister(array &$line, array $document, string $codevat, float $base): array
+    {
+        $result = [];
+        if (empty($codevat)) {
+            $line['cifnif'] = null;
+            $line['documento'] = null;
+            $line['baseimponible'] = null;
+            $line['iva'] = null;
+            $line['recargo'] = null;
+            return $result;
+        }
+
+        $vat = new Model\Impuesto();
+        if ($vat->loadFromCode($codevat)) {
+            $result = $this->getAccountVatID($document['codejercicio'], $line['codcontrapartida']);
+            $line['documento'] = $document['documento'];
+            $line['cifnif'] = $result['id'];
+            $line['iva'] = $vat->iva;
+            $line['recargo'] = $result['surcharge'] ? $vat->recargo : 0.00;
+            $line['baseimponible'] = ($result['group'] === 'CLIENT') ? ($base * -1) : $base;
+        }
+        return $result;
+    }
+
+    /**
+     * Replace concept in concepts array with macro values
+     *
+     * @param array array
+     *
+     * @return array
+     */
+    private function replaceConcept($results): array
+    {
+        $finalResults = [];
+        $idasiento = $this->request->get('code');
+        $accounting = new Model\Asiento();
+        $where = [new DataBaseWhere('idasiento', $idasiento),];
+
+        if ($accounting->loadFromCode('', $where)) {
+            $search = ['%document%', '%date%', '%date-entry%', '%month%'];
+            $replace = [$accounting->documento, date('d-m-Y'), $accounting->fecha, $this->i18n->trans(date('F', strtotime($accounting->fecha)))];
+            foreach ($results as $result) {
+                $finalValue = array('key' => str_replace($search, $replace, $result['key']), 'value' => $result['value']);
+                $finalResults[] = $finalValue;
+            }
+        }
+
+        return $finalResults;
     }
 
     /**
@@ -265,201 +464,8 @@ class EditAsiento extends ExtendedController\PanelController
         }
     }
 
-    /**
-     * Get VAT information for a sub-account
-     *
-     * @param string $exercise
-     * @param string $codeSubAccount
-     * @return array
-     */
-    private function getAccountVatID($exercise, $codeSubAccount): array
+    public function getModelClassName()
     {
-        $result = ['group' => '', 'code' => '', 'description' => '', 'id' => '', 'surcharge' => false];
-        if (empty($exercise) || empty($codeSubAccount)) {
-            return $result;
-        }
-
-        $where = [
-            new DataBaseWhere('codsubcuenta', $codeSubAccount),
-            new DataBaseWhere('codejercicio', $exercise)
-        ];
-
-        $subAccount = new Model\Subcuenta();
-        if ($subAccount->loadFromCode(null, $where)) {
-            $result['group'] = $subAccount->getSpecialAccountCode();
-            switch ($result['group']) {
-                case 'CLIENT':
-                    $this->searchVatDataFromClient($codeSubAccount, $result);
-                    break;
-
-                case 'ACREED':
-                case 'PROVEE':
-                    $this->searchVatDataFromSupplier($codeSubAccount, $result);
-                    break;
-            }
-        }
-        return $result;
-    }
-
-    private function recalculateVatRegister(array &$line, array $document, string $codevat, float $base): array
-    {
-        $result = [];
-        if (empty($codevat)) {
-            $line['cifnif'] = null;
-            $line['documento'] = null;
-            $line['baseimponible'] = null;
-            $line['iva'] = null;
-            $line['recargo'] = null;
-            return $result;
-        }
-
-        $vat = new Model\Impuesto();
-        if ($vat->loadFromCode($codevat)) {
-            $result = $this->getAccountVatID($document['codejercicio'], $line['codcontrapartida']);
-            $line['documento'] = $document['documento'];
-            $line['cifnif'] = $result['id'];
-            $line['iva'] = $vat->iva;
-            $line['recargo'] = $result['surcharge'] ? $vat->recargo : 0.00;
-            $line['baseimponible'] = ($result['group'] === 'CLIENT') ? ($base * -1) : $base;
-        }
-        return $result;
-    }
-
-    protected function recalculateDocument(&$data): array
-    {
-        $result = [
-            'total' => 0.00,
-            'unbalance' => 0.00,
-            'lines' => [],
-            'subaccount' => [],
-            'vat' => []
-        ];
-
-        if (isset($data['lines'])) {
-            // Prepare lines data
-            $lines = $this->views['EditAsiento']->processFormLines($data['lines']);
-
-            // Recalculate lines data and amounts
-            $totalCredit = $totalDebit = 0.00;
-            $result['lines'] = $this->recalculateLines($lines, $totalCredit, $totalDebit);
-            $this->calculateAmounts($result, $totalCredit, $totalDebit);
-
-            // If only change subaccount, search for subaccount data
-            if (count($data['changes']) === 1 && $data['changes'][0][1] === 'codsubcuenta') {
-                $index = $data['changes'][0][0];
-                $line = &$result['lines'][$index];
-                $result['subaccount'] = $this->getAccountData($data['document']['codejercicio'], $line['codsubcuenta']);
-
-                $result['vat'] = $this->recalculateVatRegister(
-                    $line, $data['document'], $result['subaccount']['codevat'], $result['unbalance']
-                );
-            }
-        }
-
-        return $result;
-    }
-
-    private function cloneDocument(&$data): string
-    {
-        // init document
-        $accounting = new Model\Asiento();
-        $accounting->loadFromData($data, ['action', 'active']);
-
-        // init line document
-        $entryModel = new Model\Partida();
-        $entries = $entryModel->all([new DataBaseWhere('idasiento', $data['idasiento'])]);
-
-        // start transaction
-        $dataBase = new DataBase();
-        $dataBase->beginTransaction();
-
-        // main save process
-        try {
-            $accounting->idasiento = null;
-            $accounting->fecha = date('d-m-Y');
-            $accounting->numero = $accounting->newCode('numero');
-            if (!$accounting->save()) {
-                throw new Exception(self::$i18n->trans('clone-document-error'));
-            }
-
-            foreach ($entries as $line) {
-                $line->idpartida = null;
-                $line->idasiento = $accounting->idasiento;
-                if (!$line->save()) {
-                    throw new Exception(self::$i18n->trans('clone-line-document-error'));
-                }
-            }
-            // confirm data
-            $dataBase->commit();
-            $result = $accounting->url('type') . '&action=save-ok';
-        } catch (Exception $e) {
-            self::$miniLog->alert($e->getMessage());
-            $result = '';
-        } finally {
-            if ($dataBase->inTransaction()) {
-                $dataBase->rollback();
-            }
-        }
-
-        return $result;
-    }
-
-    /**
-     * Load data view procedure
-     *
-     * @param string                      $viewName
-     * @param ExtendedController\BaseView $view
-     */
-    protected function loadData($viewName, $view)
-    {
-        switch ($viewName) {
-            case 'EditAsiento':
-                $code = $this->request->get('code');
-                $view->loadData($code);
-                if ($view->count > 0) {
-                    $where = [new DataBaseWhere('idasiento', $code)];
-                    $orderby = ['idpartida' => 'ASC'];
-                    $view->loadGridData($where, $orderby);
-                }
-                break;
-        }
-    }
-
-    /**
-     * Overwrite autocomplete function to macro concepts in accounting concept.
-     */
-    protected function autocompleteAction(): array
-    {
-        if ($this->request->get('source', '') === 'conceptos_partidas') {
-            return $this->replaceConcept(parent::autocompleteAction());
-        }
-
-        return parent::autocompleteAction();
-    }
-
-    /**
-     * Replace concept in concepts array with macro values
-     *
-     * @param array array
-     *
-     * @return array
-     */
-    private function replaceConcept($results): array
-    {
-        $finalResults = [];
-        $idasiento = $this->request->get('code');
-        $accounting = new Model\Asiento();
-        $where = [new DataBaseWhere('idasiento', $idasiento),];
-
-        if ($accounting->loadFromCode('', $where)) {
-            $search = ['%document%', '%date%', '%date-entry%', '%month%'];
-            $replace = [$accounting->documento, date('d-m-Y'), $accounting->fecha, $this->i18n->trans(date('F', strtotime($accounting->fecha)))];
-            foreach ($results as $result) {
-                $finalValue = array('key' => str_replace($search, $replace, $result['key']), 'value' => $result['value']);
-                $finalResults[] = $finalValue;
-            }
-        }
-
-        return $finalResults;
+        return 'Asiento';
     }
 }
