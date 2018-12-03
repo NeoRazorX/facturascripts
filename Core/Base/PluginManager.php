11b768fda771f27b6675b8a5f8ddd7e34af3d633
NeoRazorX <neorazorx@gmail.com>
2018-12-03 11:00:50 +0100

    Updated version.

--- Core/Base/PluginManager.php
+++ Core/Base/PluginManager.php
@@ -33,7 +33,7 @@ class PluginManager
     /**
      * FacturaScripts core version.
      */
-    const CORE_VERSION = 2018.012;
+    const CORE_VERSION = 2018.013;
 
     /**
      * Path to list plugins on file.
