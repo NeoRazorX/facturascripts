78f05397744e853f1c6c0bb9e094d6e33d919f18
NeoRazorX <neorazorx@gmail.com>
2019-01-05 00:43:55 +0100

    Now you can't remove default company.

--- Core/Model/Empresa.php
+++ Core/Model/Empresa.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2013-2018  Carlos Garcia Gomez  <carlos@facturascripts.com>
+ * Copyright (C) 2013-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -137,6 +137,20 @@ class Empresa extends Base\Contact
         parent::clear();
         $this->codpais = AppSettings::get('default', 'codpais');
         $this->regimeniva = self::$regimenIVA->defaultValue();
+    }
+
+    /**
+     * 
+     * @return bool
+     */
+    public function delete()
+    {
+        if ($this->idempresa == AppSettings::get('default', 'idempresa')) {
+            self::$miniLog->alert('you-cant-not-remove-default-company');
+            return false;
+        }
+
+        return parent::delete();
     }
 
     /**
