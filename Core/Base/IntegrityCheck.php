83d1c0c6a820c38be9f6860f3724e610fe7aa67f
NeoRazorX <neorazorx@gmail.com>
2019-01-04 20:49:29 +0100

    Minor code improvements.

--- Core/Base/IntegrityCheck.php
+++ Core/Base/IntegrityCheck.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2018 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2018-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 namespace FacturaScripts\Core\Base;
 
 /**
@@ -26,6 +25,7 @@ namespace FacturaScripts\Core\Base;
  */
 class IntegrityCheck
 {
+
     /**
      * Path to list of integrity file.
      */
