7c292bf6fdce59da9eb197474972b3f2e449e29f
NeoRazorX <neorazorx@gmail.com>
2019-01-06 17:37:50 +0100

    Minor fix.

--- Core/Model/TransferenciaStock.php
+++ Core/Model/TransferenciaStock.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2013-2018 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2013-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -20,7 +20,6 @@ namespace FacturaScripts\Core\Model;
 
 use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
 use FacturaScripts\Core\Base\Utils;
-use FacturaScripts\Dinamic\Model\LineaTransferenciaStock;
 
 /**
  * The head of transfer.
