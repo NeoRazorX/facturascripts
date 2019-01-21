fb6e921a4c8c02fd613b598f58cff496e898b46c
NeoRazorX <neorazorx@gmail.com>
2019-01-21 11:49:26 +0100

    Minor fixes to widget textarea.

--- Core/Lib/Widget/WidgetTextarea.php
+++ Core/Lib/Widget/WidgetTextarea.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2018 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -21,11 +21,12 @@ namespace FacturaScripts\Core\Lib\Widget;
 /**
  * Description of WidgetTextarea
  *
- * @author Carlos García Gómez  <carlos@facturascripts.com>
- * @author Cristo M. Estévez Hernández <cristom.estevez@gmail.com>
+ * @author Carlos García Gómez          <carlos@facturascripts.com>
+ * @author Cristo M. Estévez Hernández  <cristom.estevez@gmail.com>
  */
 class WidgetTextarea extends WidgetText
 {
+
     /**
      * Indicates the number of rows value
      *
@@ -40,7 +41,7 @@ class WidgetTextarea extends WidgetText
     public function __construct($data)
     {
         parent::__construct($data);
-        $this->rows = (int) ($data['rows'] ?? 5);
+        $this->rows = (int) ($data['rows'] ?? 3);
     }
 
     /**
