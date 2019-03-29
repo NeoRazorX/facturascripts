c1187415def43dc0d3f9c541a5991db6a02e7239
NeoRazorX <neorazorx@gmail.com>
2019-03-29 17:50:41 +0100

    Updated version.

--- Core/App/AppController.php
+++ Core/App/AppController.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2018 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -226,7 +226,7 @@ class AppController extends App
                 $template = 'Error/ControllerError.html.twig';
             }
         } else {
-            $this->miniLog->alert($this->i18n->trans('controller-not-found'));
+            $this->miniLog->critical($this->i18n->trans('controller-not-found'));
         }
 
         $this->response->setStatusCode($httpStatus);
--- Core/Base/PluginManager.php
+++ Core/Base/PluginManager.php
@@ -33,7 +33,7 @@ class PluginManager
     /**
      * FacturaScripts core version.
      */
-    const CORE_VERSION = 2018.016;
+    const CORE_VERSION = 2018.017;
 
     /**
      * Path to list plugins on file.
