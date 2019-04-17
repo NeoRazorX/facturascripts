956e43ea538f4b13f61f5598f025658ebdcb99d0
Cristo Estévez <cristom.estevez@gmail.com>
2019-04-17 12:50:08 +0100

    Added functionallity to personalize the numeration and code of sales and purchase documents

---
+++ Core/Model/SecuenciaDocumento.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * This file is part of FacturaScripts
+ * Copyright (C) 2013-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace FacturaScripts\Core\Model;
+
+/**
+ * Personalize the numeration and code of sale and purchase documents.
+ *
+ * @author Carlos García Gómez <carlos@facturascripts.com>
+ * @author Cristo M. Estévez Hernández <cristom.estevez@gmail.com>
+ */
+class SecuenciaDocumento extends Base\ModelClass
+{
+
+    use Base\ModelTrait;
+
+    /**
+     *
+     * @var string
+     */
+    public $codejercicio;
+
+    /**
+     *
+     * @var string
+     */
+    public $codserie;
+
+    /**
+     *
+     * @var int
+     */
+    public $longnumero;
+
+    /**
+     * Primary key.
+     *
+     * @var int
+     */
+    public $id;
+
+    /**
+     *
+     * @var int
+     */
+    public $numero;
+
+    /**
+     *
+     * @var string
+     */
+    public $patron;
+
+    /**
+     *
+     * @var bool
+     */
+    public $reutilizar;
+
+    /**
+     *
+     * @var string
+     */
+    public $titulo;
+
+    /**
+     *
+     * @var string
+     */
+    public $tipodoc;
+
+
+    /**
+     * Returns the name of the column that is the primary key of the model.
+     *
+     * @return string
+     */
+    public static function primaryColumn()
+    {
+        return 'id';
+    }
+
+    /**
+     * Returns the name of the table that uses this model.
+     *
+     * @return string
+     */
+    public static function tableName()
+    {
+        return 'secuenciasdocumentos';
+    }
+}
