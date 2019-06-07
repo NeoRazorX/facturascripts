409cadb74fb33c44090e4a055364403bb5100afc
NeoRazorX <neorazorx@gmail.com>
2019-06-07 20:21:49 +0200

    Added more dinamic files.

---
+++ Dinamic/Model/Base/Receipt.php
@@ -0,0 +1,10 @@
+<?php namespace FacturaScripts\Dinamic\Model\Base;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @package FacturaScripts\Dinamic\Model\Base
+ * @author Carlos García Gómez <carlos@facturascripts.com>
+ */
+abstract class Receipt extends \FacturaScripts\Core\Model\Base\Receipt
+{
+}
