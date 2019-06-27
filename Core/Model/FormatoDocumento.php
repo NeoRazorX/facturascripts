710223ca22711720d73bc2c8faa596280077febf
Cristo Estévez <cristom.estevez@gmail.com>
2019-06-27 13:23:37 +0100

    Added model, table, view  and controller to personalize the impression of documents.

---
+++ Core/Model/FormatoDocumento.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * This file is part of FacturaScripts
+ * Copyright (C) 2013-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace FacturaScripts\Core\Model;
+
+/**
+ * Model to personalize the impresion of sales and buy documents.
+ *
+ * @author Cristo M. Estévez Hernández <cristom.estevez@gmail.com>
+ */
+class FormatoDocumento extends Base\ModelClass
+{
+
+    use Base\ModelTrait;
+
+    /**
+     * Foreign key with series table
+     *
+     * @var string
+     */
+    public $codserie;
+
+    /**
+     * Primary key
+     *
+     * @var int
+     */
+    public $id;
+
+    /**
+     * Foreign key with table business
+     *
+     * @var int
+     */
+    public $idempresa;
+
+    /**
+     * Foreign key with table Attached_files
+     *
+     * @var int
+     */
+    public $icono;
+
+    /**
+     * 
+     *
+     * @var string
+     */
+    public $texto;
+
+    /**
+     * 
+     *
+     * @var string
+     */
+    public $tipodoc;
+
+    /**
+     * 
+     *
+     * @var string
+     */
+    public $titulo;
+
+    /**
+     * Returns the name of the column that is the primary key of the model.
+     *
+     * @return string
+     */
+    public static function primaryColumn()
+    {
+        return 'id';
+    }
+
+    /**
+     * Returns the name of the table that uses this model.
+     *
+     * @return string
+     */
+    public static function tableName()
+    {
+        return 'formatos_documentos';
+    }
+
+
+    /**
+     * This function is called when creating the model table. Returns the SQL
+     * that will be executed after the creation of the table. Useful to insert values
+     * default.
+     *
+     * @return string
+     */
+    public function install()
+    {
+        new Serie();
+        new Empresa();
+        new AttachedFile();
+
+        return parent::install();
+    }
+}
