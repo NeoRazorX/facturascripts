f545cac159316370d27a8726a81de783d6c28b2d
NeoRazorX <neorazorx@gmail.com>
2019-07-04 16:55:56 +0200

    Minor improvements to api key controllers.

--- Core/Model/ApiKey.php
+++ Core/Model/ApiKey.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2018  Carlos Garcia Gomez  <carlos@facturascripts.com>
+ * Copyright (C) 2018-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -93,6 +93,15 @@ class ApiKey extends Base\ModelClass
     public static function primaryColumn()
     {
         return 'id';
+    }
+
+    /**
+     * 
+     * @return string
+     */
+    public function primaryDescriptionColumn()
+    {
+        return 'description';
     }
 
     /**
