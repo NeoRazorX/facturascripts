0c8a96271443479a4238ae7c7c8bd3a7837e9318
NeoRazorX <neorazorx@gmail.com>
2019-08-10 12:45:07 +0200

    - Fixed bug with maxlength in widget link. - Normalized some table columns like telephone, fax or website.

--- Core/Lib/Widget/WidgetLink.php
+++ Core/Lib/Widget/WidgetLink.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2018 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -24,7 +24,7 @@ namespace FacturaScripts\Core\Lib\Widget;
  * @author Artex Trading sa     <jcuello@artextrading.com>
  * @author Carlos García Gómez  <carlos@facturascripts.com>
  */
-class WidgetLink extends BaseWidget
+class WidgetLink extends WidgetText
 {
 
     /**
