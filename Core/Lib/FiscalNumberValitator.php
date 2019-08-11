f6bd691c64f90486ea88ee2f0a56d2f378de5485
NeoRazorX <neorazorx@gmail.com>
2019-08-11 13:40:41 +0200

    - Fixed bug comparing data types in MySQL. - Fixed bug with non static method call on static method.

--- Core/Lib/FiscalNumberValitator.php
+++ Core/Lib/FiscalNumberValitator.php
@@ -64,16 +64,21 @@ class FiscalNumberValitator
                 return $validator->isValidNIF($upperNumber) || $validator->isValidDNI($upperNumber);
 
             case 'rfc':
-                return self::isValidRFC($upperNumber);
+                return static::isValidRFC($upperNumber);
         }
 
         return true;
     }
 
-    protected function isValidRFC($number)
+    /**
+     * 
+     * @param string $number
+     *
+     * @return bool
+     */
+    protected static function isValidRFC($number)
     {
         $pattern = "/[A-Z]{3,4}[0-9]{2}(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])[A-Z0-9]{2}[0-9A]/";
-
         return 1 === preg_match($pattern, $number);
     }
 }
