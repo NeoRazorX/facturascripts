7f3491001eab8e80b09772dd9c7af27dcb08dddd
NeoRazorX <neorazorx@gmail.com>
2019-09-02 19:21:20 +0200

    Updated Dinamic.

---
+++ Dinamic/Lib/PDF/PDFCore.php
@@ -0,0 +1,9 @@
+<?php namespace FacturaScripts\Dinamic\Lib\PDF;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @author FacturaScripts <carlos@facturascripts.com>
+ */
+class PDFCore extends \FacturaScripts\Core\Lib\PDF\PDFCore
+{
+}
