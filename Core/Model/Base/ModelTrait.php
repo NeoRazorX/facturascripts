0eca4fc585b35faff23ba89c1dad42427b28666e
NeoRazorX <neorazorx@gmail.com>
2019-09-08 11:25:09 +0200

    Minor fix in ModelTrait.

--- Core/Model/Base/ModelTrait.php
+++ Core/Model/Base/ModelTrait.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2013-2018 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2013-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -75,8 +75,8 @@ trait ModelTrait
      */
     protected function loadModelFields(DataBase &$dataBase, string $tableName)
     {
-        if (empty(self::$fields)) {
-            self::$fields = $dataBase->tableExists($tableName) ? $dataBase->getColumns($tableName) : [];
+        if (empty(static::$fields)) {
+            static::$fields = $dataBase->tableExists($tableName) ? $dataBase->getColumns($tableName) : [];
         }
     }
 }
