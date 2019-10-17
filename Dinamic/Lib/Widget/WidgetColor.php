211d66f5baa58160e194b936613db184c7d5736b
NeoRazorX <neorazorx@gmail.com>
2019-10-17 10:25:03 +0200

    Updated dinamic.

---
+++ Dinamic/Lib/Widget/WidgetColor.php
@@ -0,0 +1,9 @@
+<?php namespace FacturaScripts\Dinamic\Lib\Widget;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @author FacturaScripts <carlos@facturascripts.com>
+ */
+class WidgetColor extends \FacturaScripts\Core\Lib\Widget\WidgetColor
+{
+}
