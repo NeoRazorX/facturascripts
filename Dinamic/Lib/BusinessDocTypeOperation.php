4bde444c197ddde6f67c8275029c6fae424611da
NeoRazorX <neorazorx@gmail.com>
2019-11-22 12:58:37 +0100

    Updated Dinamic.

---
+++ Dinamic/Lib/BusinessDocTypeOperation.php
@@ -0,0 +1,9 @@
+<?php namespace FacturaScripts\Dinamic\Lib;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @author FacturaScripts <carlos@facturascripts.com>
+ */
+class BusinessDocTypeOperation extends \FacturaScripts\Core\Lib\BusinessDocTypeOperation
+{
+}
