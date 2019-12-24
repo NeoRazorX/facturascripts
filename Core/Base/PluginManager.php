fd2f0cabcf0aed37b06033c4c455a46d8ced630f
NeoRazorX <neorazorx@gmail.com>
2019-12-24 14:00:30 +0100

    - Business document author is now on the detail tab. - Updated translations. - Fixed last merge. - Updated version.

--- Core/Base/PluginManager.php
+++ Core/Base/PluginManager.php
@@ -31,7 +31,7 @@ class PluginManager
     /**
      * FacturaScripts core version.
      */
-    const CORE_VERSION = 2018.16;
+    const CORE_VERSION = 2020.01;
 
     /**
      * Path to list plugins on file.
