3ed3067868d29af7ec4d033214728a13b71eb772
NeoRazorX <neorazorx@gmail.com>
2019-12-26 11:21:49 +0100

    Fixed some minor bugs.

--- Core/Lib/BusinessDocumentGenerator.php
+++ Core/Lib/BusinessDocumentGenerator.php
@@ -37,7 +37,7 @@ class BusinessDocumentGenerator
      * @var array
      */
     public $excludeFields = [
-        'codejercicio', 'codigo', 'dtopor1', 'dtopor2', 'fecha', 'femail',
+        'codejercicio', 'codigo', 'fecha', 'femail',
         'hora', 'idestado', 'neto', 'netosindto', 'numero', 'total',
         'totalirpf', 'totaliva', 'totalrecargo',
     ];
