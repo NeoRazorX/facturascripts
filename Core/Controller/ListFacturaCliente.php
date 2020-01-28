dbf121b28e3457d147176e1e1d80b870c7ee98e3
NeoRazorX <neorazorx@gmail.com>
2020-01-28 13:36:12 +0100

    Fixed last merge.

--- Core/Controller/ListFacturaCliente.php
+++ Core/Controller/ListFacturaCliente.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2020 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -52,7 +52,7 @@ class ListFacturaCliente extends ListBusinessDocument
     {
         $this->createViewSales('ListFacturaCliente', 'FacturaCliente', 'invoices');
         $this->addFilterCheckbox('ListFacturaCliente', 'pagada', 'unpaid', '', '!=');
-        $this->addButtonCompleteDocument('ListFacturaCliente');
+        $this->addButtonLockInvoice('ListFacturaCliente');
 
         $this->createViewLines('ListLineaFacturaCliente', 'LineaFacturaCliente');
         $this->createViewReceipts();
--- Core/Controller/ListFacturaProveedor.php
+++ Core/Controller/ListFacturaProveedor.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2020 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -52,7 +52,7 @@ class ListFacturaProveedor extends ListBusinessDocument
     {
         $this->createViewPurchases('ListFacturaProveedor', 'FacturaProveedor', 'invoices');
         $this->addFilterCheckbox('ListFacturaProveedor', 'pagada', 'unpaid', '', '!=');
-        $this->addButtonCompleteDocument('ListFacturaProveedor');
+        $this->addButtonLockInvoice('ListFacturaProveedor');
 
         $this->createViewLines('ListLineaFacturaProveedor', 'LineaFacturaProveedor');
         $this->createViewReceipts();
