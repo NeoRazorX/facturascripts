41f294abbed88ead863e65dd9c54a3af2e4bbefc
NeoRazorX <neorazorx@gmail.com>
2020-02-04 12:19:55 +0100

    Prevent text wrap on mony, number and date widgets.

--- Core/Lib/Widget/BaseWidget.php
+++ Core/Lib/Widget/BaseWidget.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2020 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -311,7 +311,7 @@ class BaseWidget extends VisualItem
             }
         }
 
-        $class = [$initialClass];
+        $class = [trim($initialClass)];
         if (!empty($alternativeClass)) {
             $class[] = $alternativeClass;
         } elseif (is_null($this->value)) {
