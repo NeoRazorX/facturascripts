46717b8e999b49f0429c3b2138ac675ac2a2fcdf
Raúl <raljopa@gmail.com>
2020-02-12 22:35:35 +0100

    Tarea #362 Añadir widget porcentaje. Como el widget number pero que al listar (el método show()) añade %.

---
+++ Core/Lib/Widget/WidgetPercentage.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This file is part of FacturaScripts
+ * Copyright (C) 2017-2020 Carlos Garcia Gomez <carlos@facturascripts.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace FacturaScripts\Core\Lib\Widget;
+
+use FacturaScripts\Core\Base\NumberTools;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Description of WidgetNumber
+ *
+ * @author Carlos García Gómez  <carlos@facturascripts.com>
+ * @author Artex Trading sa     <jcuello@artextrading.com>
+ */
+class WidgetPercentage extends WidgetNumber
+{
+
+    protected function show()
+    {
+        error_log('porcentaje');
+        return is_null($this->value) ? '-' : static::$numberTools->format($this->value, $this->decimal) . '%';
+    }
+}
