954dbf25b63a285bf9f7c30ddb07e56e76185d6b
Yopli2k <yopli2000@gmail.com>
2020-02-25 08:40:23 +0100

    Add filters for company and channel

--- Core/Controller/ListReportAccounting.php
+++ Core/Controller/ListReportAccounting.php
@@ -29,6 +29,8 @@ use FacturaScripts\Core\Model\ReportBalance;
 class ListReportAccounting extends ListController
 {
 
+    private $companyList = null;
+
     /**
      * Return the basic data for this page.
      *
@@ -43,6 +45,9 @@ class ListReportAccounting extends ListController
         return $data;
     }
 
+    /**
+     * Inserts the views or tabs to display.
+     */
     protected function createViews()
     {
         $this->createViewsLedger();
@@ -50,32 +55,48 @@ class ListReportAccounting extends ListController
         $this->createViewsBalance();
     }
 
+    /**
+     * Inserts the view for amount balances.
+     */
     protected function createViewsAmount($viewName = 'ListReportAmount')
     {
         $this->addView($viewName, 'ReportAmount', 'amounts', 'fas fa-calculator');
         $this->addOrderBy($viewName, ['name'], 'name');
         $this->addOrderBy($viewName, ['idcompany', 'name'], 'company');
         $this->addSearchFields($viewName, ['name']);
+        $this->addCommonFilter($viewName);
     }
 
+    /**
+     * Inserts the view for sheet and Profit & Loss balances.
+     */
     protected function createViewsBalance($viewName = 'ListReportBalance')
     {
         $this->addView($viewName, 'ReportBalance', 'balances', 'fas fa-book');
         $this->addOrderBy($viewName, ['name'], 'name');
         $this->addOrderBy($viewName, ['idcompany', 'name'], 'company');
         $this->addSearchFields($viewName, ['name']);
-
+        $this->addCommonFilter($viewName);
         $this->loadWidgetValues($viewName);
     }
 
+    /**
+     * Inserts the view for ledger report.
+     */
     protected function createViewsLedger($viewName = 'ListReportLedger')
     {
         $this->addView($viewName, 'ReportLedger', 'ledger', 'fas fa-file-alt');
         $this->addOrderBy($viewName, ['name'], 'name');
         $this->addOrderBy($viewName, ['idcompany', 'name'], 'company');
         $this->addSearchFields($viewName, ['name']);
+        $this->addCommonFilter($viewName);
     }
 
+    /**
+     * Load values into special widget columns
+     *
+     * @param string $viewName
+     */
     protected function loadWidgetValues($viewName)
     {
         $typeColumn = $this->views[$viewName]->columnForField('type');
@@ -87,5 +108,19 @@ class ListReportAccounting extends ListController
         if ($formatColumn) {
             $formatColumn->widget->setValuesFromArray(ReportBalance::formatList());
         }
+    }
+
+    /**
+     * Add to indicated view a filter select with company list
+     *
+     * @param string $viewName
+     */
+    private function addCommonFilter($viewName)
+    {
+        if ($this->companyList === null) {
+            $this->companyList = $this->codeModel->all('empresas', 'idempresa', 'nombrecorto');
+        }
+        $this->addFilterSelect($viewName, 'idcompany', 'company', 'idcompany', $this->companyList);
+        $this->addFilterNumber($viewName, 'channel', 'channel', 'channel', '=');
     }
 }
