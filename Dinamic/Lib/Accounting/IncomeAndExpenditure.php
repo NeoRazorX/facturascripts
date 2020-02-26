b17a3b86974c638cefc6e42ee4a85ff90eb60fb4
José Antonio Cuello Principal <yopli2000@gmail.com>
2020-02-26 12:21:52 +0100

    Update Dinamic

---
+++ Dinamic/Lib/Accounting/IncomeAndExpenditure.php
@@ -0,0 +1,9 @@
+<?php namespace FacturaScripts\Dinamic\Lib\Accounting;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @author FacturaScripts <carlos@facturascripts.com>
+ */
+class IncomeAndExpenditure extends \FacturaScripts\Core\Lib\Accounting\IncomeAndExpenditure
+{
+}
