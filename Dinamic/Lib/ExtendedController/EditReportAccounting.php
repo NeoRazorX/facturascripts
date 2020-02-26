fc9be8ebc99f1319a3a4e153c4d9474ee29ba540
NeoRazorX <neorazorx@gmail.com>
2020-02-26 20:00:15 +0100

    Updated dinamic.

---
+++ Dinamic/Lib/ExtendedController/EditReportAccounting.php
@@ -0,0 +1,9 @@
+<?php namespace FacturaScripts\Dinamic\Lib\ExtendedController;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @author FacturaScripts <carlos@facturascripts.com>
+ */
+abstract class EditReportAccounting extends \FacturaScripts\Core\Lib\ExtendedController\EditReportAccounting
+{
+}
---
+++ Dinamic/Lib/Widget/WidgetPercentage.php
@@ -0,0 +1,9 @@
+<?php namespace FacturaScripts\Dinamic\Lib\Widget;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @author FacturaScripts <carlos@facturascripts.com>
+ */
+class WidgetPercentage extends \FacturaScripts\Core\Lib\Widget\WidgetPercentage
+{
+}
