260f9699ac2d492ea412e1d4f62444f7861bca82
NeoRazorX <neorazorx@gmail.com>
2020-04-28 19:06:58 +0200

    Updated Dinamic.

---
+++ Dinamic/Model/Base/AccEntryRelationTrait.php
@@ -0,0 +1,9 @@
+<?php namespace FacturaScripts\Dinamic\Model\Base;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @author FacturaScripts <carlos@facturascripts.com>
+ */
+class AccEntryRelationTrait extends \FacturaScripts\Core\Model\Base\AccEntryRelationTrait
+{
+}
