a316eebb169e085fc98d39816d2933cd4c2362e6
NeoRazorX <neorazorx@gmail.com>
2020-05-08 13:26:57 +0200

    Added plugin deploy before start testing.

--- Test/Core/Model/AlbaranClienteTest.php
+++ Test/Core/Model/AlbaranClienteTest.php
@@ -59,9 +59,7 @@ final class AlbaranClienteTest extends CustomTest
         foreach ($serieModel->all() as $serie) {
             $model->codserie = $serie->codserie;
         }
-        
-        \var_dump($model);
-        
+
         $this->assertTrue($model->save(), 'albaran-cliente-save-error');
 
         /// creating line
