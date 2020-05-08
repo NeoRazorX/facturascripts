0961ff253c1b67db3a7d7e8131b0288fb876b882
NeoRazorX <neorazorx@gmail.com>
2020-05-08 12:59:51 +0200

    Improved customer delivery note test.

--- Test/Core/Model/AlbaranClienteTest.php
+++ Test/Core/Model/AlbaranClienteTest.php
@@ -1,8 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017       Francesc Pineda Segarra <francesc.pineda.segarra@gmail.com>
- * Copyright (C) 2017-2018  Carlos Garcia Gomez     <carlos@facturascripts.com>
+ * Copyright (C) 2017-2020 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -20,10 +19,14 @@
 namespace FacturaScripts\Test\Core\Model;
 
 use FacturaScripts\Core\Model\AlbaranCliente;
+use FacturaScripts\Core\Model\Almacen;
+use FacturaScripts\Core\Model\Cliente;
+use FacturaScripts\Core\Model\FormaPago;
+use FacturaScripts\Core\Model\Serie;
 use FacturaScripts\Test\Core\CustomTest;
 
 /**
- * @covers \AlbaranCliente
+ * @covers AlbaranCliente
  */
 final class AlbaranClienteTest extends CustomTest
 {
@@ -31,5 +34,45 @@ final class AlbaranClienteTest extends CustomTest
     protected function setUp()
     {
         $this->model = new AlbaranCliente();
+    }
+
+    public function testSave()
+    {
+        /// create customer
+        $customer = new Cliente();
+        $customer->cifnif = '1234';
+        $customer->nombre = 'Pepe';
+        $this->assertTrue($customer->save(), 'cliente-save-error');
+
+        /// create document
+        $model = new AlbaranCliente();
+        $model->setSubject($customer);
+        $warehouseModel = new Almacen();
+        foreach ($warehouseModel->all() as $warehouse) {
+            $model->codalmacen = $warehouse->codalmacen;
+        }
+        $paymentModel = new FormaPago();
+        foreach ($paymentModel->all() as $payment) {
+            $model->codpago = $payment->codpago;
+        }
+        $serieModel = new Serie();
+        foreach ($serieModel->all() as $serie) {
+            $model->codserie = $serie->codserie;
+        }
+        $this->assertTrue($model->save(), 'albaran-cliente-save-error');
+
+        /// creating line
+        $newLine = $model->getNewLine();
+        $newLine->descripcion = 'test';
+        $this->assertTrue($newLine->save(), 'linea-albaran-cliente-save-error');
+
+        /// remove line
+        $this->assertTrue($newLine->delete(), 'linea-albaran-cliente-delete-error');
+
+        /// remove document
+        $this->assertTrue($model->delete(), 'albaran-cliente-delete-error');
+
+        /// remove customer
+        $this->assertTrue($customer->delete(), 'cliente-delete-error');
     }
 }
