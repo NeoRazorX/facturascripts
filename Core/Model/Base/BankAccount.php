c844a2c146c027bf0abd487504c7d9bda33de536
Raul Nazca <comercial@nazcanetworks.com>
2020-05-09 19:41:04 +0200

    Sin tarea asignada. Comprobar si un IBAN es válido. Adaptación para múltiples paises

--- Core/Model/Base/BankAccount.php
+++ Core/Model/Base/BankAccount.php
@@ -18,12 +18,15 @@
  */
 namespace FacturaScripts\Core\Model\Base;
 
+use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
+
 /**
  * This class groups the data and bank calculation methods
  * for a generic use.
  *
  * @author Carlos García Gómez  <carlos@facturascripts.com>
  * @author Artex Trading sa     <jcuello@artextrading.com>
+ * @author Raúl Jiménez          <raljopa@gmail.com>
  */
 abstract class BankAccount extends ModelClass
 {
@@ -65,6 +68,11 @@ abstract class BankAccount extends ModelClass
     public $swift;
 
     /**
+     * Message error in checked IBAN
+     */
+    private $errorMessage;
+
+    /**
      * Returns the IBAN with or without spaces.
      *
      * @param bool $spaced
@@ -83,7 +91,7 @@ abstract class BankAccount extends ModelClass
     }
 
     /**
-     * 
+     *
      * @return string
      */
     public static function primaryColumn()
@@ -92,7 +100,7 @@ abstract class BankAccount extends ModelClass
     }
 
     /**
-     * 
+     *
      * @param bool $value
      */
     public function setDisableIbanTest($value)
@@ -121,51 +129,69 @@ abstract class BankAccount extends ModelClass
     }
 
     /**
-     * Check if the DC's of an IBAN are correct.
+     * Check if a IBAN is correct.
      *
      * @param string $iban
      *
      * @return bool
      */
-    public function verifyIBAN(string $iban)
+    protected function verifyIBAN($iban)
     {
-        if (\strlen($iban) != 24) {
+        $countryCode = \strtoupper(\substr($iban, 0, 2));
+        $countryConfig = $this->getCountryConfig($countryCode);
+        $country = new \FacturaScripts\Dinamic\Model\Pais();
+        $country = $country->all([new DatabaseWhere('codiso', $countryCode)])[0];
+        if ($countryConfig['length'] == 0) {
+            return true;
+        }
+        if (\strlen($iban) != $countryConfig['length']) {
+
+            $this->errorMessage = 'bad-length ' . $country->nombre;
             return false;
         }
+        /* Check format */
+        $regex = '/' . $countryConfig['IBANRegexFormat'] . '/';
+        if (!preg_match($regex, $iban)) {
 
-        $codpais = \substr($iban, 0, 2);
-        $ccc = \substr($iban, -20);
-        return $iban == $this->calculateIBAN($ccc, $codpais);
-    }
+            $this->errorMessage = 'bad-format ' . $country->nombre;
+            return false;
+        }
+        $ccc = substr($iban, 4) . substr($iban, 0, 4);
+        $this->errorMessage = 'bad-control-digit ' . $country->nombre;
 
-    /**
-     * Calculate the IBAN from the bank account.
-     *
-     * @param string $ccc
-     * @param string $codpais
-     *
-     * @return string
-     */
-    private function calculateIBAN(string $ccc, string $codpais = '')
-    {
-        $pais = \strtoupper(\substr($codpais, 0, 2));
-        $pesos = ['A' => '10', 'B' => '11', 'C' => '12', 'D' => '13', 'E' => '14', 'F' => '15',
-            'G' => '16', 'H' => '17', 'I' => '18', 'J' => '19', 'K' => '20', 'L' => '21', 'M' => '22',
-            'N' => '23', 'O' => '24', 'P' => '25', 'Q' => '26', 'R' => '27', 'S' => '28', 'T' => '29',
-            'U' => '30', 'V' => '31', 'W' => '32', 'X' => '33', 'Y' => '34', 'Z' => '35',
-        ];
-
-        $dividendo = $ccc . $pesos[$pais[0]] . $pesos[$pais[1]] . '00';
-        $digitoControl = 98 - \bcmod($dividendo, '97');
-        if (\strlen($digitoControl) === 1) {
-            $digitoControl = '0' . $digitoControl;
+        $ibanCharacters = range('A', 'Z');
+        foreach (range(10, 35) as $tempvalue) {
+            $ibanValues[] = strval($tempvalue);
         }
 
-        return $pais . $digitoControl . $ccc;
+        $numericCcc = str_replace($ibanCharacters, $ibanValues, $ccc);
+
+        return (\bcmod($numericCcc, '97') == 1);
+    }
+
+    private function getCountryConfig($countryCode)
+    {
+        $countryData = [
+            'DK' => ['length' => '18', 'IBANFormat' => 'DK2!n4!n9!n1!n', 'IBANRegexFormat' => '^DK(\d{2})(\d{4})(\d{9})(\d{1})$'],
+            'BE' => ['length' => '16', 'IBANFormat' => 'BE2!n3!n7!n2!n', 'IBANRegexFormat' => '^BE(\d{2})(\d{3})(\d{7})(\d{2})$'],
+            'DE' => ['length' => '22', 'IBANFormat' => 'DE2!n8!n10!n', 'IBANRegexFormat' => '^DE(\d{2})(\d{8})(\d{10})$'],
+            'ES' => ['length' => '24', 'IBANFormat' => 'ES2!n4!n4!n1!n1!n10!n', 'IBANRegexFormat' => '^ES(\d{2})(\d{4})(\d{4})(\d{1})(\d{1})(\d{10})$'],
+            'FI' => ['length' => '18', 'IBANFormat' => 'FI2!n6!n7!n1!n', 'IBANRegexFormat' => '^FI(\d{2})(\d{6})(\d{7})(\d{1})$'],
+            'FR' => ['length' => '27', 'IBANFormat' => 'FR2!n5!n5!n11!c2!n', 'IBANRegexFormat' => '^FR(\d{2})(\d{5})(\d{5})([A-Za-z0-9]{11})(\d{2})$'],
+            'GB' => ['length' => '22', 'IBANFormat' => 'GB2!n4!a6!n8!n', 'IBANRegexFormat' => '^GB(\d{2})([A-Z]{4})(\d{6})(\d{8})$'],
+            'GR' => ['length' => '27', 'IBANFormat' => 'GR2!n3!n4!n16!c', 'IBANRegexFormat' => '^GR(\d{2})(\d{3})(\d{4})([A-Za-z0-9]{16})$'],
+            'IE' => ['length' => '22', 'IBANFormat' => 'IE2!n4!a6!n8!n', 'IBANRegexFormat' => '^IE(\d{2})([A-Z]{4})(\d{6})(\d{8})$'],
+            'IT' => ['length' => '27', 'IBANFormat' => 'IT2!n1!a5!n5!n12!c', 'IBANRegexFormat' => '^IT(\d{2})([A-Z]{1})(\d{5})(\d{5})([A-Za-z0-9]{12})$'],
+            'PT' => ['length' => '25', 'IBANFormat' => 'PT2!n4!n4!n11!n2!n', 'IBANRegexFormat' => '^PT(\d{2})(\d{4})(\d{4})(\d{11})(\d{2})$'],
+        ];
+        if (array_key_exists($countryCode, $countryData)) {
+            return $countryData[$countryCode];
+        }
+        return ['length' => '0', 'IBANFormat' => '', 'IBANRegexFormat' => ''];
     }
 
     /**
-     * 
+     *
      * @param array $values
      *
      * @return bool
@@ -191,6 +217,7 @@ abstract class BankAccount extends ModelClass
         }
 
         $this->toolBox()->i18nLog()->error('invalid-iban', ['%iban%' => $this->iban]);
+        $this->toolBox()->i18nLog()->error($this->errorMessage);
         return false;
     }
 }
