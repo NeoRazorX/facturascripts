5aa924eed74737ebaad6173220fc23c64d36473b
NeoRazorX <neorazorx@gmail.com>
2020-07-23 10:40:25 +0200

    Updated Dinamic.

---
+++ Dinamic/Lib/ExtendedController/ListBusinessActionTrait.php
@@ -0,0 +1,9 @@
+<?php namespace FacturaScripts\Dinamic\Lib\ExtendedController;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @author FacturaScripts <carlos@facturascripts.com>
+ */
+class ListBusinessActionTrait extends \FacturaScripts\Core\Lib\ExtendedController\ListBusinessActionTrait
+{
+}
