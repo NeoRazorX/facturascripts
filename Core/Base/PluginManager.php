7cd6f63f260345e9cd806fb60e794cf634234cd8
NeoRazorX <neorazorx@gmail.com>
2020-08-10 17:47:47 +0200

    Updated dependencies to PHP 7.1n symfony 4.4 and twig 2.13.

--- Core/Base/PluginManager.php
+++ Core/Base/PluginManager.php
@@ -31,7 +31,7 @@ class PluginManager
     /**
      * FacturaScripts core version.
      */
-    const CORE_VERSION = 2020.67;
+    const CORE_VERSION = 2020.68;
 
     /**
      * Path to list plugins on file.
