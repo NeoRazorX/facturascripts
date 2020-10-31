e40ec1b258cfdc561a645719cc4976a212faf8f4
NeoRazorX <neorazorx@gmail.com>
2020-10-31 11:21:49 +0100

    Minor code improvements.

--- Core/Base/DownloadTools.php
+++ Core/Base/DownloadTools.php
@@ -99,17 +99,19 @@ class DownloadTools
         $ch = \curl_init();
         \curl_setopt($ch, \CURLOPT_URL, $url);
         \curl_setopt($ch, \CURLOPT_RETURNTRANSFER, 1);
+        \curl_setopt($ch, \CURLOPT_TIMEOUT, $timeout);
 
         /// this function is called by curl for each header received
         \curl_setopt($ch, \CURLOPT_HEADERFUNCTION, function ($curl, $header) use (&$headers) {
             $len = \strlen($header);
-            $header = \explode(':', $header, 2);
-            if (\count($header) < 2) {
+            $headpart = \explode(':', $header, 2);
+            if (\count($headpart) < 2) {
                 /// ignore invalid headers
                 return $len;
             }
 
-            $headers[\strtolower(\trim($header[0]))][] = \trim($header[1]);
+            $key = \strtolower(\trim($headpart[0]));
+            $headers[$key][] = \trim($headpart[1]);
             return $len;
         });
 
