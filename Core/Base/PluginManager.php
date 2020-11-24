e845595150334503b1735360c23d73d89f9838f9
NeoRazorX <neorazorx@gmail.com>
2020-11-24 13:04:14 +0100

    Hide more optional code fields.

--- Core/Base/PluginManager.php
+++ Core/Base/PluginManager.php
@@ -31,7 +31,7 @@ class PluginManager
     /**
      * FacturaScripts core version.
      */
-    const CORE_VERSION = 2020.80;
+    const CORE_VERSION = 2020.81;
 
     /**
      * Path to list plugins on file.
