e179ef75ebc06173b38485921448206e78111eb7
NeoRazorX <neorazorx@gmail.com>
2020-12-01 23:11:39 +0100

    Fixed bug with wrong static call.

--- Core/Model/Base/PasswordTrait.php
+++ Core/Model/Base/PasswordTrait.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2019 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2019-2020 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -49,7 +49,7 @@ trait PasswordTrait
 
     abstract public function primaryColumnValue();
 
-    abstract protected function toolBox();
+    abstract protected static function toolBox();
 
     /**
      * Asigns the new password to the user.
