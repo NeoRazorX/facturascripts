8e3d2d129679e6eafe07a37248afe9e0ebb501a7
NeoRazorX <neorazorx@gmail.com>
2020-12-09 10:57:10 +0100

    Updated Dinamic.

---
+++ Dinamic/Model/Join/LiquidacionComisionFactura.php
@@ -0,0 +1,9 @@
+<?php namespace FacturaScripts\Dinamic\Model\Join;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @author FacturaScripts <carlos@facturascripts.com>
+ */
+class LiquidacionComisionFactura extends \FacturaScripts\Core\Model\Join\LiquidacionComisionFactura
+{
+}
