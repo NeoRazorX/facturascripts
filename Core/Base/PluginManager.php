5e89fef693873dd1f8404a335c65f3582226b686
Carlos García Gómez <neorazorx@gmail.com>
2021-03-09 10:05:56 +0100

    Improved templates and error messages about bank accounts.

--- Core/Base/PluginManager.php
+++ Core/Base/PluginManager.php
@@ -31,7 +31,7 @@ final class PluginManager
     /**
      * FacturaScripts core version.
      */
-    const CORE_VERSION = 2021.05;
+    const CORE_VERSION = 2021.06;
 
     /**
      * Path to list plugins on file.
--- Core/Model/Base/BankAccount.php
+++ Core/Model/Base/BankAccount.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2013-2020 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2013-2021 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -110,7 +110,7 @@ abstract class BankAccount extends ModelClass
     public function test()
     {
         if (!empty($this->codcuenta) && false === \is_numeric($this->codcuenta)) {
-            $this->toolBox()->i18nLog()->error('invalid-number');
+            $this->toolBox()->i18nLog()->error('invalid-number', ['%number%' => $this->codcuenta]);
             return false;
         }
 
