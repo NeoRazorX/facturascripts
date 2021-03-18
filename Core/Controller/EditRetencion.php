d6668f0e4288d499e2796d0680684c0f77eb5b58
Yopli2k <yopli2000@gmail.com>
2021-03-18 11:24:45 +0100

    Add customer and supplier tabs

--- Core/Controller/EditRetencion.php
+++ Core/Controller/EditRetencion.php
@@ -18,6 +18,7 @@
  */
 namespace FacturaScripts\Core\Controller;
 
+use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
 use FacturaScripts\Core\Lib\ExtendedController\EditController;
 
 /**
@@ -30,8 +31,24 @@ class EditRetencion extends EditController
 {
 
     /**
+     * Const for additionals views
+     */
+    private const VIEW_CUSTOMER = 'ListCliente';
+    private const VIEW_SUPPLIER = 'ListProveedor';
+
+    /**
+     * Create the view to display.
+     */
+    protected function createViews()
+    {
+        parent::createViews();
+        $this->createViewAuxiliar(self::VIEW_CUSTOMER, 'Cliente', 'customers');
+        $this->createViewAuxiliar(self::VIEW_SUPPLIER, 'Proveedor', 'suppliers');
+    }
+
+    /**
      * Returns the model name.
-     * 
+     *
      * @return string
      */
     public function getModelClassName()
@@ -51,5 +68,35 @@ class EditRetencion extends EditController
         $data['title'] = 'retention';
         $data['icon'] = 'fas fa-plus-square';
         return $data;
+    }
+
+    /**
+     * Loads the data to display.
+     *
+     * @param string   $viewName
+     * @param BaseView $view
+     */
+    protected function loadData($viewName, $view)
+    {
+        switch ($viewName) {
+            case self::VIEW_SUPPLIER:
+            case self::VIEW_CUSTOMER:
+                $mvn = $this->getMainViewName();
+                $code = $this->getViewModelValue($mvn, 'codretencion');
+                $where = [ new DataBaseWhere('codretencion', $code) ];
+                $view->loadData('', $where);
+                break;
+
+            default:
+                parent::loadData($viewName, $view);
+                break;
+        }
+    }
+
+    private function createViewAuxiliar($viewName, $model, $label)
+    {
+        $this->addListView($viewName, $model, $label, 'fas fa-users');
+        // $this->setSettings($viewName, 'btnNew', false);
+        // $this->setSettings($viewName, 'btnDelete', false);
     }
 }
