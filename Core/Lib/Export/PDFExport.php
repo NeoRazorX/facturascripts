c0d43a6754b997278a391b48aac4bdb9f0e8bcf4
Carlos García Gómez <neorazorx@gmail.com>
2021-04-21 20:59:20 +0200

    Fixed bug selecting company when exporting models to PDF.

--- Core/Lib/Export/PDFExport.php
+++ Core/Lib/Export/PDFExport.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2021 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -122,7 +122,8 @@ class PDFExport extends PDFDocument
     public function addModelPage($model, $columns, $title = ''): bool
     {
         $this->newPage();
-        $this->insertHeader();
+        $idempresa = isset($model->idempresa) ? $model->idempresa : null;
+        $this->insertHeader($idempresa);
 
         $tableCols = [];
         $tableColsTitle = [];
@@ -131,7 +132,7 @@ class PDFExport extends PDFDocument
             'showHeadings' => 0,
             'shaded' => 0,
             'lineCol' => [1, 1, 1],
-            'cols' => [],
+            'cols' => []
         ];
 
         /// Get the columns
