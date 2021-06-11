1e803dbf0503712034e8d66a9afd210a2648cc61
Carlos García Gómez <neorazorx@gmail.com>
2021-06-11 18:09:27 +0200

    - Do not break page on invoice PDFs when final text has more than 400 characters. - Updated version.

--- Core/Base/PluginManager.php
+++ Core/Base/PluginManager.php
@@ -31,7 +31,7 @@ final class PluginManager
     /**
      * FacturaScripts core version.
      */
-    const CORE_VERSION = 2021.24;
+    const CORE_VERSION = 2021.28;
 
     /**
      * Path to list plugins on file.
