6f3ca3894e3dea55989d06c4770be37379cc5dde
athosonline <info@athosonline.com>
2021-09-03 08:49:11 +0200

    fix wrong changes

--- Core/App/AppRouter.php
+++ Core/App/AppRouter.php
@@ -16,11 +16,9 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 namespace FacturaScripts\Core\App;
 
 use FacturaScripts\Core\Base\MyFilesToken;
-use FacturaScripts\Core\Base\PluginManager;
 
 /**
  * Description of AppRouter
@@ -33,7 +31,7 @@ final class AppRouter
     /**
      * Path to list of routes stored on file.
      */
-    const ROUTE_LIST_FILE = FS_FOLDER . DIRECTORY_SEPARATOR . 'MyFiles' . DIRECTORY_SEPARATOR . 'routes.json';
+    const ROUTE_LIST_FILE = \FS_FOLDER . DIRECTORY_SEPARATOR . 'MyFiles' . DIRECTORY_SEPARATOR . 'routes.json';
 
     /**
      * List of routes.
@@ -47,8 +45,8 @@ final class AppRouter
      */
     public function __construct()
     {
-        if (false === defined('FS_ROUTE')) {
-            define('FS_ROUTE', '');
+        if (false === \defined('FS_ROUTE')) {
+            \define('FS_ROUTE', '');
         }
 
         $this->routes = $this->loadFromFile();
@@ -64,14 +62,14 @@ final class AppRouter
     }
 
     /**
-     * Return the specific App controller for any kind of petition.
+     * Return the especific App controller for any kind of petition.
      *
      * @return App
      */
     public function getApp()
     {
         $uri = $this->getUri();
-        if ('/api' === $uri || '/api/' === substr($uri, 0, 5)) {
+        if ('/api' === $uri || '/api/' === \substr($uri, 0, 5)) {
             return new AppAPI($uri);
         }
 
@@ -88,11 +86,11 @@ final class AppRouter
                 return $this->newAppController($uri, $data['controller']);
             }
 
-            if ('*' !== substr($key, -1)) {
+            if ('*' !== \substr($key, -1)) {
                 continue;
             }
 
-            if (0 === strncmp($uri, $key, strlen($key) - 1)) {
+            if (0 === \strncmp($uri, $key, \strlen($key) - 1)) {
                 return $this->newAppController($uri, $data['controller']);
             }
         }
@@ -108,38 +106,36 @@ final class AppRouter
     public function getFile(): bool
     {
         $uri = $this->getUri();
-        $filePath = FS_FOLDER . urldecode($uri);
+        $filePath = \FS_FOLDER . $uri;
 
         /// favicon.ico
         if ('/favicon.ico' == $uri) {
-            $filePath = FS_FOLDER . '/Dinamic/Assets/Images/favicon.ico';
-            header('Content-Type: ' . $this->getMime($filePath));
-            readfile($filePath);
+            $filePath = \FS_FOLDER . '/Dinamic/Assets/Images/favicon.ico';
+            \header('Content-Type: ' . $this->getMime($filePath));
+            \readfile($filePath);
             return true;
         }
 
         /// Not a file? Not a safe file?
-        if (false === is_file($filePath) || false === $this->isFileSafe($filePath)) {
+        if (false === \is_file($filePath) || false === $this->isFileSafe($filePath)) {
             return false;
         }
 
         /// Allowed folder?
         $allowedFolders = ['node_modules', 'vendor', 'Dinamic', 'Core', 'Plugins', 'MyFiles/Public'];
         foreach ($allowedFolders as $folder) {
-            if ('/' . $folder === substr($uri, 0, 1 + strlen($folder))) {
-                header('Content-Type: ' . $this->getMime($filePath));
-                readfile($filePath);
+            if ('/' . $folder === \substr($uri, 0, 1 + \strlen($folder))) {
+                \header('Content-Type: ' . $this->getMime($filePath));
+                \readfile($filePath);
                 return true;
             }
         }
 
         /// MyFiles and token?
-        $token = filter_input(INPUT_GET, 'myft');
-        $fixedFilePath = substr(urldecode($uri), 1);
-        if ('/MyFiles/' === substr($uri, 0, 9) && $token && MyFilesToken::validate($fixedFilePath, $token)) {
-            header('Content-Type: ' . $this->getMime($filePath));
-            ob_end_flush(); /// disables buffer storage
-            readfile($filePath);
+        $token = \filter_input(INPUT_GET, 'myft');
+        if ('/MyFiles/' === \substr($uri, 0, 9) && $token && MyFilesToken::validate(\substr($uri, 1), $token)) {
+            \header('Content-Type: ' . $this->getMime($filePath));
+            \readfile($filePath);
             return true;
         }
 
@@ -147,19 +143,20 @@ final class AppRouter
     }
 
     /**
+     * 
      * @param string $filePath
      *
      * @return bool
      */
     public static function isFileSafe(string $filePath): bool
     {
-        $parts = explode('.', $filePath);
+        $parts = \explode('.', $filePath);
         $safe = [
             'avi', 'css', 'csv', 'eot', 'gif', 'gz', 'ico', 'jpeg', 'jpg', 'js',
             'json', 'map', 'mkv', 'mp4', 'ogg', 'pdf', 'png', 'sql', 'svg',
             'ttf', 'webm', 'woff', 'woff2', 'xls', 'xlsx', 'xml', 'xsig', 'zip'
         ];
-        return empty($parts) || in_array(end($parts), $safe, true);
+        return \count($parts) > 1 ? \in_array(\end($parts), $safe, true) : true;
     }
 
     /**
@@ -189,12 +186,12 @@ final class AppRouter
     }
 
     /**
-     * Deploy all dynamic files.
+     * Deploy all dinamic files.
      */
     private function deploy()
     {
-        if (false === file_exists(FS_FOLDER . DIRECTORY_SEPARATOR . 'Dinamic')) {
-            $pluginManager = new PluginManager();
+        if (false === \file_exists(\FS_FOLDER . \DIRECTORY_SEPARATOR . 'Dinamic')) {
+            $pluginManager = new \FacturaScripts\Core\Base\PluginManager();
             $pluginManager->deploy();
         }
     }
@@ -206,17 +203,17 @@ final class AppRouter
      *
      * @return string
      */
-    private function getMime(string $filePath): string
+    private function getMime(string $filePath)
     {
-        if ('.css' === substr($filePath, -4)) {
+        if ('.css' === \substr($filePath, -4)) {
             return 'text/css';
         }
 
-        if ('.js' === substr($filePath, -3)) {
+        if ('.js' === \substr($filePath, -3)) {
             return 'application/javascript';
         }
 
-        return mime_content_type($filePath);
+        return \mime_content_type($filePath);
     }
 
     /**
@@ -226,11 +223,11 @@ final class AppRouter
      */
     private function getUri()
     {
-        $uri = filter_input(INPUT_SERVER, 'REQUEST_URI');
-        $uri2 = is_null($uri) ? filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL) : $uri;
-        $uriArray = explode('?', $uri2);
+        $uri = \filter_input(\INPUT_SERVER, 'REQUEST_URI');
+        $uri2 = \is_null($uri) ? \filter_var($_SERVER['REQUEST_URI'], \FILTER_SANITIZE_URL) : $uri;
+        $uriArray = \explode('?', $uri2);
 
-        return substr($uriArray[0], strlen(FS_ROUTE));
+        return \substr($uriArray[0], \strlen(FS_ROUTE));
     }
 
     /**
@@ -240,10 +237,10 @@ final class AppRouter
      */
     private function loadFromFile(): array
     {
-        if (file_exists(self::ROUTE_LIST_FILE)) {
-            $content = file_get_contents(self::ROUTE_LIST_FILE);
+        if (\file_exists(self::ROUTE_LIST_FILE)) {
+            $content = \file_get_contents(self::ROUTE_LIST_FILE);
             if ($content !== false) {
-                return json_decode($content, true);
+                return \json_decode($content, true);
             }
         }
 
@@ -251,7 +248,7 @@ final class AppRouter
     }
 
     /**
-     *
+     * 
      * @param string $uri
      * @param string $pageName
      *
@@ -259,17 +256,17 @@ final class AppRouter
      */
     private function newAppController(string $uri, string $pageName = '')
     {
-        return FS_DEBUG ? new AppDebugController($uri, $pageName) : new AppController($uri, $pageName);
+        return \FS_DEBUG ? new AppDebugController($uri, $pageName) : new AppController($uri, $pageName);
     }
 
     /**
      * Save the routes in a file.
      *
-     * @return void
+     * @return bool
      */
-    private function save(): void
+    private function save(): bool
     {
-        $content = json_encode($this->routes);
-        file_put_contents(self::ROUTE_LIST_FILE, $content);
+        $content = \json_encode($this->routes);
+        return \file_put_contents(self::ROUTE_LIST_FILE, $content) !== false;
     }
 }
