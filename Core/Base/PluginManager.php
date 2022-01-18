7df3858daec837caa1e4998304b5ed339559fd30
Carlos Garcia Gomez <neorazorx@gmail.com>
2022-01-18 11:07:57 +0100

    Pequeñas correcciones.

--- Core/Base/PluginManager.php
+++ Core/Base/PluginManager.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2021 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -32,7 +32,7 @@ final class PluginManager
     /**
      * FacturaScripts core version.
      */
-    const CORE_VERSION = 2021.60;
+    const CORE_VERSION = 2021.61;
 
     /**
      * Path to list plugins on file.
