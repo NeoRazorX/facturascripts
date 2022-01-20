5616679a671b5ffd4c33ce8adb49b2585565c81f
Carlos Garcia Gomez <neorazorx@gmail.com>
2022-01-20 10:55:54 +0100

    - Añadida retención IRPF 7% para España. - Los formatos de impresión nuevos tienen ahora autoaplicar a true.

--- Core/Base/PluginManager.php
+++ Core/Base/PluginManager.php
@@ -32,7 +32,7 @@ final class PluginManager
     /**
      * FacturaScripts core version.
      */
-    const CORE_VERSION = 2021.61;
+    const CORE_VERSION = 2021.62;
 
     /**
      * Path to list plugins on file.
