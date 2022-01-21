ba6e9a0e3a66fb6396fd1dbfa50ef54e1aca8ae5
Carlos Garcia Gomez <neorazorx@gmail.com>
2022-01-21 23:38:18 +0100

    Actualizada la versión.

--- Core/Base/PluginManager.php
+++ Core/Base/PluginManager.php
@@ -32,7 +32,7 @@ final class PluginManager
     /**
      * FacturaScripts core version.
      */
-    const CORE_VERSION = 2021.62;
+    const CORE_VERSION = 2021.68;
 
     /**
      * Path to list plugins on file.
