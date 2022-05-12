68a6b302fe57612b8df975aecf8705873a827c6a
Daniel Fernández <daniel89fg@gmail.com>
2022-05-12 15:54:19 +0200

    Revert "Tarea #948 - Filtras los productos en compras y ventas para mostrar el listado correcto al buscar."
    This reverts commit 2755ffd49ed9b97dd2eab6b288c538de4ab30236.

--- Core/Base/AjaxForms/PurchasesController.php
+++ Core/Base/AjaxForms/PurchasesController.php
@@ -20,7 +20,6 @@
 namespace FacturaScripts\Core\Base\AjaxForms;
 
 use FacturaScripts\Core\Base\Calculator;
-use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
 use FacturaScripts\Core\DataSrc\Series;
 use FacturaScripts\Core\Lib\ExtendedController\BaseView;
 use FacturaScripts\Core\Lib\ExtendedController\DocFilesTrait;
@@ -101,11 +100,7 @@ abstract class PurchasesController extends PanelController
         $list = [];
         $variante = new Variante();
         $query = (string)$this->request->get('term');
-        $where = [
-            new DataBaseWhere('p.bloqueado', 0),
-            new DataBaseWhere('p.secompra', 1)
-        ];
-        foreach ($variante->codeModelSearch($query, 'referencia', $where) as $value) {
+        foreach ($variante->codeModelSearch($query, 'referencia') as $value) {
             $list[] = [
                 'key' => $this->toolBox()->utils()->fixHtml($value->code),
                 'value' => $this->toolBox()->utils()->fixHtml($value->description)
--- Core/Model/Variante.php
+++ Core/Model/Variante.php
@@ -19,7 +19,6 @@
 
 namespace FacturaScripts\Core\Model;
 
-use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
 use FacturaScripts\Dinamic\Model\AtributoValor as DinAtributoValor;
 use FacturaScripts\Dinamic\Model\Producto as DinProducto;
 use FacturaScripts\Dinamic\Model\Stock as DinStock;
@@ -133,13 +132,12 @@ class Variante extends Base\ModelClass
         $field = empty($fieldCode) ? $this->primaryColumn() : $fieldCode;
         $find = $this->toolBox()->utils()->noHtml(\mb_strtolower($query, 'UTF8'));
 
-        $sql = "SELECT p.bloqueado, v." . $field . " AS code, p.descripcion AS description, v.idatributovalor1, v.idatributovalor2, v.idatributovalor3, v.idatributovalor4, p.secompra, p.sevende"
+        $sql = "SELECT v." . $field . " AS code, p.descripcion AS description, v.idatributovalor1, v.idatributovalor2, v.idatributovalor3, v.idatributovalor4"
             . " FROM " . static::tableName() . " v"
             . " LEFT JOIN " . DinProducto::tableName() . " p ON v.idproducto = p.idproducto"
-            . DataBaseWhere::getSQLWhere($where)
-            . " AND (LOWER(v.referencia) LIKE '" . $find . "%'"
+            . " WHERE LOWER(v.referencia) LIKE '" . $find . "%'"
             . " OR LOWER(v.codbarras) = '" . $find . "'"
-            . " OR LOWER(p.descripcion) LIKE '%" . $find . "%')"
+            . " OR LOWER(p.descripcion) LIKE '%" . $find . "%'"
             . " ORDER BY v." . $field . " ASC";
 
         foreach (self::$dataBase->selectLimit($sql, CodeModel::ALL_LIMIT) as $data) {
