714bebf4c35e3eedda138f5ee912a8031bc8b1ab
Carlos Garcia Gomez <neorazorx@gmail.com>
2022-05-12 20:14:56 +0200

    Fixed string comparison to avoid automatic cast during database password check. ------ Corregida la comparación de cadenas para evitar cast automático durante la comprobación de la contraseña de la basew de datos.

--- Core/App/AppController.php
+++ Core/App/AppController.php
@@ -211,9 +211,9 @@ class AppController extends App
         $pass2 = $this->request->request->get('fsNewPasswd2');
 
         if ($pass != $pass2) {
-            ToolBox::i18nLog()->warning('different-passwords', ['%userNick%' => $nick]);
+            ToolBox::i18nLog()->warning('different-passwords', ['%userNick%' => htmlspecialchars($nick)]);
             return;
-        } elseif ($user->loadFromCode($nick) && $this->request->request->get('fsDbPasswd') == FS_DB_PASS) {
+        } elseif ($user->loadFromCode($nick) && $this->request->request->get('fsDbPasswd') === FS_DB_PASS) {
             $user->setPassword($pass);
             $user->save();
             ToolBox::i18nLog()->notice('record-updated-correctly');
