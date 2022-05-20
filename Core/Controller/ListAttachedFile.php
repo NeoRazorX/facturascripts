991b8d5c857330493f6186ea47d0c999a793fcd8
Carlos Garcia Gomez <neorazorx@gmail.com>
2022-05-20 11:14:13 +0200

    Ahora se pueden descargar en zip los archivos seleccionados de la biblioteca.

--- Core/Controller/ListAttachedFile.php
+++ Core/Controller/ListAttachedFile.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2018-2020 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2018-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -16,9 +16,11 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 namespace FacturaScripts\Core\Controller;
 
 use FacturaScripts\Core\Lib\ExtendedController\ListController;
+use ZipArchive;
 
 /**
  * Controller to list the items in the AttachedFile model
@@ -29,12 +31,7 @@ use FacturaScripts\Core\Lib\ExtendedController\ListController;
 class ListAttachedFile extends ListController
 {
 
-    /**
-     * Returns basic page attributes
-     *
-     * @return array
-     */
-    public function getPageData()
+    public function getPageData(): array
     {
         $data = parent::getPageData();
         $data['menu'] = 'admin';
@@ -48,7 +45,11 @@ class ListAttachedFile extends ListController
      */
     protected function createViews()
     {
-        $viewName = 'ListAttachedFile';
+        $this->createViewsFiles();
+    }
+
+    protected function createViewsFiles(string $viewName = 'ListAttachedFile')
+    {
         $this->addView($viewName, 'AttachedFile', 'attached-files', 'fas fa-paperclip');
         $this->addSearchFields($viewName, ['filename', 'mimetype']);
         $this->addOrderBy($viewName, ['idfile'], 'code');
@@ -56,10 +57,68 @@ class ListAttachedFile extends ListController
         $this->addOrderBy($viewName, ['filename'], 'file-name');
         $this->addOrderBy($viewName, ['size'], 'size');
 
-        /// filters
+        // filters
         $this->addFilterPeriod($viewName, 'date', 'period', 'date');
 
         $types = $this->codeModel->all('attached_files', 'mimetype', 'mimetype');
         $this->addFilterSelect($viewName, 'mimetype', 'type', 'mimetype', $types);
+
+        // buttons
+        $this->addButton($viewName, [
+            'action' => 'download',
+            'icon' => 'fas fa-download',
+            'label' => 'download'
+        ]);
+    }
+
+    protected function downloadAction(): bool
+    {
+        $codes = $this->request->request->get('code');
+        if (empty($codes)) {
+            self::toolBox()::i18nLog()->warning('no-selected-item');
+            return true;
+        }
+
+        // creamos el zip
+        $zip = new ZipArchive();
+        $filename = 'attached-files.zip';
+        $filepath = FS_FOLDER . '/MyFiles/' . $filename;
+        if ($zip->open($filepath, ZipArchive::CREATE) !== true) {
+            self::toolBox()->i18nLog()->warning('error-creating-zip-file');
+            return true;
+        }
+
+        // añadimos los archivos
+        $model = $this->views[$this->active]->model;
+        foreach ($codes as $code) {
+            $file = $model->get($code);
+            if ($file) {
+                $zip->addFile($file->getFullPath(), $file->idfile . '_' . $file->filename);
+            }
+        }
+
+        // cerramos el zip
+        $zip->close();
+
+        // descargamos el zip
+        header('Content-Type: application/zip');
+        header('Content-Disposition: attachment; filename="' . $filename . '"');
+        header('Content-Length: ' . filesize($filepath));
+        readfile($filepath);
+
+        // borramos el zip
+        unlink($filepath);
+
+        $this->setTemplate(false);
+        return false;
+    }
+
+    protected function execPreviousAction($action)
+    {
+        if ($action === 'download') {
+            return $this->downloadAction();
+        }
+
+        return parent::execPreviousAction($action);
     }
 }
