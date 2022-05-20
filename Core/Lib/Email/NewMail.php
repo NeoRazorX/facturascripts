1d97042df4e873274c8e4bb6d4fdc371d2f88218
Carlos Garcia Gomez <neorazorx@gmail.com>
2022-05-20 17:07:20 +0200

    Ahora el botón de test de la configuración de email solamente aparece cuando se selecciona smtp, ya que para el resto no hay implementada opción de test.

--- Core/Lib/Email/NewMail.php
+++ Core/Lib/Email/NewMail.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2019-2021 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2019-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -362,7 +362,7 @@ class NewMail
                 return $this->mail->smtpConnect($this->smtpOptions());
 
             default:
-                $this->toolBox()->i18nLog()->warning('not-implemented');
+                $this->toolBox()->i18nLog()->warning('test-' . $this->mail->Mailer . '-not-implemented');
                 return false;
         }
     }
