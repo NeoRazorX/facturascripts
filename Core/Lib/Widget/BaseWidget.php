2aa9a8cbb2437d1d8729014b12adf73d9f60519a
Carlos Garcia Gomez <neorazorx@gmail.com>
2022-07-23 18:32:34 +0200

    Pequeñas correcciones al tabindex.

--- Core/Lib/Widget/BaseWidget.php
+++ Core/Lib/Widget/BaseWidget.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2021 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -65,7 +65,6 @@ class BaseWidget extends VisualItem
     public $required;
 
     /**
-     *
      * @var int
      */
     public $tabindex;
@@ -91,7 +90,7 @@ class BaseWidget extends VisualItem
         $this->icon = $data['icon'] ?? '';
         $this->onclick = $data['onclick'] ?? '';
         $this->readonly = $data['readonly'] ?? 'false';
-        $this->tabindex = $data['tabindex'] ?? '';
+        $this->tabindex = intval($data['tabindex'] ?? '-1');
         $this->required = isset($data['required']) && strtolower($data['required']) === 'true';
         $this->type = $data['type'];
         $this->loadOptions($data['children']);
@@ -241,7 +240,7 @@ class BaseWidget extends VisualItem
         $params = $this->required ? ' required=""' : '';
         $params .= $this->readonly() ? ' readonly=""' : '';
         $params .= $this->autocomplete ? '' : ' autocomplete="off"';
-        $params .= $this->tabindex ? ' tabindex="' . $this->tabindex . '"' : '';
+        $params .= $this->tabindex >= 0 ? ' tabindex="' . $this->tabindex . '"' : '';
 
         return $params;
     }
