ffa6c1dc53117b188a1398a63e1da91e240e4a4a
Carlos Garcia Gomez <neorazorx@gmail.com>
2022-08-29 10:51:45 +0200

    Solucionado bug al intentar traducir null.

--- Core/Base/Translator.php
+++ Core/Base/Translator.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -175,12 +175,12 @@ class Translator
     /**
      * Translate the text into the default language.
      *
-     * @param string $txt
+     * @param ?string $txt
      * @param array $parameters
      *
      * @return string
      */
-    public function trans(string $txt, array $parameters = []): string
+    public function trans(?string $txt, array $parameters = []): string
     {
         return empty($txt) ? '' : $this->customTrans($this->currentLang, $txt, $parameters);
     }
