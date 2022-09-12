730c2578407698d3c26b7e04e967273f24eb6f28
Carlos Garcia Gomez <neorazorx@gmail.com>
2022-09-12 16:43:29 +0200

    Al generar los balances de sumas y saldos, ahora se marca por defecto el ignorar los asientos de cierre y de apertura.

--- Core/Controller/EditReportAmount.php
+++ Core/Controller/EditReportAmount.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2020-2021 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2020-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -32,21 +32,11 @@ use FacturaScripts\Dinamic\Model\ReportAmount;
 class EditReportAmount extends EditReportAccounting
 {
 
-    /**
-     * Returns the class name of the model to use in the editView.
-     *
-     * @return string
-     */
-    public function getModelClassName()
+    public function getModelClassName(): string
     {
         return 'ReportAmount';
     }
 
-    /**
-     * Return the basic data for this page.
-     *
-     * @return array
-     */
     public function getPageData(): array
     {
         $data = parent::getPageData();
