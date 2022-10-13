73ca6edf7112b798626e9947859bc5afd926e1cd
Carlos Garcia <neorazorx@gmail.com>
2022-10-13 19:34:44 +0200

    Eliminados archivos innecesarios de dinamic.

--- Dinamic/Lib/Accounting/Ledger.php
+++
@@ -1,9 +0,0 @@
-<?php namespace FacturaScripts\Dinamic\Lib\Accounting;
-
-/**
- * Class created by Core/Base/PluginManager
- * @author FacturaScripts <carlos@facturascripts.com>
- */
-class Ledger extends \FacturaScripts\Core\Lib\Accounting\Ledger
-{
-}
