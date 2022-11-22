04498713d9df234d70d6391e474ce02f352688d0
Daniel Fernández <daniel89fg@gmail.com>
2022-11-22 11:50:40 +0100

    Tarea #1842 - Corregido bug de multireqtoken con la nueva clase html.

--- Core/Lib/MultiRequestProtection.php
+++ Core/Lib/MultiRequestProtection.php
@@ -37,12 +37,12 @@ class MultiRequestProtection
     /**
      * @var string
      */
-    protected $seed;
+    protected static $seed;
 
     public function __construct()
     {
         // something unique in each installation
-        $this->seed = PHP_VERSION . __FILE__ . FS_DB_NAME . FS_DB_PASS;
+        self::$seed = PHP_VERSION . __FILE__ . FS_DB_NAME . FS_DB_PASS;
     }
 
     /**
@@ -50,7 +50,7 @@ class MultiRequestProtection
      */
     public function addSeed(string $seed)
     {
-        $this->seed .= $seed;
+        self::$seed .= $seed;
     }
 
     /**
@@ -61,10 +61,10 @@ class MultiRequestProtection
     public function newToken(): string
     {
         // something that changes every hour
-        $num = intval(date('YmdH')) + strlen($this->seed);
+        $num = intval(date('YmdH')) + strlen(self::$seed);
 
         // combine and generate the token
-        $value = $this->seed . $num;
+        $value = self::$seed . $num;
         return sha1($value) . '|' . $this->getRandomStr();
     }
 
@@ -101,12 +101,12 @@ class MultiRequestProtection
         }
 
         // check all valid tokens roots
-        $num = intval(date('YmdH')) + strlen($this->seed);
-        $valid = [sha1($this->seed . $num)];
+        $num = intval(date('YmdH')) + strlen(self::$seed);
+        $valid = [sha1(self::$seed . $num)];
         for ($hour = 1; $hour <= self::MAX_TOKEN_AGE; $hour++) {
             $time = strtotime('-' . $hour . ' hours');
-            $altNum = intval(date('YmdH', $time)) + strlen($this->seed);
-            $valid[] = sha1($this->seed . $altNum);
+            $altNum = intval(date('YmdH', $time)) + strlen(self::$seed);
+            $valid[] = sha1(self::$seed . $altNum);
         }
 
         return in_array($tokenParts[0], $valid);
