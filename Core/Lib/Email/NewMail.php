7edff818563fc5e1bc6295b3341a347c59138289
Carlos Garcia Gomez <neorazorx@gmail.com>
2023-01-10 11:53:22 +0100

    Reemplazado el mensaje de error cuando no se puede conectar al servidor de correo.

--- Core/Lib/Email/NewMail.php
+++ Core/Lib/Email/NewMail.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2019-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2019-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -292,7 +292,7 @@ class NewMail
         $this->mail->msgHTML($this->renderHTML());
 
         if ('smtp' === $this->mail->Mailer && false === $this->mail->smtpConnect($this->smtpOptions())) {
-            $this->toolBox()->i18nLog()->error('error', ['%error%' => $this->mail->ErrorInfo]);
+            $this->toolBox()->i18nLog()->warning('mail-server-error');
             return false;
         }
 
