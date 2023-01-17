32f6377d41fd0455260fbe62c4a4e2a1b90604da
Carlos Garcia Gomez <neorazorx@gmail.com>
2023-01-17 18:29:53 +0100

    Pequeñas mejoras al código.

--- Core/Model/FacturaProveedor.php
+++ Core/Model/FacturaProveedor.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2013-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2013-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -176,7 +176,8 @@ class FacturaProveedor extends Base\PurchaseDocument
                     . ' SET codigofactura = ' . self::$dataBase->var2str($codigo)
                     . ' WHERE idfactura = ' . self::$dataBase->var2str($row['idfactura']) . ';'
                     . ' UPDATE asientos'
-                    . ' SET documento = ' . self::$dataBase->var2str($codigo) . ', concepto = REPLACE(concepto, ' . self::$dataBase->var2str($row['codigo']) . ', ' . self::$dataBase->var2str($codigo) . ')'
+                    . ' SET documento = ' . self::$dataBase->var2str($codigo) . ', concepto = REPLACE(concepto, '
+                    . self::$dataBase->var2str($row['codigo']) . ', ' . self::$dataBase->var2str($codigo) . ')'
                     . ' WHERE documento = ' . self::$dataBase->var2str($row['codigo']) . ';';
             }
             ++$number;
