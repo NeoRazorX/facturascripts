cd48f3654d84d8f63b9dac5ce0369102d1a1363d
Carlos Garcia Gomez <neorazorx@gmail.com>
2023-01-23 12:30:12 +0100

    Corregido error al llamar a exists del page.

--- Core/Controller/EditUser.php
+++ Core/Controller/EditUser.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -31,12 +31,11 @@ use Symfony\Component\HttpFoundation\Cookie;
 /**
  * Controller to edit a single item from the User model
  *
- * @author Carlos García Gómez  <carlos@facturascripts.com>
+ * @author Carlos García Gómez           <carlos@facturascripts.com>
  * @author Jose Antonio Cuello Principal <yopli2000@gmail.com>
  */
 class EditUser extends EditController
 {
-
     public function getImageUrl(): string
     {
         $mvn = $this->getMainViewName();
@@ -183,7 +182,7 @@ class EditUser extends EditController
         foreach ($roleUserModel->all([new DataBaseWhere('nick', $user->nick)]) as $roleUser) {
             foreach ($roleUser->getRoleAccess() as $roleAccess) {
                 $page = $roleAccess->getPage();
-                if (false === $page->exist() || false === $page->showonmenu) {
+                if (false === $page->exists() || false === $page->showonmenu) {
                     continue;
                 }
 
