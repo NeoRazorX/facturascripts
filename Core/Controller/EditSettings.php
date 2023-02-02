821f78ea32cd60924ea4fbbec173b0b2d22f7a4a
Carlos Garcia Gomez <neorazorx@gmail.com>
2023-02-02 11:49:18 +0100

    Añadidos enlaces faltantes a los selectores de impuestos, recargo y retenciones.

--- Core/Controller/EditSettings.php
+++ Core/Controller/EditSettings.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -36,7 +36,6 @@ use FacturaScripts\Dinamic\Model\Impuesto;
  */
 class EditSettings extends PanelController
 {
-
     const KEY_SETTINGS = 'Settings';
 
     public function getPageData(): array
