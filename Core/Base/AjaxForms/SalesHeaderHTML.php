b98060e850fef1f8084c1822cfefc9f8c0b20740
Daniel Fernández <daniel89fg@gmail.com>
2023-03-08 12:40:37 +0100

    Tarea #195 - Mostrar datalist en los campos de ciudad y provincia.

--- Core/Base/AjaxForms/SalesHeaderHTML.php
+++ Core/Base/AjaxForms/SalesHeaderHTML.php
@@ -29,7 +29,9 @@ use FacturaScripts\Core\Model\Base\SalesDocument;
 use FacturaScripts\Core\Model\Cliente;
 use FacturaScripts\Core\Model\Contacto;
 use FacturaScripts\Core\Model\User;
+use FacturaScripts\Dinamic\Model\Ciudad;
 use FacturaScripts\Dinamic\Model\Pais;
+use FacturaScripts\Dinamic\Model\Provincia;
 
 /**
  * Description of SalesHeaderHTML
@@ -179,9 +181,38 @@ class SalesHeaderHTML
         $attributes = $model->editable && (empty($model->idcontactofact) || empty($model->direccion)) ?
             'name="' . $field . '" maxlength="' . $maxlength . '" autocomplete="off"' :
             'disabled=""';
+
         return '<div class="col-sm-' . $size . '">'
             . '<div class="form-group">' . $i18n->trans($label)
             . '<input type="text" ' . $attributes . ' value="' . $model->{$field} . '" class="form-control"/>'
+            . '</div>'
+            . '</div>';
+    }
+
+    private static function ciudad(Translator $i18n, SalesDocument $model, int $size, int $maxlength): string
+    {
+        $list = '';
+        $dataList = '';
+        $attributes = $model->editable && (empty($model->idcontactofact) || empty($model->direccion)) ?
+            'name="ciudad" maxlength="' . $maxlength . '" autocomplete="off"' :
+            'disabled=""';
+
+        if ($model->editable) {
+            // pre-cargamos listado de ciudades
+            $list = 'list="ciudades"';
+            $dataList = '<datalist id="ciudades">';
+
+            $ciudadModel = new Ciudad();
+            foreach ($ciudadModel->all([], ['ciudad' => 'ASC'], 0, 0) as $ciudad) {
+                $dataList .= '<option value="' . $ciudad->ciudad . '">' . $ciudad->ciudad . '</option>';
+            }
+            $dataList .= '</datalist>';
+        }
+
+        return '<div class="col-sm-' . $size . '">'
+            . '<div class="form-group">' . $i18n->trans('city')
+            . '<input type="text" ' . $attributes . ' value="' . $model->ciudad . '" ' . $list . ' class="form-control"/>'
+            . $dataList
             . '</div>'
             . '</div>';
     }
@@ -487,7 +518,7 @@ class SalesHeaderHTML
                 return self::cifnif($i18n, $model);
 
             case 'ciudad':
-                return self::addressField($i18n, $model, 'ciudad', 'city', 4, 100);
+                return self::ciudad($i18n, $model, 4, 100);
 
             case 'codagente':
                 return self::codagente($i18n, $model);
@@ -550,7 +581,7 @@ class SalesHeaderHTML
                 return self::numero2($i18n, $model);
 
             case 'provincia':
-                return self::addressField($i18n, $model, 'provincia', 'province', 6, 100);
+                return self::provincia($i18n, $model, 6, 100);
 
             case 'tasaconv':
                 return self::tasaconv($i18n, $model);
@@ -563,6 +594,34 @@ class SalesHeaderHTML
         }
 
         return null;
+    }
+
+    private static function provincia(Translator $i18n, SalesDocument $model, int $size, int $maxlength): string
+    {
+        $list = '';
+        $dataList = '';
+        $attributes = $model->editable && (empty($model->idcontactofact) || empty($model->direccion)) ?
+            'name="provincia" maxlength="' . $maxlength . '" autocomplete="off"' :
+            'disabled=""';
+
+        if ($model->editable) {
+            // pre-cargamos listado de provincias
+            $list = 'list="provincias"';
+            $dataList = '<datalist id="provincias">';
+
+            $provinciaModel = new Provincia();
+            foreach ($provinciaModel->all([], ['provincia' => 'ASC'], 0, 0) as $provincia) {
+                $dataList .= '<option value="' . $provincia->provincia . '">' . $provincia->provincia . '</option>';
+            }
+            $dataList .= '</datalist>';
+        }
+
+        return '<div class="col-sm-' . $size . '">'
+            . '<div class="form-group">' . $i18n->trans('province')
+            . '<input type="text" ' . $attributes . ' value="' . $model->provincia . '" ' . $list . ' class="form-control"/>'
+            . $dataList
+            . '</div>'
+            . '</div>';
     }
 
     private static function renderNewFields(Translator $i18n, SalesDocument $model): string
