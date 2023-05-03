5b1db2371ef4bb879452489a7dfdad93e6031854
Jorge-Prebac <info@prebac.com>
2023-05-03 09:06:11 +0200

    Tarea #2284
    Añadir filtro periodo sobre fecha de pago a los recibos de facturas (compra y venta). Para poder localizar rápidamente los pagados en unas fechas dadas.

--- Core/Controller/ListFacturaProveedor.php
+++ Core/Controller/ListFacturaProveedor.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -101,10 +101,11 @@ class ListFacturaProveedor extends ListBusinessDocument
         $this->addSearchFields($viewName, ['codigofactura', 'observaciones']);
 
         // filtros
-        $this->addFilterPeriod($viewName, 'period', 'expiration', 'vencimiento');
+        $this->addFilterPeriod($viewName, 'expiration', 'expiration', 'vencimiento');
         $this->addFilterAutocomplete($viewName, 'codproveedor', 'supplier', 'codproveedor', 'Proveedor');
         $this->addFilterNumber($viewName, 'min-total', 'amount', 'importe', '>=');
         $this->addFilterNumber($viewName, 'max-total', 'amount', 'importe', '<=');
+		$this->views[$viewName]->addFilterPeriod('payment-date', 'payment-date', 'fechapago');
 
         $currencies = Divisas::codeModel();
         if (count($currencies) > 2) {
