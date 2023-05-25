0f7f912e5cf214bac276ac67fff1867955deb09c
Carlos Garcia Gomez <neorazorx@gmail.com>
2023-05-25 12:09:49 +0200

    Corregido el proceso para copiar los archivos adjuntos al aprobar documentos.

--- Core/Lib/BusinessDocumentGenerator.php
+++ Core/Lib/BusinessDocumentGenerator.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2018-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2018-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -20,12 +20,12 @@
 namespace FacturaScripts\Core\Lib;
 
 use FacturaScripts\Core\Base\Calculator;
+use FacturaScripts\Core\Base\Database\DataBaseWhere;
 use FacturaScripts\Core\Base\ExtensionsTrait;
 use FacturaScripts\Core\Model\Base\BusinessDocument;
 use FacturaScripts\Core\Model\Base\BusinessDocumentLine;
-use FacturaScripts\Dinamic\Model\DocTransformation;
 use FacturaScripts\Dinamic\Model\AttachedFileRelation;
-use FacturaScripts\Core\Base\Database\DataBaseWhere;
+use FacturaScripts\Dinamic\Model\DocTransformation;
 
 /**
  * Description of BusinessDocumentGenerator
@@ -36,7 +36,6 @@ use FacturaScripts\Core\Base\Database\DataBaseWhere;
  */
 class BusinessDocumentGenerator
 {
-
     use ExtensionsTrait;
 
     /**
@@ -101,7 +100,7 @@ class BusinessDocumentGenerator
         foreach ($properties as $key => $value) {
             $newDoc->{$key} = $value;
         }
-   
+
         $protoLines = empty($lines) ? $prototype->getLines() : $lines;
         if ($newDoc->save() && $this->cloneLines($prototype, $newDoc, $protoLines, $quantity)) {
             // recalculate totals on new document
@@ -109,7 +108,6 @@ class BusinessDocumentGenerator
             if (Calculator::calculate($newDoc, $newLines, true)) {
                 // add to last doc list
                 $this->lastDocs[] = $newDoc;
-                $this->translateRelFiles($prototype);
                 return true;
             }
         }
@@ -187,58 +185,36 @@ class BusinessDocumentGenerator
             }
         }
 
+        // copy related files
+        $this->copyRelatedFiles($prototype, $newDoc);
+
         if (false === $this->pipeFalse('cloneLines', $prototype, $newDoc, $lines, $quantity)) {
             return false;
         }
 
         return true;
     }
-    /**
-     * Translate links of related files to new Documento
-     * @param  BusinessDocument $prototype
-     * @return bool
-     */
-    public function translateRelFiles($prototype): bool {
 
-        $docsRels = new AttachedFileRelation();
-        $docRelsNewDoc = new AttachedFileRelation();
-        $docsOfChildren = new AttachedFileRelation();
-
-        $originClassname = substr(strrchr(get_class($prototype), "\\"), 1);
-        $primaryField = $prototype->primaryColumn();
-
-        $childs = $prototype->childrenDocuments();
+    public function copyRelatedFiles(BusinessDocument $prototype, BusinessDocument $newDoc): bool
+    {
+        $relationModel = new AttachedFileRelation();
         $whereDocs = [
-            New DatabaseWhere('model', $originClassname)
-            , new DataBaseWhere('modelid', $prototype->{$primaryField})
+            new DatabaseWhere('model', $prototype->modelClassName()),
+            new DataBaseWhere('modelid', $prototype->primaryColumnValue())
         ];
-        $docsRels = $docsRels->all($whereDocs, ['id' => 'ASC']);
-
-        foreach ($docsRels as $doc) {
-            foreach ($childs as $child) {
-
-                $whereDocsChildren = [
-                    new DataBaseWhere('model', $originClassname)
-                    , new DataBaseWhere('modelid', $primaryField)
-                    , new DataBaseWhere('idfile', $doc->idfile)
-                ];
-                if ($docsOfChildren->cocunt($whereDocsChildren) == 0) {
-                    $modelName = substr(strrchr(get_class($child), "\\"), 1);
-                    $docRelsNewDoc->clear();
-                    $docRelsNewDoc->creationdate = $doc->creationdate;
-                    $docRelsNewDoc->idfile = $doc->idfile;
-                    $docRelsNewDoc->model = $modelName;
-                    $docRelsNewDoc->modelid = $child->{$child->primaryColumn()};
-                    $docRelsNewDoc->nick = $doc->nick;
-                    $docRelsNewDoc->observations = $doc->observations;
-                    $docRelsNewDoc->modelcode = $child->codigo;
-                    if (!$docRelsNewDoc->save()) {
-                        return false;
-                    }
-                }
+        foreach ($relationModel->all($whereDocs, ['id' => 'ASC']) as $relation) {
+            $newRelation = new AttachedFileRelation();
+            $newRelation->idfile = $relation->idfile;
+            $newRelation->model = $newDoc->modelClassName();
+            $newRelation->modelid = $newDoc->primaryColumnValue();
+            $newRelation->nick = $relation->nick;
+            $newRelation->observations = $relation->observations;
+            $newRelation->modelcode = $newDoc->codigo;
+            if (false === $newRelation->save()) {
+                return false;
             }
         }
+
         return true;
     }
-
 }
