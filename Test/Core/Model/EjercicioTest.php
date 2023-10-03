71a10ded77a8a451a746cedf2a6fd96a1ed86bab
Carlos Garcia Gomez <neorazorx@gmail.com>
2023-10-03 23:38:50 +0200

    Pequeñas mejoras al código del test de ejercicios.

--- Test/Core/Model/EjercicioTest.php
+++ Test/Core/Model/EjercicioTest.php
@@ -1,18 +1,31 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
+/**
+ * This file is part of FacturaScripts
+ * Copyright (C) 2023 Carlos Garcia Gomez <carlos@facturascripts.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace FacturaScripts\Test\Core\Model;
 
-use FacturaScripts\Core\Base\DataBase;
-use FacturaScripts\Core\Base\MiniLog;
 use FacturaScripts\Core\Model\Ejercicio;
 use FacturaScripts\Test\Traits\DefaultSettingsTrait;
 use FacturaScripts\Test\Traits\LogErrorsTrait;
 use FacturaScripts\Test\Traits\RandomDataTrait;
 use PHPUnit\Framework\TestCase;
 
-class EjercicioTest extends TestCase
+final class EjercicioTest extends TestCase
 {
     use LogErrorsTrait;
     use RandomDataTrait;
@@ -20,118 +33,111 @@ class EjercicioTest extends TestCase
 
     protected function setUp(): void
     {
-        $db = new DataBase();
-        $db->connect();
-
-        static::setDefaultSettings();
+        self::setDefaultSettings();
     }
 
-    //Comprobar que se puede crear un ejercicio (y que se crea abierto) y borrarlo.
-    public function testItCanCreateExercise()
+    // Comprobar que se puede crear un ejercicio (y que se crea abierto) y borrarlo.
+    public function testItCanCreateExercise(): void
     {
         // Lo creamos con fecha del próximo año porque al pasar los
         // test se instancia la clase 'Ejercicio' y se crea un ejercicio para este año automaticamente
         // por lo que da un error si se quiere crear otro ejercicio para este mismo año
         $nextYear = date('Y', strtotime(date('Y') . ' + 1 year'));
 
-        $codejercicio = 'test';
+        $codejercicio = 't001';
 
         $ejercicio = new Ejercicio();
         $ejercicio->codejercicio = $codejercicio;
         $ejercicio->nombre = 'exercise-test';
         $ejercicio->fechainicio = $nextYear . '-01-01';
         $ejercicio->fechafin = $nextYear . '-12-31';
-        self::assertTrue($ejercicio->save());
+        $this->assertTrue($ejercicio->save());
 
         // Obtenemos el ejercicio de la base de datos
         // y comprobamos que se crea abierto
         $ejercicio = new Ejercicio();
         $ejercicio->loadFromCode($codejercicio);
-        self::assertEquals(Ejercicio::EXERCISE_STATUS_OPEN, $ejercicio->estado);
+        $this->assertEquals(Ejercicio::EXERCISE_STATUS_OPEN, $ejercicio->estado);
 
         // Comprobamos que se elimina correctamente
-        self::assertTrue($ejercicio->delete());
+        $this->assertTrue($ejercicio->delete());
 
         // Obtenemos el ejercicio de la base de datos
         // y comprobamos que ya no existe
         $ejercicio = new Ejercicio();
-        self::assertFalse($ejercicio->loadFromCode($codejercicio));
+        $this->assertFalse($ejercicio->loadFromCode($codejercicio));
     }
 
-    //Comprobar que no se puede crear un ejercio con fecha de inicio posterior a la fecha de fin.
-    public function testItCanNotCreateExerciseWrongDate()
+    // Comprobar que no se puede crear un ejercicio con fecha de inicio posterior a la fecha de fin.
+    public function testItCanNotCreateExerciseWrongDate(): void
     {
-        MiniLog::clear();
-
         $ejercicio = new Ejercicio();
-        $ejercicio->codejercicio = 'test';
+        $ejercicio->codejercicio = 't002';
         $ejercicio->nombre = 'exercise-test';
         $ejercicio->fechainicio = '2033-06-16';
         $ejercicio->fechafin = '2033-06-15';
-        self::assertFalse($ejercicio->save());
-
-        $miniLog = MiniLog::read();
-        self::assertEquals('start-date-later-end-date', $miniLog[0]['message']);
+        $this->assertFalse($ejercicio->save());
     }
 
-    //Crear un ejercicio para 2099 y comprobar que si solicitamos un ejercicio para esa fecha, lo devuelve.
-    public function testItCanReturnExerciseFromDate()
+    // Crear un ejercicio para 2099 y comprobar que si solicitamos un ejercicio para esa fecha, lo devuelve.
+    public function testItCanReturnExerciseFromDate(): void
     {
         $idempresa = 1;
 
         $ejercicio = new Ejercicio();
         $ejercicio->idempresa = $idempresa;
-        $ejercicio->codejercicio = 'test';
+        $ejercicio->codejercicio = 't003';
         $ejercicio->nombre = 'exercise-test';
         $ejercicio->fechainicio = '2099-01-01';
         $ejercicio->fechafin = '2099-12-31';
-        self::assertTrue($ejercicio->save());
+        $this->assertTrue($ejercicio->save());
 
-        $ejercicio = new Ejercicio();
-        $ejercicio->idempresa = $idempresa;
-        self::assertTrue($ejercicio->loadFromDate('2099-06-15', true, false));
+        $ejercicio2 = new Ejercicio();
+        $ejercicio2->idempresa = $idempresa;
+        $this->assertTrue($ejercicio2->loadFromDate('2099-06-15', true, false));
+        $this->assertEquals($ejercicio->codejercicio, $ejercicio2->codejercicio);
 
         // Eliminamos
-        self::assertTrue($ejercicio->delete());
+        $this->assertTrue($ejercicio->delete());
     }
 
-    //Comprobar que se pueden crear dos ejercicios para la misma fecha pero distinta empresa.
-    public function testItCanCreateExercisesFromDifferentCompanies()
+    // Comprobar que se pueden crear dos ejercicios para la misma fecha pero distinta empresa.
+    public function testItCanCreateExercisesFromDifferentCompanies(): void
     {
-        $fechainicio = '2099-01-01';
-        $fechafin = '2099-12-31';
+        $inicio = '2099-01-01';
+        $fin = '2099-12-31';
 
         $empresa1 = $this->getRandomCompany();
-        self::assertTrue($empresa1->save());
+        $this->assertTrue($empresa1->save());
 
         $empresa2 = $this->getRandomCompany();
-        self::assertTrue($empresa2->save());
+        $this->assertTrue($empresa2->save());
 
         $ejercicioEmpresa1 = new Ejercicio();
         $ejercicioEmpresa1->idempresa = $empresa1->idempresa;
         $ejercicioEmpresa1->codejercicio = 'E-1';
         $ejercicioEmpresa1->nombre = 'exercise-test-1';
-        $ejercicioEmpresa1->fechainicio = $fechainicio;
-        $ejercicioEmpresa1->fechafin = $fechafin;
-        self::assertTrue($ejercicioEmpresa1->save());
+        $ejercicioEmpresa1->fechainicio = $inicio;
+        $ejercicioEmpresa1->fechafin = $fin;
+        $this->assertTrue($ejercicioEmpresa1->save());
 
         $ejercicioEmpresa2 = new Ejercicio();
         $ejercicioEmpresa2->idempresa = $empresa2->idempresa;
         $ejercicioEmpresa2->codejercicio = 'E-2';
         $ejercicioEmpresa2->nombre = 'exercise-test-2';
-        $ejercicioEmpresa2->fechainicio = $fechainicio;
-        $ejercicioEmpresa2->fechafin = $fechafin;
-        self::assertTrue($ejercicioEmpresa2->save());
+        $ejercicioEmpresa2->fechainicio = $inicio;
+        $ejercicioEmpresa2->fechafin = $fin;
+        $this->assertTrue($ejercicioEmpresa2->save());
 
         $ejercicio = new Ejercicio();
-        self::assertTrue($ejercicio->loadFromCode($ejercicioEmpresa1->codejercicio));
+        $this->assertTrue($ejercicio->loadFromCode($ejercicioEmpresa1->codejercicio));
 
         $ejercicio = new Ejercicio();
-        self::assertTrue($ejercicio->loadFromCode($ejercicioEmpresa2->codejercicio));
+        $this->assertTrue($ejercicio->loadFromCode($ejercicioEmpresa2->codejercicio));
 
         // Eliminamos
-        self::assertTrue($ejercicioEmpresa1->delete());
-        self::assertTrue($ejercicioEmpresa2->delete());
+        $this->assertTrue($ejercicioEmpresa1->delete());
+        $this->assertTrue($ejercicioEmpresa2->delete());
     }
 
     protected function tearDown(): void
