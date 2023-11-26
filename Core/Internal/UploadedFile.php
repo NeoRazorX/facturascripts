3dc765099c8f5481e4aa9482bd94d4df1e0ff73f
Carlos Garcia <neorazorx@gmail.com>
2023-11-26 19:02:20 +0100

    Añadidas funciones a UploadedFile para dar compatibilidad con symfony.

--- Core/Internal/UploadedFile.php
+++ Core/Internal/UploadedFile.php
@@ -50,9 +50,58 @@ final class UploadedFile
         return pathinfo($this->name, PATHINFO_EXTENSION);
     }
 
-    public function isUploadedFile(): bool
+    public function getClientOriginalName(): string
+    {
+        return $this->name;
+    }
+
+    public function getErrorMessage(): string
+    {
+        switch ($this->error) {
+            case UPLOAD_ERR_INI_SIZE:
+                return 'The uploaded file exceeds the upload_max_filesize directive in php.ini.';
+
+            case UPLOAD_ERR_FORM_SIZE:
+                return 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.';
+
+            case UPLOAD_ERR_PARTIAL:
+                return 'The uploaded file was only partially uploaded.';
+
+            case UPLOAD_ERR_NO_FILE:
+                return 'No file was uploaded.';
+
+            case UPLOAD_ERR_NO_TMP_DIR:
+                return 'Missing a temporary folder.';
+
+            case UPLOAD_ERR_CANT_WRITE:
+                return 'Failed to write file to disk.';
+
+            case UPLOAD_ERR_EXTENSION:
+                return 'A PHP extension stopped the file upload.';
+
+            default:
+                return 'Unknown upload error.';
+        }
+    }
+
+    public function getMimeType(): string
+    {
+        return mime_content_type($this->tmp_name);
+    }
+
+    public function getPathname(): string
+    {
+        return $this->tmp_name;
+    }
+
+    public function isUploaded(): bool
     {
         return is_uploaded_file($this->tmp_name);
+    }
+
+    public function isValid(): bool
+    {
+        return $this->error === UPLOAD_ERR_OK;
     }
 
     public function moveTo(string $targetPath): bool
