771c260f998356737b81b5a8d80feef46f291d7f
Carlos Garcia <neorazorx@gmail.com>
2023-11-30 12:09:09 +0100

    Actualizado el Dinamic.

---
+++ Dinamic/Model/WorkEvent.php
@@ -0,0 +1,9 @@
+<?php namespace FacturaScripts\Dinamic\Model;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @author FacturaScripts <carlos@facturascripts.com>
+ */
+class WorkEvent extends \FacturaScripts\Core\Model\WorkEvent
+{
+}
