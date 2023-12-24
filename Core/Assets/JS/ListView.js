4c42c9695a38fbef2239deedb11a3dd1588ad340
Facundo <coregf@gmail.com>
2023-12-24 14:55:05 -0300

    Reemplazo de Bootbox en listViewDelete por Fsbootbox.confirmAction.

--- Core/Assets/JS/ListView.js
+++ Core/Assets/JS/ListView.js
@@ -22,56 +22,45 @@ var listViewDeleteMessage = "";
 var listViewDeleteTitle = "";
 
 function listViewCheckboxes(viewName) {
-    var checked = $("#form" + viewName + " .listActionCB").prop("checked");
-    $("#form" + viewName + " .listAction").each(function () {
-        $(this).prop("checked", checked);
-    });
+  var checked = $("#form" + viewName + " .listActionCB").prop("checked");
+  $("#form" + viewName + " .listAction").each(function () {
+    $(this).prop("checked", checked);
+  });
 }
 
 function listViewDelete(viewName) {
-    bootbox.confirm({
-        title: listViewDeleteTitle,
-        message: listViewDeleteMessage,
-        closeButton: false,
-        buttons: {
-            cancel: {
-                label: '<i class="fas fa-times"></i> ' + listViewDeleteCancel
-            },
-            confirm: {
-                label: '<i class="fas fa-check"></i> ' + listViewDeleteConfirm,
-                className: "btn-danger"
-            }
-        },
-        callback: function (result) {
-            if (result) {
-                listViewSetAction(viewName, "delete");
-            }
-        }
-    });
+  Fsbootbox.confirmAction(
+    viewName,
+    "delete",
+    listViewDeleteTitle,
+    listViewDeleteMessage
+  );
 
-    return false;
+  return false;
 }
 
 function listViewOpenTab(viewName) {
-    // buscamos todos los elementos con la clase toggle-ext-link
-    $("#form" + viewName + " .toggle-ext-link").each(function () {
-        // si tiene la clase d-none, la quitamos
-        if ($(this).hasClass("d-none")) {
-            $(this).removeClass("d-none");
-        } else {
-            // si no la tiene, la añadimos
-            $(this).addClass("d-none");
-        }
-    });
+  // buscamos todos los elementos con la clase toggle-ext-link
+  $("#form" + viewName + " .toggle-ext-link").each(function () {
+    // si tiene la clase d-none, la quitamos
+    if ($(this).hasClass("d-none")) {
+      $(this).removeClass("d-none");
+    } else {
+      // si no la tiene, la añadimos
+      $(this).addClass("d-none");
+    }
+  });
 }
 
 function listViewPrintAction(viewName, option) {
-    $("#form" + viewName).attr("target", "_blank");
-    $("#form" + viewName + " :input[name=\"action\"]").val('export');
-    $("#form" + viewName).append('<input type="hidden" name="option" value="' + option + '"/>');
-    $("#form" + viewName).submit();
-    $("#form" + viewName + " :input[name=\"action\"]").val('');
-    $("#form" + viewName).attr("target", "");
+  $("#form" + viewName).attr("target", "_blank");
+  $("#form" + viewName + ' :input[name="action"]').val("export");
+  $("#form" + viewName).append(
+    '<input type="hidden" name="option" value="' + option + '"/>'
+  );
+  $("#form" + viewName).submit();
+  $("#form" + viewName + ' :input[name="action"]').val("");
+  $("#form" + viewName).attr("target", "");
 }
 
 function listViewSetAction(viewName, value) {
@@ -80,53 +69,53 @@ function listViewSetAction(viewName, value) {
 }
 
 function listViewSetLoadFilter(viewName, value) {
-    $("#form" + viewName + " :input[name=\"loadfilter\"]").val(value);
-    $("#form" + viewName).submit();
+  $("#form" + viewName + ' :input[name="loadfilter"]').val(value);
+  $("#form" + viewName).submit();
 }
 
 function listViewSetOffset(viewName, value) {
-    $("#form" + viewName + " :input[name=\"action\"]").val('');
-    $("#form" + viewName + " :input[name=\"offset\"]").val(value);
-    $("#form" + viewName).submit();
+  $("#form" + viewName + ' :input[name="action"]').val("");
+  $("#form" + viewName + ' :input[name="offset"]').val(value);
+  $("#form" + viewName).submit();
 }
 
 function listViewSetOrder(viewName, value) {
-    $("#form" + viewName + " :input[name=\"action\"]").val('');
-    $("#form" + viewName + " :input[name=\"order\"]").val(value);
-    $("#form" + viewName).submit();
+  $("#form" + viewName + ' :input[name="action"]').val("");
+  $("#form" + viewName + ' :input[name="order"]').val(value);
+  $("#form" + viewName).submit();
 }
 
 function listViewShowFilters(viewName) {
-    $("#form" + viewName + "Filters").toggle(500);
+  $("#form" + viewName + "Filters").toggle(500);
 }
 
 $(document).ready(function () {
-    $(".clickableListRow").mousedown(function (event) {
-        if (event.which === 1 || event.which === 2) {
-            var href = $(this).attr("data-href");
-            var target = $(this).attr("data-target");
-            if (typeof href !== typeof undefined && href !== false) {
-                if (typeof target !== typeof undefined && target === "_blank") {
-                    window.open($(this).attr("data-href"));
-                } else if (event.which === 2) {
-                    // buscamos todos los elementos con la clase toggle-ext-link
-                    $(".toggle-ext-link").each(function () {
-                        // si tiene la clase d-none, la quitamos
-                        if ($(this).hasClass("d-none")) {
-                            $(this).removeClass("d-none");
-                        } else {
-                            // si no la tiene, la añadimos
-                            $(this).addClass("d-none");
-                        }
-                    });
-                } else {
-                    parent.document.location = $(this).attr("data-href");
-                }
+  $(".clickableListRow").mousedown(function (event) {
+    if (event.which === 1 || event.which === 2) {
+      var href = $(this).attr("data-href");
+      var target = $(this).attr("data-target");
+      if (typeof href !== typeof undefined && href !== false) {
+        if (typeof target !== typeof undefined && target === "_blank") {
+          window.open($(this).attr("data-href"));
+        } else if (event.which === 2) {
+          // buscamos todos los elementos con la clase toggle-ext-link
+          $(".toggle-ext-link").each(function () {
+            // si tiene la clase d-none, la quitamos
+            if ($(this).hasClass("d-none")) {
+              $(this).removeClass("d-none");
+            } else {
+              // si no la tiene, la añadimos
+              $(this).addClass("d-none");
             }
+          });
+        } else {
+          parent.document.location = $(this).attr("data-href");
         }
-    });
-    // disable enter key press
-    $(".noEnterKey").keypress(function (e) {
-        return !(e.which == 13 || e.keyCode == 13);
-    });
+      }
+    }
+  });
+  // disable enter key press
+  $(".noEnterKey").keypress(function (e) {
+    return !(e.which == 13 || e.keyCode == 13);
+  });
 });
