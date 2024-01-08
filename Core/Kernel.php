15cc45d8488c25f227c88a073dde5a90d1e4b79f
Carlos Garcia Gomez <neorazorx@gmail.com>
2024-01-08 19:53:37 +0100

    Solucionado bug al personalizar pantallas de error desde plugins.

--- Core/Kernel.php
+++ Core/Kernel.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2024 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -208,6 +208,11 @@ final class Kernel
 
             $mainClass = '\\FacturaScripts\\Core\\Error\\' . $exception->handler;
             return new $mainClass($exception);
+        }
+
+        $dynClass = '\\FacturaScripts\\Dinamic\\Error\\DefaultError';
+        if (class_exists($dynClass)) {
+            return new $dynClass($exception);
         }
 
         return new DefaultError($exception);
--- Core/Template/ErrorController.php
+++ Core/Template/ErrorController.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2024 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -30,7 +30,7 @@ abstract class ErrorController implements ErrorControllerInterface
     protected $exception;
 
     /** @var bool */
-    private $save_crash = true;
+    protected $save_crash = true;
 
     /** @var string */
     protected $url;
