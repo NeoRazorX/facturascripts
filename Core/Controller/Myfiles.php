2bae080711b8da4aba909077008fe7a1e9f3fee6
Carlos Garcia Gomez <neorazorx@gmail.com>
2024-01-20 14:23:15 +0100

    Fixed XSS via fake png file (svg). --- Solucionado bug XSS mediante archivo svg malicioso disfrazado de png.

--- Core/Controller/Myfiles.php
+++ Core/Controller/Myfiles.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2024 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -97,7 +97,7 @@ class Myfiles implements ControllerInterface
         }
 
         // force to download svg files to prevent XSS attacks
-        if (strpos($this->filePath, '.svg') !== false) {
+        if ($this->isSvg($this->filePath)) {
             header('Content-Disposition: attachment; filename="' . basename($this->filePath) . '"');
         }
 
@@ -121,5 +121,20 @@ class Myfiles implements ControllerInterface
         }
 
         return mime_content_type($filePath);
+    }
+
+    private function isSvg(string $filePath): bool
+    {
+        // comprobamos la extensión
+        if (strpos($filePath, '.svg') !== false) {
+            return true;
+        }
+
+        // comprobamos mime
+        if (strpos($this->getMime($filePath), 'image/svg') !== false) {
+            return true;
+        }
+
+        return false;
     }
 }
--- Core/Model/ProductoImagen.php
+++ Core/Model/ProductoImagen.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2012-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2012-2024 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -29,6 +29,7 @@ use Throwable;
 /**
  * Description of ProductoImagen
  *
+ * @author Carlos García Gómez           <carlos@facturascripts.com>
  * @author José Antonio Cuello Principal <yopli2000@gmail.com>
  */
 class ProductoImagen extends Base\ModelClass
@@ -179,6 +180,17 @@ class ProductoImagen extends Base\ModelClass
     public static function tableName(): string
     {
         return 'productos_imagenes';
+    }
+
+    public function test(): bool
+    {
+        // si el archivo no es una imagen, devolvemos false
+        if (false === $this->getFile()->isImage()) {
+            Tools::log()->error('not-valid-image');
+            return false;
+        }
+
+        return parent::test();
     }
 
     public function url(string $type = 'auto', string $list = 'List'): string
