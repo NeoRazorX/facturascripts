c697060e2db8a7b3dc42b0649825cc5959e57f4a
Carlos Garcia Gomez <neorazorx@gmail.com>
2024-01-24 13:30:39 +0100

    Mejorada la búsqueda de asientos descuadrados.

--- Core/Controller/ListAsiento.php
+++ Core/Controller/ListAsiento.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2017-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2024 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -151,7 +151,7 @@ class ListAsiento extends ListController
     {
         $ids = [];
         $sql = 'SELECT partidas.idasiento, ABS(SUM(partidas.debe) - SUM(partidas.haber))'
-            . ' FROM partidas GROUP BY 1 HAVING ABS(SUM(partidas.debe) - SUM(partidas.haber)) >= 0.01';
+            . ' FROM partidas GROUP BY 1 HAVING ROUND(ABS(SUM(partidas.debe) - SUM(partidas.haber)), 2) >= 0.01';
         foreach ($this->dataBase->select($sql) as $row) {
             $ids[] = $row['idasiento'];
         }
