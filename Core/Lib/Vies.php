9a6e7c4c85aeabd18e14c2f1d787d1f01069986e
Daniel Fernández <daniel89fg@gmail.com>
2024-02-22 13:55:08 +0100

    Tarea #3150 - Comprobar que el país sea de la unión europea al comprobar vies

--- Core/Lib/Vies.php
+++ Core/Lib/Vies.php
@@ -31,6 +31,13 @@ class Vies
 {
     const VIES_URL = "https://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl";
 
+    // constante con los países de la unión europea
+    const EU_COUNTRIES = [
+        'AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'EL', 'ES',
+        'FI', 'FR', 'HR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV',
+        'MT', 'NL', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK',
+    ];
+
     private static $lastError = '';
 
     public static function check(string $cifnif, string $codiso): int
@@ -38,6 +45,12 @@ class Vies
         // comprobamos si la extensión soap está instalada
         if (!extension_loaded('soap')) {
             Tools::log()->warning('soap-extension-not-installed');
+            return -1;
+        }
+
+        // si el país no es de la unión europea, devolvemos error
+        if (!in_array($codiso, self::EU_COUNTRIES)) {
+            Tools::log()->warning('country-not-in-eu', ['%codiso%' => $codiso]);
             return -1;
         }
 
