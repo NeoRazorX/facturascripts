827da11c41f17e5a2a806c1eae00ab62150b528b
Antonio <elguitarraverde@hotmail.com>
2024-03-01 12:47:17 +0100

    actualizar y configurar phpstan

---
+++ .ci/code-analysers-config.php
@@ -0,0 +1,34 @@
+<?php
+
+
+define('FS_COOKIES_EXPIRE', 604800);
+
+define('FS_LANG', 'es_ES');
+define('FS_TIMEZONE', 'Europe/Madrid');
+define('FS_FOLDER', '');
+define('FS_ROUTE', '');
+
+define('FS_DB_TYPE', 'mysql');
+define('FS_DB_HOST', 'localhost');
+define('FS_DB_PORT', '3306');
+define('FS_DB_NAME', 'facturascripts');
+define('FS_DB_USER', 'root');
+define('FS_DB_PASS', 'toor');
+define('FS_DB_FOREIGN_KEYS', true);
+define('FS_DB_TYPE_CHECK', true);
+define('FS_MYSQL_CHARSET', 'utf8');
+define('FS_MYSQL_COLLATE', 'utf8_bin');
+
+define('FS_HIDDEN_PLUGINS', '');
+define('FS_DEBUG', false);
+define('FS_DISABLE_ADD_PLUGINS', false);
+define('FS_DISABLE_RM_PLUGINS', false);
+
+define('FS_NF0', 2);
+define('FS_NF1', ',');
+define('FS_NF2', ' ');
+
+define('FS_CURRENCY_POS', 'right');
+
+define('FS_ITEM_LIMIT', 50);
+
