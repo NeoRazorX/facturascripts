2f383b3f1d841b6581aff3bc1109c91c4186451b
Carlos Garcia Gomez <neorazorx@gmail.com>
2024-06-05 18:14:04 +0200

    Solucionado bug al visualizar archivos vinculados a modelos de plugins que no existe.

--- Core/Model/AttachedFileRelation.php
+++ Core/Model/AttachedFileRelation.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2021-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2021-2024 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -124,10 +124,12 @@ class AttachedFileRelation extends Base\ModelClass
     {
         if ($this->model) {
             $modelClass = '\\FacturaScripts\\Dinamic\\Model\\' . $this->model;
-            $model = new $modelClass();
-            $code = empty($this->modelcode) ? $this->modelid : $this->modelcode;
-            if ($model->loadFromCode($code)) {
-                return $model->url();
+            if (class_exists($modelClass)) {
+                $model = new $modelClass();
+                $code = empty($this->modelcode) ? $this->modelid : $this->modelcode;
+                if ($model->loadFromCode($code)) {
+                    return $model->url();
+                }
             }
         }
 
