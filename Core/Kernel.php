bb36a05bc99e5959d331d116f66abf26156ef9a1
Carlos Garcia Gomez <neorazorx@gmail.com>
2024-06-17 18:44:51 +0200

    Solucionado bug al escapar el html en los campos del modelo ApiKey.

--- Core/Kernel.php
+++ Core/Kernel.php
@@ -232,7 +232,7 @@ final class Kernel
 
     public static function version(): float
     {
-        return 2024.7;
+        return 2024.8;
     }
 
     private static function getErrorHandler(Exception $exception): ErrorControllerInterface
--- Core/Model/ApiKey.php
+++ Core/Model/ApiKey.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2018-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2018-2024 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -75,6 +75,16 @@ class ApiKey extends Base\ModelClass
     public static function tableName(): string
     {
         return 'api_keys';
+    }
+
+    public function test(): bool
+    {
+        // escapamos el html
+        $this->apikey = Tools::noHtml($this->apikey);
+        $this->description = Tools::noHtml($this->description);
+        $this->nick = Tools::noHtml($this->nick);
+
+        return parent::test();
     }
 
     public function url(string $type = 'auto', string $list = 'EditSettings?activetab=List'): string
