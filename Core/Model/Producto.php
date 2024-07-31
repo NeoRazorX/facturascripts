62969480745709df41ad55f608c550da670c9ebb
Carlos Garcia Gomez <neorazorx@gmail.com>
2024-07-31 13:18:27 +0200

    Pequeñas mejoras al código.

--- Core/Model/Producto.php
+++ Core/Model/Producto.php
@@ -386,7 +386,7 @@ class Producto extends ModelClass
         }
 
         // si hay cambios, actualizamos el producto
-        if ($newPrecio != $this->precio || strcmp($newReferencia, $this->referencia) !== 0) {
+        if ($newPrecio != $this->precio || $newReferencia !== $this->referencia) {
             $this->precio = $newPrecio;
             $this->referencia = $newReferencia;
             $this->save();
