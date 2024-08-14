7127527d9494bcf41940139689cd22c02b0042dd
Carlos Garcia Gomez <neorazorx@gmail.com>
2024-08-14 17:25:28 +0200

    Actualizada la versión.

--- Core/Kernel.php
+++ Core/Kernel.php
@@ -232,7 +232,7 @@ final class Kernel
 
     public static function version(): float
     {
-        return 2024.9;
+        return 2024.91;
     }
 
     private static function getErrorHandler(Exception $exception): ErrorControllerInterface
