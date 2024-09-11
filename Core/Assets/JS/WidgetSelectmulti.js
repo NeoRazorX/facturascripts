7828ff4feaa7897c089ec0c66e2f9706135c9f9f
Daniel Fernández <daniel89fg@gmail.com>
2024-09-11 08:40:10 +0200

    correciones

---
+++ Core/Assets/JS/WidgetSelectmulti.js
@@ -0,0 +1,4 @@
+$('select.select2').select2({
+    width: 'style',
+    theme: 'bootstrap4'
+});
--- Core/Lib/Widget/WidgetSelectmulti.php
+++ Core/Lib/Widget/WidgetSelectmulti.php
@@ -1,34 +1,44 @@
 <?php
+/**
+ * This file is part of FacturaScripts
+ * Copyright (C) 2024 Carlos Garcia Gomez <carlos@facturascripts.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace FacturaScripts\Core\Lib\Widget;
 
 use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
 use FacturaScripts\Core\Lib\AssetManager;
 
+/**
+ * Description of WidgetSelectmulti
+ *
+ * @author Daniel Fernández Giménez <hola@danielfg.es>
+ */
 class WidgetSelectmulti extends WidgetSelect
 {
-    /**
-     *
-     * @var string
-     */
+    /** @var string */
     protected $modelDest;
 
-    /**
-     *
-     * @var string
-     */
+    /** @var string */
     protected $modelID1;
 
-    /**
-     *
-     * @var string
-     */
+    /** @var string */
     protected $modelID2;
 
-    /**
-     *
-     * @var mixed
-     */
+    /** @var mixed */
     protected $modelValue;
 
     /**
