2bc2bb60672d33fd3a95d6a3c98074d1aeeb2db4
Antonio <elguitarraverde@hotmail.com>
2024-09-13 19:55:40 +0200

    Tarea #1272 - Actualizar a Bootstrap 5

--- Core/Lib/ExtendedController/PanelController.php
+++ Core/Lib/ExtendedController/PanelController.php
@@ -113,7 +113,7 @@ abstract class PanelController extends BaseController
     }
 
     /**
-     * Sets the tabs position, by default is set to 'left', also supported 'bottom', 'top' and 'start-bottom.
+     * Sets the tabs position, by default is set to 'left', also supported 'bottom', 'top' and 'left-bottom.
      *
      * @param string $position
      */
@@ -125,7 +125,7 @@ abstract class PanelController extends BaseController
                 $this->setTemplate('Master/PanelControllerBottom');
                 break;
 
-            case 'start-bottom':
+            case 'left-bottom':
                 $this->setTemplate('Master/PanelControllerLeftBottom');
                 break;
 
