efc0c3ed776b6a6f78135bf9c674422cb0ff5a10
Carlos Garcia Gomez <neorazorx@gmail.com>
2025-02-12 12:24:42 +0100

    Solucionado bug cuando el email es null.

--- Core/Controller/SendMail.php
+++ Core/Controller/SendMail.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2018-2023 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2018-2025 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -342,7 +342,7 @@ class SendMail extends Controller
         $model->loadFromCode($this->request->get('modelCode', ''));
         $this->loadDataDefault($model);
 
-        if (property_exists($model, 'email')) {
+        if (property_exists($model, 'email') && $model->email) {
             $this->newMail->to($model->email);
             return;
         }
