8778181d78d2cc7c21872875766a312b7ebfc2df
Carlos Garcia Gomez <neorazorx@gmail.com>
2025-04-06 22:38:41 +0200

    La lista de empresas se carga ahora de caché.

--- Core/Base/Controller.php
+++ Core/Base/Controller.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2013-2024 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2013-2025 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -133,7 +133,7 @@ class Controller implements ControllerInterface
         Session::set('uri', $uri);
 
         $this->dataBase = new DataBase();
-        $this->empresa = new Empresa();
+        $this->empresa = Empresas::default();
         $this->multiRequestProtection = new MultiRequestProtection();
         $this->request = Request::createFromGlobals();
         $this->template = $this->className . '.html.twig';
