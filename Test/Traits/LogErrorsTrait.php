1b8f08fe75e3347a93f653b0f3de8e6bed02a84d
Carlos Garcia <carlos@facturascripts.com>
2025-05-25 14:12:26 +0200

    Ahora cuando falla un test se guarda un archivo de log en MyFiles con las consultas sql ejecutadas.

--- Test/Traits/LogErrorsTrait.php
+++ Test/Traits/LogErrorsTrait.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2021-2022 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2021-2025 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -20,6 +20,7 @@
 namespace FacturaScripts\Test\Traits;
 
 use FacturaScripts\Core\Base\MiniLog;
+use FacturaScripts\Core\Tools;
 
 trait LogErrorsTrait
 {
@@ -32,6 +33,15 @@ trait LogErrorsTrait
                     error_log(print_r($item['context'], true));
                 }
             }
+
+            // guardamos la lista de consultas sql en un archivo
+            $queries = [];
+            foreach (MiniLog::read('database', ['debug']) as $item) {
+                $queries[] = $item['message'];
+            }
+            $file_path = Tools::folder('MyFiles', 'test_error_' . date('Y-m-d_H-i-s_') . rand(0, 1000) . '.log');
+            file_put_contents($file_path, implode(PHP_EOL, $queries) . PHP_EOL, FILE_APPEND);
+            error_log('Database queries in ' . $file_path . PHP_EOL);
         }
 
         MiniLog::clear();
