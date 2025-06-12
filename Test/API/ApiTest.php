476fff16faf1d104e7d5b5b25d23893d1252239c
Pablo Ferrandez Roca <pferrandezroca2@gmail.com>
2025-06-12 12:56:09 +0200

    modificados tests

---
+++ Test/API/ApiTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ApiTest extends TestCase
+{
+
+    public function testApiConnection()
+    {
+        $url = "http://127.0.0.2:8000/api/3/proveedores";
+        $token = "prueba";
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            "Token: $token"
+        ]);
+        $respuesta = curl_exec($ch);
+        echo 'Respuesta: ' . $respuesta;
+        $this->assertEquals('[{"cifnif": "","email": "","fax": "","fechaalta": "03-06-2025","langcode": "es_ES","nombre": "pepe","observaciones": "","personafisica": true,"telefono1": "","telefono2": "","tipoidfiscal": "NIF","codcliente": null,"codpago": null,"codproveedor": "1","codretencion": null,"codserie": null,"codsubcuenta": "","debaja": false,"fechabaja": null,"razonsocial": "pepe","regimeniva": "General","web": "","codimpuestoportes": "IVA21","idcontacto": 1}', $respuesta);
+    }
+}
+/*
+$url = "http://127.0.0.2:8000/api/3/divisas?filter[descripcion_like]=PESO";
+$token = "prueba";
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "Token: $token"
+]);
+
+$respuesta = curl_exec($ch);
+
+if (curl_errno($ch)) {
+    echo 'Error en la solicitud: ' . curl_error($ch);
+} else {
+    echo 'Respuesta: ' . $respuesta;
+}
+
+curl_close($ch);
+*/
