e638c45acc79258b2486d8c63305942081738f75
Antonio <elguitarraverde@hotmail.com>
2025-08-04 12:50:05 +0200

    actualizar y usar phpstan

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+define('FS_CODPAIS', '');
+define('FS_COOKIES_EXPIRE', '');
+define('FS_DB_FOREIGN_KEYS', '');
+define('FS_DB_HOST', '');
+define('FS_DB_NAME', '');
+define('FS_DB_PASS', '');
+define('FS_DB_PORT', '');
+define('FS_DB_TYPE', '');
+define('FS_DB_TYPE_CHECK', '');
+define('FS_DB_USER', '');
+define('FS_DEBUG', '');
+define('FS_FOLDER', '');
+define('FS_ITEM_LIMIT', '');
+define('FS_LANG', '');
+define('FS_NF0', '');
+define('FS_NF1', '');
+define('FS_ROUTE', '');
