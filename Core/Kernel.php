e649b704de7498db6dec5666b674c05154e55f1e
Carlos Garcia <carlos@facturascripts.com>
2025-09-23 16:56:20 +0200

    Update version number and deprecate getClientOriginalExtension method

--- Core/Kernel.php
+++ Core/Kernel.php
@@ -244,7 +244,7 @@ final class Kernel
 
     public static function version(): float
     {
-        return 2025.4;
+        return 2025.41;
     }
 
     private static function checkControllerClass(string $controller): array
