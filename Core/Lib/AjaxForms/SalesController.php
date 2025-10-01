26fc02d1c53fd091e54151d8e2175360b00dabee
Daniel Fernández Giménez <hola@danielfg.es>
2025-10-01 17:44:42 +0200

    Cambios en la forma de marcar como pagada una factura desde el modal

--- Core/Lib/AjaxForms/SalesController.php
+++ Core/Lib/AjaxForms/SalesController.php
@@ -434,15 +434,13 @@ abstract class SalesController extends PanelController
             return false;
         }
 
-        // guardamos el documento
-        if ($this->getModel()->editable && false === $this->saveDocAction()) {
-            return false;
-        }
+        // cargamos el modelo actualizado y los datos del form
+        $model = $this->getModel();
+        $formData = json_decode($this->request->input('data'), true);
 
         // si la factura es de 0 €, la marcamos como pagada
-        $model = $this->getModel();
         if (empty($model->total) && $model->hasColumn('pagada')) {
-            $model->pagada = (bool)$this->request->input('selectedLine');
+            $model->pagada = (bool)$formData['paid-status'];
             $model->save();
             $this->sendJsonWithLogs(['ok' => true, 'newurl' => $model->url() . '&action=save-ok']);
             return false;
@@ -457,15 +455,14 @@ abstract class SalesController extends PanelController
         }
 
         // marcamos los recibos como pagados, eso marca la factura como pagada
-        $formData = json_decode($this->request->input('data'), true);
         foreach ($receipts as $receipt) {
             $receipt->nick = $this->user->nick;
             // si no está pagado, actualizamos fechapago y codpago
             if (false == $receipt->pagado) {
-                $receipt->fechapago = $formData['fechapagorecibo'] ?? Tools::date();
-                $receipt->codpago = $model->codpago;
+                $receipt->fechapago = $formData['paid-date-modal'] ?? Tools::date();
+                $receipt->codpago = $formData['paid-payment-modal'] ?? $model->codpago;
             }
-            $receipt->pagado = (bool)$this->request->input('selectedLine');
+            $receipt->pagado = (bool)$formData['paid-status'];
             if (false === $receipt->save()) {
                 $this->sendJsonWithLogs(['ok' => false]);
                 return false;
