bd597bbf6fecd9841af6b11735234f0397c7e2b7
Carlos Garcia <carlos@facturascripts.com>
2026-01-21 12:39:04 +0100

    - Actualizadas las cabeceras al exportar a CSV, PDF o Excel. - Solucionado problema con el content-type de los xlsx en algunos casos.

--- Core/Lib/Export/CSVExport.php
+++ Core/Lib/Export/CSVExport.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2013-2021 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2013-2026 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -241,7 +241,7 @@ class CSVExport extends ExportBase
     public function show(Response &$response)
     {
         $response->headers->set('Content-Type', 'text/csv; charset=utf-8');
-        $response->headers->set('Content-Disposition', 'attachment;filename=' . $this->getFileName() . '.csv');
+        $response->headers->set('Content-Disposition', 'attachment; filename=' . $this->getFileName() . '.csv');
         $response->setContent($this->getDoc());
     }
 
