93d59bce8a65f77b92ea88c46e9ac9a83181144b
Carlos Garcia <carlos@facturascripts.com>
2026-01-26 12:48:18 +0100

    Actualizada la versión.

--- Core/Kernel.php
+++ Core/Kernel.php
@@ -245,7 +245,7 @@ final class Kernel
 
     public static function version(): float
     {
-        return 2025.8;
+        return 2025.81;
     }
 
     private static function checkControllerClass(string $controller): array
