5a29336c69909abc063d35ed2699c0ffb4958bfe
Carlos Garcia <neorazorx@gmail.com>
2026-03-11 20:28:39 +0100

    Actualizada la versión

--- Core/Kernel.php
+++ Core/Kernel.php
@@ -245,7 +245,7 @@ final class Kernel
 
     public static function version(): float
     {
-        return 2025.93;
+        return 2026;
     }
 
     private static function checkControllerClass(string $controller): array
