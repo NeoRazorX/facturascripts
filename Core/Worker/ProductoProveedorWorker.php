d0fd50c30f477abf55477c2ba0dbc31a2a7ea945
Esteban Sánchez Martínez <estebansm85@gmail.com>
2026-05-05 13:05:38 +0200

    Corregidas fechas copyright

--- Core/Worker/ProductoProveedorWorker.php
+++ Core/Worker/ProductoProveedorWorker.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of FacturaScripts
- * Copyright (C) 2025 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2026 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -24,6 +24,10 @@ use FacturaScripts\Core\Model\WorkEvent;
 use FacturaScripts\Core\Template\WorkerClass;
 use FacturaScripts\Core\Where;
 use FacturaScripts\Dinamic\Model\Variante;
+
+/**
+ * @author Esteban Sánchez Martínez <esteban@factura.city>
+ */
 
 class ProductoProveedorWorker extends WorkerClass
 {
